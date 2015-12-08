bc882377ce28c4a8a53d9dfa12e8786c84737607
Jamie McAtamney <jmcatamney@pivotal.io>
2015-12-08 11:12:52 -0800

    Refactored 'there are no backup files' step

--- gpMgmt/bin/gppylib/test/behave_utils/utils.py
+++ gpMgmt/bin/gppylib/test/behave_utils/utils.py
@@ -801,26 +801,27 @@ def get_hosts(dbname='template1'):
     get_hosts_sql = "select distinct hostname from gp_segment_configuration where role='p';"
     return getRows(dbname, get_hosts_sql)
 
-def get_backup_dir_for_host(hostname, dbname='template1'):
-    get_backup_dir_sql = "select f.fselocation from pg_filespace_entry f inner join gp_segment_configuration g on \
-                            f.fsedbid=g.dbid and g.role='p' and g.hostname = '%s'" % (hostname)
-    return getRows(dbname, get_backup_dir_sql)
-   
-def cleanup_dir(context, host, location):
-    cleanup_cmd = "gpssh -h %s -e 'rm -rf %s/db_dumps %s/gpcrondump.pid'" % (host, location, location)
-    run_command(context, cleanup_cmd)
-    if context.exception:
-        raise context.exception
-    
+def get_backup_dirs_for_hosts(dbname='template1'):
+    get_backup_dir_sql = "select f.fselocation from pg_filespace_entry f inner join gp_segment_configuration g on f.fsedbid=g.dbid and g.role='p'"
+    results = getRows(dbname, get_backup_dir_sql)
+    dir_map = {}
+    for res in results:
+        host,dir = res
+        dir_map.setdefault(host,[]).append(dir)
+    return dir_map
+
 def cleanup_backup_files(context, dbname, location=None):
-    hosts = get_hosts(dbname)
-    for host in hosts:
+    dir_map = get_backup_dirs_for_hosts(dbname)
+    for host in dir_map:
         if location:
-            cleanup_dir(context, host[0], location)
+            cmd_str = "ssh %s 'DIR=%s;if [ -d \"$DIR/db_dumps/\" ]; then rm -rf $DIR/db_dumps $DIR/gpcrondump.pid; fi'"
+            cmd = cmd_str % (host, location)
         else:
-            dirs = get_backup_dir_for_host(host[0], dbname)
-            for dir in dirs:
-                cleanup_dir(context, host[0], dir[0])
+            cmd_str = "ssh %s 'for DIR in %s; do if [ -d \"$DIR/db_dumps/\" ]; then rm -rf $DIR/db_dumps $DIR/gpcrondump.pid; fi; done'"
+            cmd = cmd_str % (host, " ".join(dir_map[host]))
+        run_command(context, cmd)
+        if context.exception:
+            raise context.exception
 
 def cleanup_report_files(context, master_data_dir):
     if not master_data_dir:
@@ -1334,16 +1335,15 @@ def wait_till_resync_transition(host='localhost', port=os.environ.get('PGPORT'),
         return True 
 
 def check_dump_dir_exists(context, dbname):
-    hosts = get_hosts(dbname)
-    for host in hosts:
-	dirs = get_backup_dir_for_host(host[0], dbname)
-	for dir in dirs:
-	    cmd = "gpssh -h %s 'if [ -d %s/db_dumps/ ]; then echo 'EXISTS'; else echo 'NOT FOUND'; fi'" % (host[0], dir[0])
-	    run_command(context, cmd)
-	    if context.exception:
-		raise context.exception
-	    if 'EXISTS' in context.stdout_message:
-		raise Exception("db_dumps directory is present in master/segments.")
+    dir_map = get_backup_dirs_for_hosts(dbname)
+    cmd_str = "ssh %s 'for DIR in %s; do if [ -d \"$DIR/db_dumps/\" ]; then echo \"$DIR EXISTS\"; else echo \"$DIR NOT FOUND\"; fi; done'"
+    for host in dir_map:
+        cmd = cmd_str % (host, " ".join(dir_map[host]))
+        run_command(context, cmd)
+        if context.exception:
+            raise context.exception
+        if 'EXISTS' in context.stdout_message:
+            raise Exception("db_dumps directory is present in master/segments.")
 
 def verify_restored_table_is_analyzed(context, table_name, dbname):
     ROW_COUNT_SQL = """SELECT count(*) FROM %s""" % table_name
