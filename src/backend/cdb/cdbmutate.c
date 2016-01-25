04691fd6f1d8cddd2e9dd283a369925e54ab8d20
Xin Zhang <xzhang@pivotal.io>
2016-01-25 11:08:55 -0800

    Add debug assert and ERROR message when sub-plan for share_id is NULL.

--- src/backend/cdb/cdbmutate.c
+++ src/backend/cdb/cdbmutate.c
@@ -2167,9 +2167,15 @@ shareinput_mutator_xslice_3(Node *node, ApplyShareInputContext *ctxt, bool fPop)
 
 		shareinput_find_sharenode(ctxt, sisc->share_id, &plan_slicemark);
 
+		Assert(plan_slicemark.plan);
+
 		if(plan_slicemark.plan)
 		{
 			stype = get_plan_share_type(plan_slicemark.plan);
+		}
+		else
+		{
+			elog(ERROR, "Sub-plan for share_id %d cannot be NULL.", sisc->share_id);
 		}
 
 		switch(stype)
