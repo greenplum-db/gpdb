9d11c22307e69167266d7db0a66e6112d26f61ad
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-02-11 13:54:00 +0000

    Removing "gpadmin" user creation as it causes problems with source code access rights

--- vagrant/centos/vagrant-setup.sh
+++ vagrant/centos/vagrant-setup.sh
@@ -17,14 +17,11 @@ sudo yum -y install openssl-libs openssl-devel
 sudo yum -y install libyaml libyaml-devel
 sudo yum -y install epel-release
 sudo yum -y install htop
+sudo yum -y install perl-Env
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip install psi lockfile paramiko setuptools epydoc
 rm get-pip.py
-
-# Create gpadmin user
-sudo useradd gpadmin -d /home/gpadmin -s /bin/bash
-echo changeme | sudo passwd gpadmin --stdin
 
 # Misc
 sudo yum -y install vim mc psmisc
