79073892d24017130bd688a7797e95aac8f0ecb4
Venkatesh Raghavan <vraghavan@pivotal.io>
2016-03-04 13:58:28 -0800

    Porting Subquery

--- src/test/regress/sql/qp_subquery.sql
+++ src/test/regress/sql/qp_subquery.sql
@@ -282,14 +282,263 @@ insert into Tbl8352_t2a values(1,2);
 select * from Tbl8352_t2a;
 select * from Tbl8352_t1a where (Tbl8352_t1a.a,Tbl8352_t1a.b) not in (select Tbl8352_t2a.a,Tbl8352_t2a.b from Tbl8352_t2a) order by 1,2;
 
-drop table Tbl8352_t1;
-drop table Tbl8352_t2;
-drop table Tbl8352_t1a;
-drop table Tbl8352_t2a;
-
 select (1,null::int) not in (select 1,1);
 select (3,null::int) not in (select 1,1);
 
+-- start_ignore
+create table t1(a int, b int);
+create table t2(a int, b int);
+create table t3(a int, b int);
+create table t4(a int, b int);
+
+insert into t1 values(1,2);
+insert into t1 values(3,4);
+insert into t1 values(5,6);
+
+insert into t2 values(1,2);
+insert into t2 values(3,4);
+insert into t2 values(7,8);
+
+insert into t3 values(1,2);
+insert into t3 values(3,4);
+
+insert into t4 values(1,2);
+
+create table i1(a int, b int);
+create table i2(a int, b int);
+
+insert into i1 values(1,2);
+-- end_ignore
+
+--
+-- not in subquery involving vars from different rels with inner join
+--
+
+select t1.a, t2.b from t1, t2 where t1.a=t2.a and ((t1.a,t2.b) not in (select i1.a,i1.b from i1));
+
+select t1.a, t2.b from t1 inner join t2 on  (t1.a=t2.a and ((t1.a,t2.b) not in (select i1.a,i1.b from i1)));
+
+select t1.a, t2.b from t1 inner join t2 on  (t1.a=t2.a) where ((t1.a,t2.b) not in (select i1.a,i1.b from i1));
+
+-- unsupported case
+explain select t1.a, t2.b from t1, t2 where t1.a=t2.a or ((t1.a,t2.b) not in (select i1.a,i1.b from i1));
+
+--
+-- not in subquery involving vars from different rels with left join. 
+--
+select t1.a, t2.b from t1 left join t2 on  (t1.a=t2.a) where ((t1.a,t2.b) not in (select i1.a,i1.b from i1));
+
+select t1.a, t2.b from t1 left join t2 on  (t1.a=t2.a and ((t1.a,t2.b) not in (select i1.a,i1.b from i1)));
+
+--
+-- not in subquery involving vars from different rels with outer join
+--
+
+select t1.a, t2.b from t1 full outer join t2 on  (t1.a=t2.a) where ((t1.a,t2.b) not in (select i1.a,i1.b from i1));
+
+-- unsupported case
+select t1.a, t2.b from t1 full outer join t2 on  (t1.a=t2.a and ((t1.a,t2.b) not in (select i1.a,i1.b from i1))); 
+
+--
+-- more complex case
+--
+
+select t1.a,t2.b from t1 left join (t2 inner join t3 on (t3.a not in (select t4.a from t4))) on (t1.a=t2.a);
+
+-- start_ignore
+create table Tbl01(a int, b int, c int);
+
+insert into Tbl01 values(1,2,3);
+insert into Tbl01 values(4,5,6);
+insert into Tbl01 values(7,8,9);
+insert into Tbl01 values(null,11,12);
+
+create table Tbl03(a int);
+
+insert into Tbl03 values(1),(4);
+
+create or replace function foo(int) returns int as $$
+	select case when $1 is null then 13::int
+	       	    else null::int
+	       end;
+$$ language sql immutable;
+-- end_ignore
+
+select Tbl01.*,foo(Tbl01.a) as foo from Tbl01; -- showing foo values
+
+select Tbl01.* from Tbl01 where foo(Tbl01.a) not in (select a from Tbl03);
+
+-- start_ignore
+create table Tbl02 as select Tbl01.*,foo(Tbl01.a) as foo from Tbl01;
+-- end_ignore
+
+select Tbl02.* from Tbl02 where foo not in (select a from Tbl03);
+
+-- start_ignore
+create table Tbl04(a int, b int);
+insert into Tbl04 values(1,2),(3,4),(5,6);
+
+create table Tbl05(a int, b int);
+insert into Tbl05 values(1,2);
+
+create table Tbl06(a int, b int);
+insert into Tbl06 values(1,2),(3,4);
+
+create table i3(a int not null, b int not null);
+insert into i3 values(1,2);
+
+create table Tbl07(a int, b int);
+insert into Tbl07 values(1,2),(3,4),(null,null);
+
+create table Tbl08(a int, b int);
+insert into Tbl08 values(1,2),(3,4),(null,null);
+
+create table Tbl09(a int, b int);
+insert into Tbl09 values(1,2),(5,null),(null,8);
+-- end_ignore
+
+--
+-- Positive cases: We should be inferring non-nullability of the not-in subquery. This should result in HLASJ.
+--
+
+-- non-nullability due to inner join
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05,Tbl06 where Tbl05.a=Tbl06.a and Tbl05.b < 10); -- expected: (3,4),(5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07 inner join Tbl08 on (Tbl07.a=Tbl08.a and Tbl07.b=Tbl08.b) inner join i3 on (i3.a=Tbl08.a and i3.b=Tbl08.b)); -- expected:(3,4), (5,6)
+
+-- non-nullability due to where clause condition
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05 where Tbl05.a < 2 and Tbl05.b < 10); -- expected: (3,4), (5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07 left join Tbl08 on (Tbl07.a=Tbl08.a) where Tbl07.a = 1 and Tbl07.b = 2); -- expected: (3,4),(5,6)
+
+-- not null condition in the where clause
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07 full outer join Tbl08 on (Tbl07.a=Tbl08.a) where Tbl07.a is not null and Tbl07.b is not null); -- (5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07 left join Tbl08 on (Tbl07.a=Tbl08.a) where Tbl07.a is not null and Tbl07.b is not null); -- (5,6)
+
+-- or clauses that should lead to non-nullability
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05 where (Tbl05.a < 2 or Tbl05.a > 100) AND (Tbl05.b < 4 or Tbl05.b > 100)); -- expected: (3,4), (5,6)
+
+-- base-table constraints
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a,i3.b from i3); -- expected: (3,4),(5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05,i3 where	Tbl05.a = i3.a and	Tbl05.b = i3.b);
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05,i3 where Tbl05.a < i3.a and Tbl05.b > i3.b);
+
+-- non-null constant values
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select 1,2); -- (3,4),(5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in ((1,2));
+
+-- multiple NOT-IN expressions
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl06.a,Tbl06.b from Tbl06) and (Tbl04.a,Tbl04.b) not in (select i3.a, i3.b from i3); -- expected: (5,6)
+
+select Tbl04.* from Tbl04 where not ((Tbl04.a,Tbl04.b) in (select Tbl06.a,Tbl06.b from Tbl06) or (Tbl04.a,Tbl04.b) in (select i3.a, i3.b from i3)); -- expected: (5,6)
+
+-- single column in the target list should always give a HLASJ
+
+select Tbl04.* from Tbl04 where Tbl04.a NOT IN (select Tbl09.a from Tbl09 where Tbl09.b is null); -- (1,2) (3,4)
+
+select Tbl04.* from Tbl04 where Tbl04.a NOT IN (select i3.a from i3);
+
+select Tbl04.* from Tbl04 where Tbl04.a NOT IN (select Tbl05.a from	Tbl05 left	join i3	on (Tbl05.a=i3.a));
+
+--
+-- Negative tests: we should not be inferring non-nullability in these cases. Therefore, we should see NLASJ.
+--
+
+-- No where clause
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05); -- expected: (3,4), (5,6)
+
+-- INDF in the where clause
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07,Tbl08 where Tbl07.a is not distinct from Tbl08.a and Tbl07.b is not distinct from Tbl08.b); -- no rows
+
+-- null conditions in the where clause
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07 left join Tbl08 on (Tbl07.a=Tbl08.a and Tbl07.b=Tbl08.b) where Tbl07.a is null and Tbl07.b is null); -- no rows
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07 full outer join Tbl08 on (Tbl07.a=Tbl08.a and Tbl07.b=Tbl08.b) where Tbl07.a is null and Tbl07.b is null); -- no rows
+
+-- OR clauses that should not lead to non-nullability
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl07.a,Tbl07.b from Tbl07,Tbl08 where Tbl07.a is not distinct from Tbl08.a or Tbl07.a=1); -- no rows
+
+-- values list: we don't support it yet. not worth the effort.
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (values(1,2),(3,4)); -- (3,4),(5,6)
+
+-- functions/ops in the target list of the subquery
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a+2,i3.b+2 from i3); -- expected: (5,6)
+
+-- group by does not guarantee removal of nulls. 
+
+select Tbl09.a, Tbl09.b from Tbl09;
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl09.a,Tbl09.b from Tbl09); -- expected: (3,4)
+
+select Tbl09.a, Tbl09.b from Tbl09 group by Tbl09.a, Tbl09.b;
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl09.a, Tbl09.b from Tbl09 group by Tbl09.a, Tbl09.b); -- expected: (3,4)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select sum(i3.b),i3.a from i3 group by i3.a); -- (1,2),(3,4),(5,6)
+
+-- infering not-nullability for only one of the columns
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a,Tbl05.b from i3,Tbl05 where	i3.a=Tbl05.a); -- (3,4),(5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) NOT IN (select i3.a,i3.b from Tbl07 left join i3 on (i3.a=Tbl07.a and i3.b=Tbl07.b) where i3.a > 2);
+
+--
+-- Unsupported test: These tests are meant to illustrate NOT-IN subqueries we do not support
+-- Started supporting since RIO
+--
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a,i3.b from i3 union select Tbl07.a, Tbl07.b from Tbl07); -- nulls in the inner side, should not return any rows
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a,i3.b from i3 union all select Tbl07.a, Tbl07.b from Tbl07); -- nulls in the innder side, should not return any rows
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select 1,2 union select 3,4); --(5,6)
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a,i3.b from i3) or (Tbl04.a,Tbl04.b) not in (select Tbl07.a, Tbl07.b from Tbl07);
+
+-- Cases where the planner "should have" determined not-nullabitlity
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select i3.a,i3.b from i3 left join Tbl07 on (i3.a=Tbl07.a and i3.b=Tbl07.b));
+
+select Tbl04.* from Tbl04 where (Tbl04.a,Tbl04.b) not in (select Tbl05.a,Tbl05.b from Tbl05 where (Tbl05.a IN (select i3.a from i3)) AND (Tbl05.b IN (select i3.b from i3)));
+
+-- additional queries
+
+-- start_ignore
+drop table if exists Tbl04;
+
+create table Tbl04(x int, y int);
+insert into Tbl04 values(1,2);
+insert into Tbl04 values(3,4);
+
+create table Tbl10(x int, y int);
+insert into Tbl10 values(1,null);
+-- end_ignore
+
+select * from Tbl04 where (x,y) not in (select x,y from Tbl10);
+
+select * from Tbl04 where (x,y) not in (select 1,y from Tbl10);
+
+-- start_ignore
+alter table Tbl10 alter column x set not null;
+-- end_ignore
+select * from Tbl04 where (x,y) not in (select x,y from Tbl10);
 -- start_ignore
 drop schema qp_subquery cascade;
 -- end_ignore
