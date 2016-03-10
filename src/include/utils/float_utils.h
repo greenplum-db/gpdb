5d69577383415d0199bf9dc4395a7cef7341742f
Atri Sharma <atsharma@pivotal.io>
2016-03-11 00:54:27 +0530

    Cherry pick support for Complex Number type part 1

--- src/include/utils/float_utils.h
+++ src/include/utils/float_utils.h
@@ -6,6 +6,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  * Portions Copyright (c) 2016, Pivotal Software Inc
+=======
  *
  *
  * NOTE
