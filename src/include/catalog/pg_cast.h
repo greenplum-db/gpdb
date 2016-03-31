704cc30ee98cf9d9b807c1ed1125fbfd423e6bfa
Atri Sharma <atsharma@pivotal.io>
2016-03-31 20:03:27 +0530

    Fix old cast OIDs

--- src/include/catalog/pg_cast.h
+++ src/include/catalog/pg_cast.h
@@ -448,13 +448,13 @@ DATA(insert OID = 9902 ( 3300	 27    0 e ));
 
 
 /* complex category */
-DATA(insert OID=9069 (701  193 3460 i));
-DATA(insert OID=9070 (700  193 3461 i));
-DATA(insert OID=9088 (20   193 3462 i));
-DATA(insert OID=9089 (23   193 3463 i));
-DATA(insert OID=9090 (21   193 3464 i));
+DATA(insert OID=9069 (701  193 3584 i));
+DATA(insert OID=9070 (700  193 3585 i));
+DATA(insert OID=9088 (20   193 3586 i));
+DATA(insert OID=9089 (23   193 3587 i));
+DATA(insert OID=9090 (21   193 3588 i));
 DATA(insert OID=9091 (600  193 0 i));
 DATA(insert OID=9092 (193  600 0 i));
-DATA(insert OID=9093 (1700 193 3468 i));
+DATA(insert OID=9093 (1700 193 3592 i));
 
 #endif   /* PG_CAST_H */
