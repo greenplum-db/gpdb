50c131f7f21bc3d073e3d2bd6cdcc1d7fb69ebdd
Foyzur Rahman <foyzur@gmail.com>
2016-04-27 11:08:11 -0700

    Adding size header in vmem allocations.

--- src/backend/executor/nodeShareInputScan.c
+++ src/backend/executor/nodeShareInputScan.c
@@ -578,7 +578,7 @@ static void shareinput_clean_lk_ctxt(ShareInput_Lk_Context *lk_ctxt)
 		lk_ctxt->del_done = false;
 	}
 
-	gp_free2 (lk_ctxt, sizeof(ShareInput_Lk_Context));
+	gp_free(lk_ctxt);
 }
 
 static void XCallBack_ShareInput_FIFO(XactEvent ev, void* vp)
