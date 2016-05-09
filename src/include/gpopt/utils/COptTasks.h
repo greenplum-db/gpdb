f412557d6490085a9f32af762a7403837e7ce2c3
Venkatesh (Venky) Raghavan <vraghavan@pivotal.io>
2016-05-09 11:23:05 -0700

    Feedback

--- src/include/gpopt/utils/COptTasks.h
+++ src/include/gpopt/utils/COptTasks.h
@@ -163,10 +163,6 @@ class COptTasks
 		static
 		void Execute ( void *(*pfunc) (void *), void *pfuncArg);
 
-		// task that does the translation from planned stmt to XML
-		static
-		void* PvDXLFromPlstmtTask(void *pv);
-
 		// task that does the translation from xml to dxl to pplstmt
 		static
 		void* PvPlstmtFromDXLTask(void *pv);
