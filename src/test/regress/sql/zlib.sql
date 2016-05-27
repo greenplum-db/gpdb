fb1b513cc7f0238d3c622111d91b4e9d372eca1e
Nikos Armenatzoglou <nikos.armenatzoglou@gmail.com>
2016-05-27 15:44:30 -0700

    Make sure that hashagg and hashjoin spill
    Signed-off-by: George Caragea <gcaragea@pivotal.io>

--- src/test/regress/sql/zlib.sql
+++ src/test/regress/sql/zlib.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS test_zlib_hashjoin;
 CREATE TABLE test_zlib_hashjoin (i1 int, i2 int, i3 int, i4 int, i5 int, i6 int, i7 int, i8 int) WITH (APPENDONLY=true) DISTRIBUTED BY (i1) ; 
-INSERT INTO test_zlib_hashjoin SELECT i,i,i,i,i,i,i,i FROM generate_series (0,999999) i; 
+INSERT INTO test_zlib_hashjoin SELECT i,i,i,i,i,i,i,i FROM 
+	(select generate_series(1, nsegments * 333333) as i from 
+	(select count(*) as nsegments from gp_segment_configuration where role='p' and content >= 0) foo) bar;
 
 SET gp_workfile_type_hashjoin=bfz;
 SET gp_workfile_compress_algorithm=zlib;
@@ -16,7 +18,9 @@ SELECT COUNT(t1.*) FROM test_zlib_hashjoin AS t1, test_zlib_hashjoin AS t2 WHERE
 RESET statement_mem;
 DROP TABLE IF EXISTS test_zlib_hagg; 
 CREATE TABLE test_zlib_hagg (i1 int, i2 int, i3 int, i4 int);
-INSERT INTO test_zlib_hagg SELECT i,i,i,i from generate_series(0,299999) i;
+INSERT INTO test_zlib_hagg SELECT i,i,i,i FROM 
+	(select generate_series(1, nsegments * 300000) as i from 
+	(select count(*) as nsegments from gp_segment_configuration where role='p' and content >= 0) foo) bar;
 
 SET statement_mem=2000;
 
