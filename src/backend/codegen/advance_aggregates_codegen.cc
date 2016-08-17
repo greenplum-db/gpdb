7a36f3b7224fa1336fc5d1baa5c7ae8fb940d82d
Nikos Armenatzoglou <nikos.armenatzoglou@gmail.com>
2016-08-17 13:33:29 -0700

    Clean up

--- src/backend/codegen/advance_aggregates_codegen.cc
+++ src/backend/codegen/advance_aggregates_codegen.cc
@@ -9,29 +9,17 @@
 //    Generates code for AdvanceAggregates function.
 //
 //---------------------------------------------------------------------------
-#include <assert.h>
-#include <stddef.h>
-#include <cstdint>
-#include <memory>
-#include <string>
-
 #include "codegen/advance_aggregates_codegen.h"
-#include "codegen/base_codegen.h"
-#include "codegen/codegen_wrapper.h"
 #include "codegen/op_expr_tree_generator.h"
 #include "codegen/pg_func_generator_interface.h"
-
 
 #include "codegen/utils/gp_codegen_utils.h"
 #include "codegen/utils/utility.h"
 
 extern "C" {
 #include "postgres.h"  // NOLINT(build/include)
-#include "nodes/execnodes.h"
 #include "executor/nodeAgg.h"
-#include "utils/elog.h"
 #include "utils/palloc.h"
-#include "executor/tuptable.h"
 #include "executor/executor.h"
 #include "nodes/nodes.h"
 }
@@ -72,12 +60,13 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
       codegen_utils, GetUniqueFuncName());
 
   // BasicBlock of function entry.
-  llvm::BasicBlock* llvm_entry_block = codegen_utils->CreateBasicBlock(
+  llvm::BasicBlock* entry_block = codegen_utils->CreateBasicBlock(
       "entry block", advance_aggregates_func);
-  llvm::BasicBlock* llvm_error_block = codegen_utils->CreateBasicBlock(
-      "error block", advance_aggregates_func);
-  llvm::BasicBlock* llvm_advance_transition_function_block = codegen_utils->CreateBasicBlock(
-      "advance_transition_function block", advance_aggregates_func);
+  llvm::BasicBlock* fallback_block = codegen_utils->CreateBasicBlock(
+      "fallback block", advance_aggregates_func);
+  llvm::BasicBlock* advance_transition_function_block = codegen_utils->
+      CreateBasicBlock("advance_transition_function block",
+                       advance_aggregates_func);
 
 
   // External functions
@@ -95,9 +84,11 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
   llvm::Value *llvm_tuplecontext = codegen_utils->GetConstant<MemoryContext>(
       aggstate_->tmpcontext->ecxt_per_tuple_memory);
 
-  irb->SetInsertPoint(llvm_entry_block);
+  irb->SetInsertPoint(entry_block);
 
-  codegen_utils->CreateElog(INFO, "Codegen'ed expression evaluation called!");
+#ifdef CODEGEN_DEBUG
+  codegen_utils->CreateElog(DEBUG1, "Codegen'ed advance_aggregates called!");
+#endif
 
   // Temporary variables that replace the use of slot and FunctionInfoData
   llvm::Value *llvm_arg = irb->CreateAlloca(
@@ -110,14 +101,14 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
     AggStatePerAgg peraggstate = &aggstate_->peragg[aggno];
 
     if (peraggstate->numSortCols > 0) {
-      elog(INFO, "We don't codegen DISTINCT and/or ORDER by case");
+      elog(DEBUG1, "We don't codegen DISTINCT and/or ORDER by case");
       return false;
     }
 
     Aggref *aggref = peraggstate->aggref;
     if (!aggref)
     {
-      elog (INFO, "We don't codegen non-aggref functions");
+      elog (DEBUG1, "We don't codegen non-aggref functions");
       return false;
     }
 
@@ -148,9 +139,6 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
           llvm_arg,
           llvm_argnull
       });
-
-      codegen_utils->CreateElog(INFO, "Variable = %d",
-                                irb->CreateLoad(llvm_arg));
     }
     else
     {
@@ -162,29 +150,26 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
           codegen_utils->GetConstant(peraggstate->evalproj->pi_itemIsDone),
           codegen_utils->GetConstant<ExprDoneCond *>(nullptr)
       });
-
-      codegen_utils->CreateElog(INFO, "Variable = %d", llvm_arg);
     }
 
-    // Fallback if attribute is NULL.
+    // Fall-back if attribute is NULL.
     // TODO(nikos): Support null attributes.
     irb->CreateCondBr(irb->CreateLoad(llvm_argnull),
-                      llvm_error_block /*true*/,
-                      llvm_advance_transition_function_block /*false*/);
+                      fallback_block /*true*/,
+                      advance_transition_function_block /*false*/);
 
     // advance_transition_function block
     // ----------
     // We generate code for advance_transition_function.
-    irb->SetInsertPoint(llvm_advance_transition_function_block);
+    irb->SetInsertPoint(advance_transition_function_block);
 
-    //    advance_transition_function(aggstate, peraggstate, pergroupstate,
-    //                        &fcinfo, mem_manager); {{{
+    // advance_transition_function {{{
 
-    //
     // TODO(nikos): Currently we do not support NULL attributes. However, if we
-    // support NULL attributes and transition function is
-    // strict (transfn->fn_strict), then we have to initiate transValue,
-    // noTransvalue, transValueIsNull accordingly (see invoke_agg_trans_func).
+    // support NULL attributes and transition function is strict, then we might
+    // have to initiate transValue, noTransvalue, and transValueIsNull
+    // accordingly (see invoke_agg_trans_func).
+    // In case of SUM, we do not have to support this.
 
     //oldContext = MemoryContextSwitchTo(tuplecontext);
     llvm::Value *llvm_oldContext = irb->CreateCall(llvm_MemoryContextSwitchTo,
@@ -203,19 +188,23 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
         codegen_utils->GetPointerToMember(
             llvm_pergroupstate, &AggStatePerGroupData::noTransValue);
 
-    //newVal = FunctionCallInvoke(fcinfo); {{
+    if (!peraggstate->transtypeByVal) {
+      elog(DEBUG1, "We do not support pass-by-ref datatypes.");
+      return false;
+    }
+
+    // FunctionCallInvoke(fcinfo); {{
     PGFuncGeneratorInfo pg_func_info(
         advance_aggregates_func,
-        llvm_error_block,
+        fallback_block,
         {irb->CreateLoad(llvm_pergroupstate_transValue),
             irb->CreateLoad(llvm_arg)}
     );
 
-    PGFuncGeneratorInterface* pg_func_gen = OpExprTreeGenerator::
-        GetPGFuncGenerator(peraggstate->transfn.fn_oid);
-    if (nullptr == pg_func_gen)
-    {
-      elog(INFO, "We do not support function with oid = %d",
+    PGFuncGeneratorInterface* pg_func_gen =
+        OpExprTreeGenerator::GetPGFuncGenerator(peraggstate->transfn.fn_oid);
+    if (nullptr == pg_func_gen) {
+      elog(DEBUG1, "We do not support built-in function with oid = %d",
            peraggstate->transfn.fn_oid);
       return false;
     }
@@ -230,16 +219,8 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
 
     // }}} advance_transition_function
 
-
-    if (!peraggstate->transtypeByVal) {
-      elog(DEBUG1, "We do not support pass-by-ref datatypes.");
-      return false;
-    }
-
-    // pergroupstate->transValue = newval {{{
+    // pergroupstate->transValue = newval
     irb->CreateStore(result, llvm_pergroupstate_transValue);
-    // }}}
-
 
     // Currently we do not support null attributes.
     // Thus we set transValueIsNull and noTransValue to false by default.
@@ -249,16 +230,16 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
     irb->CreateStore(codegen_utils->GetConstant<bool>(false),
                      llvm_pergroupstate_noTransValue);
 
-    codegen_utils->CreateElog(INFO, "transValue = %d",
-                              irb->CreateLoad(llvm_pergroupstate_transValue));
-
   } // End of for loop
 
   irb->CreateRetVoid();
 
-  irb->SetInsertPoint(llvm_error_block);
+  // Fall back block
+  // ---------------
+  irb->SetInsertPoint(fallback_block);
 
-  codegen_utils->CreateElog(INFO, "An error has occurred and we are falling back");
+  codegen_utils->CreateElog(DEBUG1,
+                            "Falling back to regular advance_aggregates");
 
   codegen_utils->CreateFallback<AdvanceAggregatesFn>(
       codegen_utils->GetOrRegisterExternalFunction(advance_aggregates,
@@ -269,7 +250,8 @@ bool AdvanceAggregatesCodegen::GenerateAdvanceAggregates(
 }
 
 
-bool AdvanceAggregatesCodegen::GenerateCodeInternal(GpCodegenUtils* codegen_utils) {
+bool AdvanceAggregatesCodegen::GenerateCodeInternal(
+    GpCodegenUtils* codegen_utils) {
   bool isGenerated = GenerateAdvanceAggregates(codegen_utils);
 
   if (isGenerated) {
