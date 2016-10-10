9747d3788a4bd0aab60128b04016edcb9b3ff899
Ning Wu <nwu@pivotal.io>
2016-10-10 19:02:57 +0800

    Refactor gphdfs test cases to work with pg_regress

---
+++ gpAux/extensions/gphdfs/regression/build_test_jar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+unset HADOOP_HOME
+
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
+export HADOOP_HOME=/usr/hdp/2.3.2.0-2950
+source /home/gpadmin/gpdb4391/greenplum-db/lib/hadoop/hadoop_env.sh; 
+
+cd /home/gpadmin/gpdb/gpAux/extensions/gphdfs/regression/legacy;
+javac  -cp .:$CLASSPATH:/home/gpadmin/gpdb4391/greenplum-db-4.3.9.1/lib/hadoop/gphd-2.0.2-gnet-1.2.0.0.jar  javaclasses/*.java
+jar cf maptest.jar javaclasses/*.class
