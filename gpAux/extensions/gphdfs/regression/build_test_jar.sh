4d14bfba48a5aca4f16331dedf4cb4f27b1f47f6
Ning Wu <nwu@pivotal.io>
2016-10-12 20:57:53 +0800

    don't hardcode path variables

--- gpAux/extensions/gphdfs/regression/build_test_jar.sh
+++ gpAux/extensions/gphdfs/regression/build_test_jar.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -x
 
 unset HADOOP_HOME
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
-export HADOOP_HOME=/usr/hdp/2.3.2.0-2950
-source /home/gpadmin/gpdb4391/greenplum-db/lib/hadoop/hadoop_env.sh;
 
-cd /home/gpadmin/gpdb/gpAux/extensions/gphdfs/regression/legacy;
-javac  -cp .:$CLASSPATH:/home/gpadmin/gpdb4391/greenplum-db-4.3.9.1/lib/hadoop/gphd-2.0.2-gnet-1.2.0.0.jar  javaclasses/*.java
+if [ -z "$1" ]
+then
+	export HADOOP_HOME=/usr/hdp/2.3.2.0-2950
+else
+	export HADOOP_HOME=$1
+fi
+
+source $GPHOME/lib/hadoop/hadoop_env.sh; 
+
+cd $CURDIR/legacy;
+javac -cp .:$CLASSPATH:$GPHOME/lib/hadoop/gphd-2.0.2-gnet-1.2.0.0.jar javaclasses/*.java
 jar cf maptest.jar javaclasses/*.class
