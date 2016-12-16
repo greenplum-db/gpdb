6568b080c8ce50cb7586eda73a1216b0ff585515
Nikos Armenatzoglou <nikos.armenatzoglou@gmail.com>
2016-12-16 14:50:36 -0800

    Write statement in master's log if an exception was encountered during its execution

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -4740,7 +4740,11 @@ PostgresMain(int argc, char *argv[],
 		 * Make sure debug_query_string gets reset before we possibly clobber
 		 * the storage it points at.
 		 */
-		debug_query_string = NULL;
+		if (debug_query_string != NULL)
+		{
+			write_stderr("An exception was encountered during the execution of statement: %s", debug_query_string);
+			debug_query_string = NULL;
+		}
 
 		/* No active snapshot any more either */
 		ActiveSnapshot = NULL;
