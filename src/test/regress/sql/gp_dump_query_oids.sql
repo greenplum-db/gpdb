4a1d7e63bfbd6ad1d7757a5dff5130cb307bf093
root <root@localhost.localdomain>
2017-04-01 03:24:51 -0400

    modify testcases to make it work

--- src/test/regress/sql/gp_dump_query_oids.sql
+++ src/test/regress/sql/gp_dump_query_oids.sql
@@ -14,6 +14,7 @@ SELECT gp_dump_query_oids('SELECT * FROM nonexistent_table');
 SELECT gp_dump_query_oids('SELECT with syntax error');
 
 -- Test partition tables
+CREATE TABLE minirepro_partition_test (id int, info json);
 CREATE TABLE p3_sales (id int, year int, a int, b int, c int, d int, region text)
  DISTRIBUTED BY (id)
  PARTITION BY RANGE (year)
@@ -27,5 +28,11 @@ CREATE TABLE p3_sales (id int, year int, a int, b int, c int, d int, region text
                DEFAULT SUBPARTITION other_b )
 ( START (2002) END (2012) EVERY (1),
    DEFAULT PARTITION outlying_years );
-SELECT gp_dump_query_oids('SELECT * FROM p3_sales');
-SELECT gp_dump_query_oids('SELECT * FROM p3_sales_1_prt_11_2_prt_3');
+INSERT INTO minirepro_partition_test VALUES (1, (select gp_dump_query_oids('SELECT * FROM p3_sales') ) :: json);
+INSERT INTO minirepro_partition_test VALUES (2, (select gp_dump_query_oids('SELECT * FROM p3_sales_1_prt_11_2_prt_3') ) :: json);
+INSERT INTO minirepro_partition_test VALUES (3, (select gp_dump_query_oids('SELECT * FROM pg_class') ) :: json);
+SELECT array_length((SELECT string_to_array((SELECT info->>'relids' FROM minirepro_partition_test WHERE id = 1),',')),1);
+SELECT array_length((SELECT string_to_array((SELECT info->>'relids' FROM minirepro_partition_test WHERE id = 2),',')),1);
+SELECT array_length((SELECT string_to_array((SELECT info->>'relids' FROM minirepro_partition_test WHERE id = 3),',')),1);
+DROP TABLE p3_sales;
+DROP TABLE minirepro_partition_test;
