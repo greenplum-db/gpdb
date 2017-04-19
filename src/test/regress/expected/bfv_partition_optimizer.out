cloudberrydb/cloudberrydb

commit e378d84bf32922b324a8ce6e8a66be358a0b805f

Use PartitionSelectors for partition elimination, even without ORCA.
The old mechanism was to scan the complete plan, searching for a pattern
with a Join, where the outer side included an Append node. The inner
side was duplicated into an InitPlan, with the pg_partition_oid aggregate
to collect the Oids of all the partitions that can match. That was
inefficient and broken: if the duplicated plan was volatile, you might
choose wrong partitions. And scanning the inner side twice can obviously
be slow, if there are a lot of tuples.

Rewrite the way such plans are generated. Instead of using an InitPlan,
inject a PartitionSelector node into the inner side of the join.

Fixes github issues #2100 and #2116.

src/backend/executor/nodePartitionSelector.c
@@ -19,6 +19,7 @@
 #include "executor/executor.h"
 #include "executor/instrument.h"
 #include "executor/nodePartitionSelector.h"
+#include "nodes/makefuncs.h"
 #include "utils/memutils.h"
 
 static void 
@@ -77,6 +78,25 @@ ExecInitPartitionSelector(PartitionSelector *node, EState *estate, int eflags)
 		outerPlanState(psstate) = ExecInitNode(outerPlan(node), estate, eflags);
 	}
 
+	/*
+	 * Initialize projection, to produce a tuple that has the partitioning key
+	 * columns at the same positions as in the partitioned table.
+	 */
+	if (node->partTabTargetlist)
+	{
+		List	   *exprStates;
+
+		exprStates = (List *) ExecInitExpr((Expr *) node->partTabTargetlist,
+										   (PlanState *) psstate);
+
+		psstate->partTabDesc = ExecTypeFromTL(node->partTabTargetlist, false);
+		psstate->partTabSlot = MakeSingleTupleTableSlot(psstate->partTabDesc);
+		psstate->partTabProj = ExecBuildProjectionInfo(exprStates,
+													   psstate->ps.ps_ExprContext,
+													   psstate->partTabSlot,
+													   ExecGetResultType(&psstate->ps));
+	}
+
 	initGpmonPktForPartitionSelector((Plan *)node, &psstate->ps.gpmon_pkt, estate);
 
 	return psstate;
@@ -111,6 +131,13 @@ ExecInitPartitionSelector(PartitionSelector *node, EState *estate, int eflags)
  *		It evaluates partition constraints with the input tuple and
  *		propagate matched partition table Oids.
  *
+ *
+ * Instead of a Dynamic Table Scan, there can be other nodes that use
+ * a PartSelected qual to filter rows, based on which partitions are
+ * selected. Currently, ORCA uses Dynamic Table Scans, while plans
+ * produced by the non-ORCA planner use gating Result nodes with
+ * PartSelected quals, to exclude unwanted partitions.
+ *
  * ----------------------------------------------------------------
  */
 TupleTableSlot *
@@ -118,6 +145,10 @@ ExecPartitionSelector(PartitionSelectorState *node)
 {
 	PartitionSelector *ps = (PartitionSelector *) node->ps.plan;
 	EState	   *estate = node->ps.state;
+	ExprContext *econtext = node->ps.ps_ExprContext;
+	TupleTableSlot *inputSlot;
+	TupleTableSlot *candidateOutputSlot;
+
 	if (ps->staticSelection)
 	{
 		/* propagate the part oids obtained via static partition selection */
@@ -143,7 +174,6 @@ ExecPartitionSelector(PartitionSelectorState *node)
 									);
 	}
 
-	TupleTableSlot *inputSlot = NULL;
 	if (NULL != outerPlanState(node))
 	{
 		/* Join partition elimination */
@@ -160,30 +190,62 @@ ExecPartitionSelector(PartitionSelectorState *node)
 	}
 
 	/* partition elimination with the given input tuple */
-	SelectedParts *selparts = processLevel(node, 0 /* level */, inputSlot);
+	ResetExprContext(econtext);
+	node->ps.ps_OuterTupleSlot = inputSlot;
+	econtext->ecxt_outertuple = inputSlot;
+	econtext->ecxt_scantuple = inputSlot;
 
-	/* partition propagation */
-	if (NULL != ps->propagationExpression)
+	candidateOutputSlot = ExecProject(node->ps.ps_ProjInfo, NULL);
+
+	/*
+	 * If we have a partitioning projection, project the input tuple
+	 * into a tuple that looks like tuples from the partitioned table, and use
+	 * selectPartitionMulti() to select the partitions. (The traditional
+	 * Postgres planner uses this method.)
+	 */
+	if (ps->partTabTargetlist)
 	{
-		partition_propagation(estate, selparts->partOids, selparts->scanIds, ps->selectorId);
+		TupleTableSlot *slot;
+		List	   *oids;
+		ListCell   *lc;
+
+		slot = ExecProject(node->partTabProj, NULL);
+		slot_getallattrs(slot);
+
+		oids = selectPartitionMulti(node->rootPartitionNode,
+									slot_get_values(slot),
+									slot_get_isnull(slot),
+									slot->tts_tupleDescriptor,
+									node->accessMethods);
+		if (oids != NIL)
+		{
+			foreach (lc, oids)
+			{
+				InsertPidIntoDynamicTableScanInfo(estate, ps->scanId, lfirst_oid(lc), ps->selectorId);
+			}
+		}
+		else
+		{
+			/* no partitions matched. */
+			InsertPidIntoDynamicTableScanInfo(estate, ps->scanId, InvalidOid, ps->selectorId);
+		}
 	}
-
-	list_free(selparts->partOids);
-	list_free(selparts->scanIds);
-	pfree(selparts);
-
-	TupleTableSlot *candidateOutputSlot = NULL;
-	if (NULL != inputSlot)
+	else
 	{
-		ExprContext *econtext = node->ps.ps_ExprContext;
-		ResetExprContext(econtext);
-		node->ps.ps_OuterTupleSlot = inputSlot;
-		econtext->ecxt_outertuple = inputSlot;
-		econtext->ecxt_scantuple = inputSlot;
+		/*
+		 * Select the partitions based on levelEqExpressions and
+		 * levelExpressions. (ORCA uses this method)
+		 */
+		SelectedParts *selparts = processLevel(node, 0 /* level */, inputSlot);
 
-		ExprDoneCond isDone = ExprSingleResult;
-		candidateOutputSlot = ExecProject(node->ps.ps_ProjInfo, &isDone);
-		Assert (ExprSingleResult == isDone);
+		/* partition propagation */
+		if (NULL != ps->propagationExpression)
+		{
+			partition_propagation(estate, selparts->partOids, selparts->scanIds, ps->selectorId);
+		}
+		list_free(selparts->partOids);
+		list_free(selparts->scanIds);
+		pfree(selparts);
 	}
 
 	node->acceptedLeafOid = InvalidOid;

src/test/regress/expected/bfv_partition.out
@@ -1673,7 +1673,7 @@ select * from ds_4 a1,ds_4 a2 where a1.month_id = a2.month_id and a1.month_id > 
 select count_operator('explain select * from ds_4 a1,ds_4 a2 where a1.month_id = a2.month_id and a1.month_id > E''200800'';','Partition Selector');
  count_operator 
 ----------------
-              0
+              1
 (1 row)
 
 -- CLEANUP

src/backend/cdb/cdbpartition.c
@@ -9001,6 +9001,9 @@ static List *PartitionChildren(PartitionNode *p)
  * big difference around nulls. If there is a null value corresponding to a partitioning attribute,
  * then all children are considered matches.
  *
+ * The input values/isnull should match the layout of tuples in the
+ * partitioned table.
+ *
  * Output:
  *	leafPartitionOids - list of leaf partition oids, null if there are no matches
  */

src/backend/cdb/cdbtargeteddispatch.c
@@ -516,6 +516,7 @@ AssignContentIdsToPlanData_Walker(Node *node, void *context)
 			case T_Hash:
 			case T_SetOp:
 			case T_Limit:
+			case T_PartitionSelector:
 				break;
 			case T_Motion:
 				/* receiving end knows nothing (since we don't analyze the distribution key right now) */

src/backend/executor/execQual.c
@@ -41,6 +41,7 @@
 #include "catalog/pg_proc.h"
 #include "catalog/pg_type.h"
 #include "cdb/cdbvars.h"
+#include "cdb/cdbpartition.h"
 #include "cdb/partitionselection.h"
 #include "commands/typecmds.h"
 #include "executor/execdebug.h"
@@ -195,6 +196,9 @@ static Datum ExecEvalCurrentOfExpr(ExprState *exprstate, ExprContext *econtext,
 					  bool *isNull, ExprDoneCond *isDone);
 
 static Datum ExecEvalPartOidExpr(PartOidExprState *exprstate,
+						ExprContext *econtext,
+						bool *isNull, ExprDoneCond *isDone);
+static Datum ExecEvalPartSelectedExpr(PartSelectedExprState *exprstate,
 						ExprContext *econtext,
 						bool *isNull, ExprDoneCond *isDone);
 static Datum ExecEvalPartDefaultExpr(PartDefaultExprState *exprstate,
@@ -4671,6 +4675,31 @@ static Datum ExecEvalPartOidExpr(PartOidExprState *exprstate,
 }
 
 /* ----------------------------------------------------------------
+ *		ExecEvalPartSelectedExpr
+ *
+ *		Evaluate a PartSelectedExpr
+ * ----------------------------------------------------------------
+ */
+static Datum
+ExecEvalPartSelectedExpr(PartSelectedExprState *exprstate,
+						 ExprContext *econtext,
+						 bool *isNull, ExprDoneCond *isDone)
+{
+	Assert(NULL != exprstate);
+	Assert(NULL != isNull);
+
+	PartSelectedExpr *expr = (PartSelectedExpr *) exprstate->xprstate.expr;
+
+	if (isDone)
+		*isDone = ExprSingleResult;
+
+	*isNull = false;
+	return BoolGetDatum(isPartitionSelected(econtext->ecxt_estate,
+											expr->dynamicScanId,
+											expr->partOid));
+}
+
+/* ----------------------------------------------------------------
  *		ExecEvalPartDefaultExpr
  *
  *		Evaluate a PartDefaultExpr
@@ -5934,6 +5963,14 @@ ExecInitExpr(Expr *node, PlanState *parent)
 				 * be holding the actual acceptedLeafOid.
 				 */
 				exprstate->acceptedLeafOid = &(psstate->acceptedLeafOid);
+
+				state = (ExprState *) exprstate;
+			}
+			break;
+		case T_PartSelectedExpr:
+			{
+				PartSelectedExprState *exprstate = makeNode(PartSelectedExprState);
+				exprstate->xprstate.evalfunc = (ExprStateEvalFunc) ExecEvalPartSelectedExpr;
 
 				state = (ExprState *) exprstate;
 			}

src/backend/nodes/copyfuncs.c
@@ -1224,6 +1224,7 @@ _copyPartitionSelector(const PartitionSelector *from)
 	COPY_SCALAR_FIELD(staticSelection);
 	COPY_NODE_FIELD(staticPartOids);
 	COPY_NODE_FIELD(staticScanIds);
+	COPY_NODE_FIELD(partTabTargetlist);
 
 	return newnode;
 }
@@ -2698,6 +2699,16 @@ _copyPartOidExpr(const PartOidExpr *from)
 {
 	PartOidExpr *newnode = makeNode(PartOidExpr);
 	COPY_SCALAR_FIELD(level);
+
+	return newnode;
+}
+
+static PartSelectedExpr *
+_copyPartSelectedExpr(const PartSelectedExpr *from)
+{
+	PartSelectedExpr *newnode = makeNode(PartSelectedExpr);
+	COPY_SCALAR_FIELD(dynamicScanId);
+	COPY_SCALAR_FIELD(partOid);
 
 	return newnode;
 }
@@ -5213,6 +5224,9 @@ copyObject(void *from)
 			break;
 		case T_PartOidExpr:
 			retval = _copyPartOidExpr(from);
+			break;
+		case T_PartSelectedExpr:
+			retval = _copyPartSelectedExpr(from);
 			break;
 		case T_PartDefaultExpr:
 			retval = _copyPartDefaultExpr(from);

src/backend/nodes/outfast.c
@@ -1929,6 +1929,10 @@ _outNode(StringInfo str, void *obj)
 				_outPartOidExpr(str, obj);
 				break;
 
+			case T_PartSelectedExpr:
+				_outPartSelectedExpr(str, obj);
+				break;
+
 			case T_PartDefaultExpr:
 				_outPartDefaultExpr(str, obj);
 				break;

src/backend/nodes/outfuncs.c
@@ -1103,6 +1103,7 @@ _outPartitionSelector(StringInfo str, PartitionSelector *node)
 	WRITE_BOOL_FIELD(staticSelection);
 	WRITE_NODE_FIELD(staticPartOids);
 	WRITE_NODE_FIELD(staticScanIds);
+	WRITE_NODE_FIELD(partTabTargetlist);
 
 	_outPlanInfo(str, (Plan *) node);
 }
@@ -3234,6 +3235,15 @@ _outPartOidExpr(StringInfo str, PartOidExpr *node)
 }
 
 static void
+_outPartSelectedExpr(StringInfo str, PartSelectedExpr *node)
+{
+	WRITE_NODE_TYPE("PARTSELECTEDEXPR");
+
+	WRITE_INT_FIELD(dynamicScanId);
+	WRITE_OID_FIELD(partOid);
+}
+
+static void
 _outPartDefaultExpr(StringInfo str, PartDefaultExpr *node)
 {
 	WRITE_NODE_TYPE("PARTDEFAULTEXPR");
@@ -5089,6 +5099,10 @@ _outNode(StringInfo str, void *obj)
 
 			case T_PartOidExpr:
 				_outPartOidExpr(str, obj);
+				break;
+
+			case T_PartSelectedExpr:
+				_outPartSelectedExpr(str, obj);
 				break;
 
 			case T_PartDefaultExpr:

greenplum-db/gpdb

commit 2ccd70f2df1733c746fd5d41d3ef08367c6e0965

Fix infinite execution of partition selector
This fixes a bug introduced in e378d84bf32922b324a8ce6e8a66be358a0b805f
where the partition selector may go in an infinite loop because it never
got a chance to actually _select_ the partitions.

src/backend/executor/nodePartitionSelector.c
@@ -146,8 +146,8 @@ ExecPartitionSelector(PartitionSelectorState *node)
 	PartitionSelector *ps = (PartitionSelector *) node->ps.plan;
 	EState	   *estate = node->ps.state;
 	ExprContext *econtext = node->ps.ps_ExprContext;
-	TupleTableSlot *inputSlot;
-	TupleTableSlot *candidateOutputSlot;
+	TupleTableSlot *inputSlot = NULL;
+	TupleTableSlot *candidateOutputSlot = NULL;
 
 	if (ps->staticSelection)
 	{
@@ -195,7 +195,10 @@ ExecPartitionSelector(PartitionSelectorState *node)
 	econtext->ecxt_outertuple = inputSlot;
 	econtext->ecxt_scantuple = inputSlot;
 
-	candidateOutputSlot = ExecProject(node->ps.ps_ProjInfo, NULL);
+	if (NULL != inputSlot)
+	{
+		candidateOutputSlot = ExecProject(node->ps.ps_ProjInfo, NULL);
+	}
 
 	/*
 	 * If we have a partitioning projection, project the input tuple

src/test/regress/expected/bfv_partition.out
@@ -1938,6 +1938,31 @@ explain analyze select a.* from mpp8031 a, mpp8031 b where a.oid = b.oid;
 (34 rows)
 
 drop table mpp8031;
+-- Test Query on Partition table when optimizer_static_partition_selection is OFF.
+-- start_ignore
+DROP TABLE IF EXISTS partitioned_table;
+NOTICE:  table "partitioned_table" does not exist, skipping
+-- end_ignore
+CREATE TABLE partitioned_table (a int)
+PARTITION BY RANGE(a)
+(
+   END(10),
+   END(20),
+   END(30)
+);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+NOTICE:  CREATE TABLE will create partition "partitioned_table_1_prt_1" for table "partitioned_table"
+NOTICE:  CREATE TABLE will create partition "partitioned_table_1_prt_2" for table "partitioned_table"
+NOTICE:  CREATE TABLE will create partition "partitioned_table_1_prt_3" for table "partitioned_table"
+INSERT INTO partitioned_table VALUES(11);
+SET optimizer_static_partition_selection = off;
+SELECT * FROM partitioned_table;
+ a
+----
+ 11
+(1 row)
+
 -- CLEANUP
 -- start_ignore
 drop schema if exists bfv_partition;

src/test/regress/expected/bfv_partition_optimizer.out
@@ -1938,6 +1938,31 @@ explain analyze select a.* from mpp8031 a, mpp8031 b where a.oid = b.oid;
 (31 rows)
 
 drop table mpp8031;
+-- Test Query on Partition table when optimizer_static_partition_selection is OFF.
+-- start_ignore
+DROP TABLE IF EXISTS partitioned_table;
+NOTICE:  table "partitioned_table" does not exist, skipping
+-- end_ignore
+CREATE TABLE partitioned_table (a int)
+PARTITION BY RANGE(a)
+(
+   END(10),
+   END(20),
+   END(30)
+);
+NOTICE:  Table doesn't have 'DISTRIBUTED BY' clause -- Using column named 'a' as the Greenplum Database data distribution key for this table.
+HINT:  The 'DISTRIBUTED BY' clause determines the distribution of data. Make sure column(s) chosen are the optimal data distribution key to minimize skew.
+NOTICE:  CREATE TABLE will create partition "partitioned_table_1_prt_1" for table "partitioned_table"
+NOTICE:  CREATE TABLE will create partition "partitioned_table_1_prt_2" for table "partitioned_table"
+NOTICE:  CREATE TABLE will create partition "partitioned_table_1_prt_3" for table "partitioned_table"
+INSERT INTO partitioned_table VALUES(11);
+SET optimizer_static_partition_selection = off;
+SELECT * FROM partitioned_table;
+ a
+----
+ 11
+(1 row)
+
 -- CLEANUP
 -- start_ignore
 drop schema if exists bfv_partition;

src/test/regress/sql/bfv_partition.sql
@@ -794,6 +794,25 @@ EVERY ('2 mons'::interval)
 explain analyze select a.* from mpp8031 a, mpp8031 b where a.oid = b.oid;
 drop table mpp8031;
 
+-- Test Query on Partition table when optimizer_static_partition_selection is OFF.
+-- start_ignore
+DROP TABLE IF EXISTS partitioned_table;
+-- end_ignore
+
+CREATE TABLE partitioned_table (a int)
+PARTITION BY RANGE(a)
+(
+   END(10),
+   END(20),
+   END(30)
+);
+
+INSERT INTO partitioned_table VALUES(11);
+
+SET optimizer_static_partition_selection = off;
+
+SELECT * FROM partitioned_table;
+
 -- CLEANUP
 -- start_ignore
 drop schema if exists bfv_partition;