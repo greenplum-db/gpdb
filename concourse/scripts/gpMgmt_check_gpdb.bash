b5f2653b9511d24b649caa73fca3f3cb07d2c015
Chris Hajas <chajas@pivotal.io>
2017-05-22 14:14:06 -0700

    Fix MU_check concourse task to print failing unit test on error.

--- concourse/scripts/gpMgmt_check_gpdb.bash
+++ concourse/scripts/gpMgmt_check_gpdb.bash
@@ -7,27 +7,20 @@ source "${CWDIR}/common.bash"
 
 function gen_env(){
   cat > /opt/run_test.sh <<-EOF
+
+		RESULT_FILE="\${1}/gpdb_src/gpMgmt/gpMgmt_testunit_results.log"
 		trap look4results ERR
 
 		function look4results() {
 
-		    results_files="../gpMgmt/gpMgmt_testunit_results.log"
+		echo "======================================================================"
+		echo "RESULT FILE: \${RESULT_FILE}"
+		echo "======================================================================"
 
-		    for results_file in \${results_files}; do
-			if [ -f "\${results_file}" ]; then
-			    cat <<-FEOF
-
-						======================================================================
-						RESULTS FILE: \${results_file}
-						----------------------------------------------------------------------
-
-						\$(cat "\${results_file}")
-
-					FEOF
-			fi
-		    done
-		    exit 1
+		cat "\${RESULT_FILE}"
+		exit 1
 		}
+
 		base_path=\${1}
 		source /usr/local/greenplum-db-devel/greenplum_path.sh
 		source /opt/gcc_env.sh
