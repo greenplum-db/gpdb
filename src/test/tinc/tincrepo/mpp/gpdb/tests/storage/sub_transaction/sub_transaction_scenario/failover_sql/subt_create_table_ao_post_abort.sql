32c27c607f803802ea6baa902397507cea4b91c1
Xin Zhang <xzhang@pivotal.io>
2017-05-23 17:37:32 -0700

    Delete sub_transaction TINC test.
    The failover scenarios with and without subtransactions are equivalent and
    covered elsewhere, e.g. in filerep-end-to-end suite.
    
    Signed-off-by: Asim R P <apraveen@pivotal.io>

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/sub_transaction/sub_transaction_scenario/failover_sql/subt_create_table_ao_post_abort.sql
+++
@@ -1,5 +0,0 @@
-Create table subt_tab_distby_ao10 (i int, x text,c char,v varchar, d date, n numeric, t timestamp without time zone, tz time with time zone) with (appendonly=true) distributed by (i);
-Create table subt_tab_distby_ao66 (i int, x text,c char,v varchar, d date, n numeric, t timestamp without time zone, tz time with time zone) with (appendonly=true) distributed by (i);
-Create table subt_ctas_tab_ao as select * from subt_for_ctas_ao;
-
-
