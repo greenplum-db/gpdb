3b25e1703421a4bc4f89dbef237336519a5b5114
Gao Zengqi <pgf00a@gmail.com>
2017-06-20 02:22:11 +0000

    Dummy commit to trigger the concourse-ci.

--- src/test/regress/sql/partition_pruning.sql
+++ src/test/regress/sql/partition_pruning.sql
@@ -792,7 +792,7 @@ EXCHANGE PARTITION FOR ('usa') WITH TABLE sales_exchange_part ;
 select get_selected_parts('explain analyze select * from sales where region = ''usa'' or region = ''asia'';');
 select * from sales where region = 'usa' or region = 'asia';
 
--- Test DynamicIndexScan with dropped column
+-- Test DynamicIndexScan with extra filter
 create index idx_sales_date on sales(date);
 explain select * from sales where date = '2011-01-01' and region = 'usa';
 select * from sales where date = '2011-01-01' and region = 'usa';
