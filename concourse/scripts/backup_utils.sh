07f7212bc5500df7c1cad2ee9cb8c4bf37711f4a
Chris Hajas <chajas@pivotal.io>
2017-07-20 12:16:54 -0700

    Address pull request comments
    Signed-off-by: Jamie McAtamney <jmcatamney@pivotal.io>

--- concourse/scripts/backup_utils.sh
+++ concourse/scripts/backup_utils.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 copy_backup_files() {
-    for node in mdw sdw1 ; do
-      ssh $node "bash -c \"\
-          cd /data/gpdata
-          find . -type d -name db_dumps > /tmp/db_dumps.files
-          tar cf /tmp/db_dumps.tar -T /tmp/db_dumps.files
-      \""
-    done
+    gpssh -h cluster_env_files/hostfile_all "bash -c \"\
+        cd /data/gpdata
+        find . -type d -name db_dumps > /tmp/db_dumps.files
+        tar cf /tmp/db_dumps.tar -T /tmp/db_dumps.files
+    \""
 }
 
 restore_backup_files() {
-    for node in mdw sdw1 ; do
-      ssh $node "bash -c \"\
-          tar xf /tmp/db_dumps.tar -C /data/gpdata
-      \""
-    done
+    gpssh -h cluster_env_files/hostfile_all "bash -c \"\
+        tar xf /tmp/db_dumps.tar -C /data/gpdata
+    \""
 }
 
 destroy_gpdb() {
--- concourse/scripts/run_behave_test.sh
+++ concourse/scripts/run_behave_test.sh
@@ -3,20 +3,24 @@
 BEHAVE_FLAGS=$@
 
 run_behave_tests() {
-    local gpdb_host=$1
+    local gpdb_master_host=$1
 
     yum install -y openssh-clients
-    ssh -t $gpdb_host "bash -c \"\
-        # Setup environment
-        source /usr/local/greenplum-db-devel/greenplum_path.sh; \
-        export PGDATABASE=gptest; \
-        export PGPORT=5432; \
-        export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
-        createdb ${PGDATABASE}; \
-        # Run behave tests
-        cd /home/gpadmin/gpdb_src/gpMgmt/; \
-        make -f Makefile.behave behave flags='${BEHAVE_FLAGS}'; \
-    \""
+    remote_script=/tmp/run_behave_test.sh
+    script=`mktemp`
+    cat <<HERE > $script
+source /usr/local/greenplum-db-devel/greenplum_path.sh
+export PGPORT=5432
+export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
+cd /home/gpadmin/gpdb_src/gpMgmt
+make -f Makefile.behave behave flags='${BEHAVE_FLAGS}'
+
+HERE
+
+scp $script $gpdb_master_host:$remote_script
+
+ssh -t $gpdb_master_host "bash $remote_script"
+
 }
 
 # Look at ~/.ssh/config for available hosts
