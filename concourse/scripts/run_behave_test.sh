39274d9040914e7ab010abf91c5f87bd4a068a4a
Chris Hajas <chajas@pivotal.io>
2017-07-21 16:15:53 -0700

    Fix quoting of behave flags
    Signed-off-by: Jamie McAtamney <jmcatamney@pivotal.io>

--- concourse/scripts/run_behave_test.sh
+++ concourse/scripts/run_behave_test.sh
@@ -6,5 +6,4 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh
 export PGPORT=5432
 export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1
 cd /home/gpadmin/gpdb_src/gpMgmt
-make -f Makefile.behave behave flags='${BEHAVE_FLAGS}'
-
+make -f Makefile.behave behave flags="$BEHAVE_FLAGS"
