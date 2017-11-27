34fe84be4c599c7e2b15cb54919d05f9d2cd95ec
Asim R P <apraveen@pivotal.io>
2017-11-27 10:53:24 -0800

    WIP: PANIC instead of ERROR upon assertion failure, so as to get a core

--- src/backend/replication/syncrep.c
+++ src/backend/replication/syncrep.c
@@ -345,7 +345,7 @@ SyncRepWaitForLSN(XLogRecPtr XactCommitLSN)
 	 */
 	if (!SHMQueueIsDetached(&(MyProc->syncRepLinks)))
 	{
-		elog(LOG,
+		elog(PANIC,
 			 "WalSndCtl->lsn[SYNC_REP_WAIT_WRITE]=%d/%x, "
 			 "lsn[SYNC_REP_WAIT_FLUSH]=%d/%x "
 			 "MyProc->syncRepState=%d, MyProc->waitLSN=%d/%x "
@@ -354,20 +354,6 @@ SyncRepWaitForLSN(XLogRecPtr XactCommitLSN)
 			 WalSndCtl->lsn[SYNC_REP_WAIT_FLUSH].xlogid, WalSndCtl->lsn[SYNC_REP_WAIT_FLUSH].xrecoff,
 			 MyProc->syncRepState, MyProc->waitLSN.xlogid, MyProc->waitLSN.xrecoff,
 			 MyProc->syncRepLinks.prev, MyProc->syncRepLinks.next);
-		PGPROC *proc;
-		proc = (PGPROC *) SHMQueueNext(&(WalSndCtl->SyncRepQueue[mode]),
-									   &(WalSndCtl->SyncRepQueue[mode]),
-									   offsetof(PGPROC, syncRepLinks));
-		while (proc)
-		{
-			ereport(LOG, (errmsg("pid=%d, syncRepState=%d, waitLSN=%d/%x",
-								 proc->pid, proc->syncRepState, proc->waitLSN.xlogid,
-								 proc->waitLSN.xrecoff), errprintstack(true)));
-			proc = (PGPROC *) SHMQueueNext(&(WalSndCtl->SyncRepQueue[mode]),
-										   &proc->syncRepLinks,
-										   offsetof(PGPROC, syncRepLinks));
-		}
-		elog(ERROR, "Assertion failed: SHMQueueIsDetached(&(MyProc->syncRepLinks))");
 	}
 	MyProc->syncRepState = SYNC_REP_NOT_WAITING;
 	MyProc->waitLSN.xlogid = 0;
