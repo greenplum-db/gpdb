814bf4741588425eea46bdb0aadeaa4f56b9b4e3
Ivan Leskin <leskin.in@arenadata.io>
2017-11-27 15:45:15 +0300

    pg_compression: Zstandard added
    This commit adds Zstandard (https://github.com/facebook/zstd) compression algorithm to GPDB.
    A dynamically linked zstd library is used.
    
    The compression type name (as defined in 'src/backend/catalog/pg_compression.c') is 'zstd'.
    
    Functions interacting with the 'zstd' library are located in 'src/backend/catalog/zstd_compression.c'.
    
    ZSTD_compressCCtx and ZSTD_compressDCtx are created
    when a 'callCompressionConstructor()' is called (in 'src/backend/catalog/pg_compression.c').
    
    ZSTD_compressCCtx and ZSTD_compressDCtx are destroyed
    when a 'callCompressionDestructor()' is called (in 'src/backend/catalog/pg_compression.c').

---
+++ src/backend/catalog/zstd_compression.c
@@ -0,0 +1,167 @@
+/*---------------------------------------------------------------------
+ *
+ * zstd_compression.c
+ *
+ * IDENTIFICATION
+ *	    src/backend/catalog/zstd_compression.c
+ *
+ *---------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+#include "fmgr.h"
+
+#include "access/genam.h"
+#include "access/reloptions.h"
+#include "access/tupdesc.h"
+#include "access/tupmacs.h"
+#include "catalog/pg_attribute_encoding.h"
+#include "catalog/pg_compression.h"
+#include "catalog/dependency.h"
+#include "storage/gp_compress.h"
+#include "utils/builtins.h"
+#include "utils/fmgroids.h"
+#include "utils/formatting.h"
+#include "utils/guc.h"
+#include "utils/lsyscache.h"
+#include "utils/rel.h"
+#include "utils/relcache.h"
+#include "utils/syscache.h"
+#include "utils/faultinjector.h"
+
+#include <zstd.h>
+
+
+/* Internal state for zstd */
+typedef struct zstd_state
+{
+	int level;							// Compression level
+
+	bool compress;						// Compress if true, decompress otherwise
+
+	ZSTD_CCtx* zstd_compress_context;	// ZSTD compression context
+
+	ZSTD_DCtx* zstd_decompress_context;	// ZSTD decompression context
+
+	size_t (*compress_fn) (ZSTD_CCtx *cctx,
+						   void *dst,
+						   size_t dstCapacity,
+						   const void* src,
+						   size_t srcSize,
+						   int level);
+
+	size_t (*decompress_fn) (ZSTD_DCtx *dctx,
+							 void* dst,
+							 size_t dstCapacity,
+							 const void* src,
+							 size_t srcSize);
+
+} zstd_state;
+
+Datum
+zstd_constructor(PG_FUNCTION_ARGS)
+{
+	/* PG_GETARG_POINTER(0) is TupleDesc that is currently unused. */
+
+	StorageAttributes *sa = (StorageAttributes *) PG_GETARG_POINTER(1);
+	CompressionState *cs = palloc0(sizeof(CompressionState));
+	zstd_state *state = palloc0(sizeof(zstd_state));
+	bool compress = PG_GETARG_BOOL(2);
+
+	cs->opaque = (void *) state;
+	cs->desired_sz = NULL;
+
+	Insist(PointerIsValid(sa->comptype));
+
+	if (sa->complevel == 0)
+		sa->complevel = 1;
+
+	state->level = sa->complevel;
+	state->compress = compress;
+	state->compress_fn = ZSTD_compressCCtx;
+	state->decompress_fn = ZSTD_decompressDCtx;
+	state->zstd_compress_context = ZSTD_createCCtx();
+	state->zstd_decompress_context = ZSTD_createDCtx();
+
+	PG_RETURN_POINTER(cs);
+}
+
+Datum
+zstd_destructor(PG_FUNCTION_ARGS)
+{
+	CompressionState *cs = (CompressionState *) PG_GETARG_POINTER(0);
+	zstd_state *state = (zstd_state *) cs->opaque;
+
+	if (cs != NULL && cs->opaque != NULL)
+ 	{
+		ZSTD_freeCCtx(state->zstd_compress_context);
+		ZSTD_freeDCtx(state->zstd_decompress_context);
+		pfree(cs->opaque);
+	}
+
+	PG_RETURN_VOID();
+}
+
+Datum
+zstd_compress(PG_FUNCTION_ARGS)
+{
+    // FIXME: Change types to ZSTD::size_t
+	const void *src = PG_GETARG_POINTER(0);
+	int32 src_sz = PG_GETARG_INT32(1);
+	void *dst = PG_GETARG_POINTER(2);
+	int32 dst_sz = PG_GETARG_INT32(3);
+	int32 *dst_used = (int32 *) PG_GETARG_POINTER(4);
+	CompressionState *cs = (CompressionState *) PG_GETARG_POINTER(5);
+	zstd_state *state = (zstd_state *) cs->opaque;
+
+	unsigned long dst_length_used;
+
+	dst_length_used = state->compress_fn(state->zstd_compress_context,
+										 dst, dst_sz, 
+                                         src, src_sz,
+                                         state->level);
+
+	if (ZSTD_isError(dst_length_used)) {
+        elog(ERROR, "%s", ZSTD_getErrorName(dst_length_used));
+    }
+
+    *dst_used = (int32) dst_length_used;
+
+	PG_RETURN_VOID();
+}
+
+Datum
+zstd_decompress(PG_FUNCTION_ARGS)
+{
+    // FIXME: Change types to ZSTD::size_t
+	const void *src = PG_GETARG_POINTER(0);
+	int32 src_sz = PG_GETARG_INT32(1);
+	void *dst = PG_GETARG_POINTER(2);
+	int32 dst_sz = PG_GETARG_INT32(3);
+	int32 *dst_used = (int32 *) PG_GETARG_POINTER(4);
+	CompressionState *cs = (CompressionState *) PG_GETARG_POINTER(5);
+	zstd_state *state = (zstd_state *) cs->opaque;
+
+	unsigned long dst_length_used;
+
+	Insist(src_sz > 0 && dst_sz > 0);
+
+	dst_length_used = state->decompress_fn(state->zstd_decompress_context,
+										   dst, dst_sz,
+									       src, src_sz);
+
+	
+    if (ZSTD_isError(dst_length_used)) {
+        elog(ERROR, "%s", ZSTD_getErrorName(dst_length_used));
+    }
+
+    *dst_used = (int32) dst_length_used;
+
+	PG_RETURN_VOID();
+}
+
+Datum
+zstd_validator(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_VOID();
+}
