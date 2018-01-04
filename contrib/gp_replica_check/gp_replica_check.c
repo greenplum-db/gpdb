b2b80de85fbbdd07271881847e72e6af3c0c59e6
Daniel Gustafsson <dgustafsson@pivotal.io>
2018-01-04 13:50:47 +0100

    Close files on retries before returning from function
    There were windows where we could return with open filehandles,
    ensure that we always close up and free up resources on retries
    before exiting.

--- contrib/gp_replica_check/gp_replica_check.c
+++ contrib/gp_replica_check/gp_replica_check.c
@@ -261,6 +261,18 @@ compare_files(char *primaryfilepath, char *mirrorfilepath, RelfilenodeEntry *ren
 retry:
 	CHECK_FOR_INTERRUPTS();
 
+	/* If the files were still open from previous attempt, close them first. */
+	if (primaryFile != -1)
+	{
+		FileClose(primaryFile);
+		primaryFile = -1;
+	}
+	if (mirrorFile != -1)
+	{
+		FileClose(mirrorFile);
+		mirrorFile = -1;
+	}
+
 	if (attempts == NUM_RETRIES)
 	{
 		ereport(WARNING,
@@ -280,18 +292,6 @@ retry:
 		 */
 		if (!sync_wait())
 			return false;
-	}
-
-	/* If the files were still open from previous attempt, close them first. */
-	if (primaryFile != -1)
-	{
-		FileClose(primaryFile);
-		primaryFile = -1;
-	}
-	if (mirrorFile != -1)
-	{
-		FileClose(mirrorFile);
-		mirrorFile = -1;
 	}
 
 	/*
