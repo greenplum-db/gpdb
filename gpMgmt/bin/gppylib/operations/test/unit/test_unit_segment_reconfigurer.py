a3189451f1a9b103ebe0ee205cd2e1088a7d6d61
David Kimura <dkimura@pivotal.io>
2018-05-10 17:14:13 -0700

    patch dbconn.connect in setUp

--- gpMgmt/bin/gppylib/operations/test/unit/test_unit_segment_reconfigurer.py
+++ gpMgmt/bin/gppylib/operations/test/unit/test_unit_segment_reconfigurer.py
@@ -7,6 +7,7 @@ from gppylib.test.unit.gp_unittest import GpTestCase
 from pygresql import pgdb
 import mock
 from mock import Mock, patch, call, MagicMock
+import contextlib
 
 
 class SegmentReconfiguerTestCase(GpTestCase):
@@ -16,27 +17,28 @@ class SegmentReconfiguerTestCase(GpTestCase):
         self.worker_pool = Mock()
         self.db_url = 'dbUrl'
 
-        self.db_url_mock = MagicMock(return_value=self.db_url)
-        cm = patch('gppylib.db.dbconn.DbURL', new=(MagicMock(return_value=self.db_url)))
+        self.connect = MagicMock()
+        cm = contextlib.nested(
+                patch('gppylib.db.dbconn.connect', new=self.connect),
+                patch('gppylib.db.dbconn.DbURL', return_value=self.db_url),
+                )
         cm.__enter__()
         self.cm = cm
 
     def tearDown(self):
-        self.cm.__exit__()
+        self.cm.__exit__(None, None, None)
 
-    @patch('gppylib.db.dbconn.connect')
-    def test_it_retries_the_connection(self, connect):
-        connect.configure_mock(side_effect=[pgdb.DatabaseError, pgdb.DatabaseError, self.conn])
+    def test_it_retries_the_connection(self):
+        self.connect.configure_mock(side_effect=[pgdb.DatabaseError, pgdb.DatabaseError, self.conn])
 
         reconfigurer = SegmentReconfigurer(self.logger, self.worker_pool)
         reconfigurer.reconfigure()
 
-        connect.assert_has_calls([call(self.db_url), call(self.db_url), call(self.db_url), ])
+        self.connect.assert_has_calls([call(self.db_url), call(self.db_url), call(self.db_url), ])
         self.conn.close.assert_any_call()
 
     @patch('time.time')
-    @patch('gppylib.db.dbconn.connect')
-    def test_it_gives_up_after_30_seconds(self, connect, now_mock):
+    def test_it_gives_up_after_30_seconds(self, now_mock):
         start_datetime = datetime.datetime(2018, 5, 9, 16, 0, 0)
         start_time = time.mktime(start_datetime.timetuple())
         now_mock.configure_mock(return_value=start_time)
@@ -49,11 +51,11 @@ class SegmentReconfiguerTestCase(GpTestCase):
                 yield pgdb.DatabaseError
 
 
-        connect.configure_mock(side_effect=fail_for_half_a_minute())
+        self.connect.configure_mock(side_effect=fail_for_half_a_minute())
 
         reconfigurer = SegmentReconfigurer(self.logger, self.worker_pool)
         with self.assertRaises(pgdb.DatabaseError):
             reconfigurer.reconfigure()
 
-        connect.assert_has_calls([call(self.db_url), call(self.db_url), ])
+        self.connect.assert_has_calls([call(self.db_url), call(self.db_url), ])
         self.conn.close.assert_has_calls([])
