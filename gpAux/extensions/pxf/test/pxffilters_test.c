b440d1275c6a0ce243657a8c72f9ccddd054f549
Shivram Mani <smani@pivotal.io>
2018-08-23 14:29:36 -0700

    Added unittests to test filterstring with pxf predicate pushdown

--- gpAux/extensions/pxf/test/pxffilters_test.c
+++ gpAux/extensions/pxf/test/pxffilters_test.c
@@ -304,6 +304,34 @@ test__list_const_to_str__int(void **state) {
 	verify__list_const_to_str(INT4ARRAYOID, "s2d11s2d22", 2, dats32);
 }
 
+
+void
+test__list_const_to_str__boolean(void **state)
+{
+    Datum dats1[2] = {BoolGetDatum(true), BoolGetDatum(false)};
+    verify__list_const_to_str(BOOLARRAYOID, "s4dtrues5dfalse", 2, dats1);
+
+    Datum dats2[2] = {BoolGetDatum(false), BoolGetDatum(true)};
+    verify__list_const_to_str(BOOLARRAYOID, "s5dfalses4dtrue", 2, dats2);
+
+    Datum dats3[1] = {BoolGetDatum(true)};
+    verify__list_const_to_str(BOOLARRAYOID, "s4dtrue", 1, dats3);
+
+    Datum dats4[1] = {BoolGetDatum(false)};
+    verify__list_const_to_str(BOOLARRAYOID, "s5dfalse", 1, dats4);
+}
+
+void
+test__list_const_to_str__text(void **state)
+{
+
+    Datum dats1[2] = {CStringGetDatum("row1"), CStringGetDatum("row2")};
+    verify__list_const_to_str(TEXTARRAYOID, "s4drow1s4drow2", 2, dats1);
+
+    Datum dats2[3] = {CStringGetDatum("r,o,w,1"), CStringGetDatum("r'o'w2"), CStringGetDatum("r\"o\"w3")};
+    verify__list_const_to_str(TEXTARRAYOID, "s7dr,o,w,1s6dr'o'w2s6dr\"o\"w3", 3, dats2);
+}
+
 void run__scalar_const_to_str(Const* input, StringInfo result, char* expected)
 {
 	scalar_const_to_str(input, result);
@@ -412,6 +440,13 @@ test__scalar_const_to_str__text(void **state)
 	verify__scalar_const_to_str(false, "funny", CHAROID, "funny");
 	verify__scalar_const_to_str(false, "anymore", BYTEAOID, "anymore");
 	verify__scalar_const_to_str(false, "iamdate", DATEOID, "iamdate");
+}
+
+void
+test__scalar_const_to_str__boolean(void **state)
+{
+    verify__scalar_const_to_str(false, "t", BOOLOID, "true");
+    verify__scalar_const_to_str(false, "f", BOOLOID, "false");
 }
 
 void
@@ -771,6 +806,77 @@ test__opexpr_to_pxffilter__unsupportedOpNot(void **state)
 	pfree(expr);
 }
 
+void test__pxf_serialize_filter_list__oneFilter(void **state)
+{
+    List* expressionItems = NIL;
+
+    ExpressionItem* filterExpressionItem = build_expression_item(1, TEXTOID, "1984", TEXTOID, TextEqualOperator);
+
+    expressionItems = lappend(expressionItems, filterExpressionItem);
+
+    char* result = pxf_serialize_filter_list(expressionItems);
+    assert_string_equal(result, "a0c25s4d1984o5");
+
+    pxf_free_expression_items_list(expressionItems, true);
+    expressionItems = NIL;
+    pfree(result);
+
+}
+
+void test__pxf_serialize_fillter_list__nullFilter(void **state)
+{
+
+    List* expressionItems = NIL;
+
+    ExpressionItem* filterExpressionItem = build_null_expression_item(1, TEXTOID, IS_NULL);
+
+    expressionItems = lappend(expressionItems, filterExpressionItem);
+
+    char* result = pxf_serialize_filter_list(expressionItems);
+    assert_string_equal(result, "a0o8");
+
+    pxf_free_expression_items_list(expressionItems, true);
+    expressionItems = NIL;
+    pfree(result);
+
+}
+
+void
+test__pxf_serialize_filter_list__manyFilters(void **state)
+{
+    char* result = NULL;
+    List* expressionItems = NIL;
+
+    ExpressionItem* expressionItem1 = build_expression_item(1, TEXTOID, "1984", TEXTOID, TextEqualOperator);
+    ExpressionItem* expressionItem2 = build_expression_item(2, TEXTOID, "George Orwell", TEXTOID, TextEqualOperator);
+    ExpressionItem* expressionItem3 = build_expression_item(3, TEXTOID, "Winston", TEXTOID, TextEqualOperator);
+    ExpressionItem* expressionItem4 = build_expression_item(4, TEXTOID, "Eric-%", TEXTOID, 1209);
+    ExpressionItem* expressionItem5 = build_expression_item(5, TEXTOID, "\"Ugly\" string with quotes", TEXTOID, TextEqualOperator);
+    ExpressionItem* expressionItem6 = build_expression_item(6, TEXTOID, "", TEXTOID, TextEqualOperator);
+    ExpressionItem* expressionItem7 = build_null_expression_item(7, TEXTOID, IS_NOT_NULL);
+
+    expressionItems = lappend(expressionItems, expressionItem1);
+    expressionItems = lappend(expressionItems, expressionItem2);
+    expressionItems = lappend(expressionItems, expressionItem3);
+    expressionItems = lappend(expressionItems, expressionItem4);
+    expressionItems = lappend(expressionItems, expressionItem5);
+
+    expressionItems = lappend(expressionItems, expressionItem6);
+    expressionItems = lappend(expressionItems, expressionItem7);
+
+    result = pxf_serialize_filter_list(expressionItems);
+    assert_string_equal(result, "a0c25s4d1984o5a1c25s13dGeorge Orwello5a2c25s7dWinstono5a3c25s6dEric-%o7a4c25s25d\"Ugly\" string with quoteso5a5c25s0do5a6o9");
+    pfree(result);
+
+    int trivialExpressionItems = expressionItems->length;
+    enrich_trivial_expression(expressionItems);
+
+    assert_int_equal(expressionItems->length, 2*trivialExpressionItems - 1);
+
+    pxf_free_expression_items_list(expressionItems, true);
+    expressionItems = NIL;
+}
+
 void
 test__extractPxfAttributes_empty_quals(void **state)
 {
@@ -839,8 +945,11 @@ main(int argc, char* argv[])
 			unit_test(test__scalar_const_to_str__null),
 			unit_test(test__scalar_const_to_str__int),
 			unit_test(test__scalar_const_to_str__text),
+            unit_test(test__scalar_const_to_str__boolean),
 			unit_test(test__scalar_const_to_str__NegativeCircle),
 			unit_test(test__list_const_to_str__int),
+            unit_test(test__list_const_to_str__boolean),
+            unit_test(test__list_const_to_str__text),
 			unit_test(test__opexpr_to_pxffilter__null),
 			unit_test(test__opexpr_to_pxffilter__unary_expr),
 			unit_test(test__opexpr_to_pxffilter__intGT),
@@ -851,6 +960,9 @@ main(int argc, char* argv[])
 			unit_test(test__opexpr_to_pxffilter__twoVars),
 			unit_test(test__opexpr_to_pxffilter__unsupportedOpNot),
 			unit_test(test__opexpr_to_pxffilter__attributeIsNull),
+            unit_test(test__pxf_serialize_filter_list__oneFilter),
+            unit_test(test__pxf_serialize_fillter_list__nullFilter),
+            unit_test(test__pxf_serialize_filter_list__manyFilters),
 			unit_test(test__extractPxfAttributes_empty_quals),
 			unit_test(test__extractPxfAttributes_supported_function_one_arg)
 	};
