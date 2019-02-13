5121ed6830e18e10feefa17245ce1f47f992bf31
Daniel Gustafsson <dgustafsson@pivotal.io>
2019-02-13 23:53:29 +0100

    Backfill contrib/ with missing file and make it test
    Our contrib/ directory was quite out of sync with upstream, with lots
    of files missing and the Makefile targetlist far from complete. This
    brings in all the missing upstream files, and massages the expected
    output formats to enable "make installcheck" to pass for most of the
    modules that have tests.
    
    The cube and earthdistance modules are disabled in the Makefile due
    to Greenplum having CUBE as a keyword; postgres_fdw and tsearch2 are
    disabled due to test failures which are beyond the scope of this PR
    to fix.

--- contrib/btree_gist/sql/not_equal.sql
+++ contrib/btree_gist/sql/not_equal.sql
@@ -22,6 +22,7 @@ SELECT * FROM test_ne WHERE a <> '2009-01-01' AND b <> 10.7;
 
 RESET enable_indexscan;
 
+-- start_ignore
 -- test search for "not equals" using an exclusion constraint
 
 CREATE TABLE zoo (
@@ -34,3 +35,4 @@ INSERT INTO zoo VALUES(123, 'zebra');
 INSERT INTO zoo VALUES(123, 'zebra');
 INSERT INTO zoo VALUES(123, 'lion');
 INSERT INTO zoo VALUES(124, 'lion');
+-- end_ignore
