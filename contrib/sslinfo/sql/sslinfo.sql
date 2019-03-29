477eada1c9abe0379cc9d32f3fb3c052d9fa94a2
Hao Wu <hawu@pivotal.io>
2019-03-29 07:47:22 +0000

    Add configuration of SSL to master and standby node
    Use `echo` + `sed` to add/remove options in postgresql.conf of
    master node and standby node. This method could completely restore
    the SSL related options in postgresql.conf. The imperfect point is
    this way may overwrite the existing certificates and keys under data directory.

--- contrib/sslinfo/sql/sslinfo.sql
+++ contrib/sslinfo/sql/sslinfo.sql
@@ -1,29 +1,8 @@
-\! echo "Enable SSL in postgresql.conf with master only..."
-\! echo "#BEGIN SSLINFO CONF" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-\! echo "ssl=on" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-\! echo "ssl_ciphers='HIGH:MEDIUM:+3DES:!aNULL'" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-\! echo "ssl_cert_file='server.crt'" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-\! echo "ssl_key_file='server.key'" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-\! echo "ssl_ca_file='root.crt'" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-\! echo "#END SSLINFO CONF" >> $MASTER_DATA_DIRECTORY/postgresql.conf
-
-\! echo "preparing CRTs and KEYs"
-\! cp -f data/root.crt   $MASTER_DATA_DIRECTORY/
-\! cp -f data/server.crt $MASTER_DATA_DIRECTORY/
-\! cp -f data/server.key $MASTER_DATA_DIRECTORY/
-\! chmod 400 $MASTER_DATA_DIRECTORY/server.key
-\! chmod 644 $MASTER_DATA_DIRECTORY/server.crt
-\! chmod 644 $MASTER_DATA_DIRECTORY/root.crt
-\! mkdir -p ~/.postgresql
-\! cp -f data/root.crt         ~/.postgresql/
-\! cp -f data/postgresql.crt   ~/.postgresql/
-\! cp -f data/postgresql.key   ~/.postgresql/
-\! chmod 400 ~/.postgresql/postgresql.key
-\! chmod 644 ~/.postgresql/postgresql.crt
-\! chmod 644 ~/.postgresql/root.crt
-
-\! gpstop -arf 2>&1  >/dev/null
-\! echo "gpstop ret = $?"
+\! bash tools.bash prepare
+-- start_ignore
+\! gpstop -arf
+-- end_ignore
+\! echo "gpstop begin ret = $?"
 
 \c - - localhost
 
@@ -51,6 +30,8 @@ SELECT ssl_issuer_field('OU') AS issuer_OU;
 
 DROP EXTENSION sslinfo;
 
-\! sed -ri '/#BEGIN SSLINFO CONF/,/#END SSLINFO CONF/d' $MASTER_DATA_DIRECTORY/postgresql.conf
-\! gpstop -arf 2>&1  >/dev/null
-\! echo "gpstop ret = $?"
+\! bash tools.bash clean
+-- start_ignore
+\! gpstop -arf
+-- end_ignore
+\! echo "gpstop end ret = $?"
