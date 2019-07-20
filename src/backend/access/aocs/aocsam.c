d60fd96a15fc629a43846d7ae2689a1b8fa12fcd
David Kimura <dkimura@pivotal.io>
2019-07-20 00:23:42 +0000

    Fix aocs table block version mismatch
    Issue is that drop column followed by reorganize leads to loss of column
    encoding settings of dropped column. When the column's compresstype
    encoding is incorrect, we can encounter block version mismatch error
    later during block info validation check of the dropped column.
    
    Fix is to skip dropped columns when constructing AOCSScanDesc. As the
    column is already considered drop, there is no reason to include it in
    the scan.
    
    Co-authored-by: Soumyadeep Chakraborty <sochakraborty@pivotal.io>

--- src/backend/access/aocs/aocsam.c
+++ src/backend/access/aocs/aocsam.c
@@ -448,6 +448,7 @@ aocs_beginscan_internal(Relation relation,
 						TupleDesc relationTupleDesc, bool *proj)
 {
 	AOCSScanDesc scan;
+	Form_pg_attribute att;
 	int			nvp;
 	int			i;
 
@@ -483,7 +484,8 @@ aocs_beginscan_internal(Relation relation,
 	scan->num_proj_atts = 0;
 	for (i = 0; i < scan->relationTupleDesc->natts; i++)
 	{
-		if (proj[i])
+		att = scan->relationTupleDesc->attrs[i];
+		if (proj[i] && !att->attisdropped)
 			scan->proj_atts[scan->num_proj_atts++] = i;
 	}
 
