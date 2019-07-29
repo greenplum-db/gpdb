06b0c377503c9e5050d24864ab0060b7e2701c43
Georgios Kokolatos <gkokolatos@pivotal.io>
2019-07-29 11:34:20 +0000

    Add a test that is explicitly describing the desired behaviour
    The behaviour deviates from the upstream postgres version that greenplum
    is based on (8.3). Postgres catches up with this behaviour in 9.3.

--- src/test/regress/sql/plancache.sql
+++ src/test/regress/sql/plancache.sql
@@ -121,6 +121,41 @@ alter table s1.abc add column f2 float8;   -- force replan
 
 execute p1;
 
+-- GPDB: Check that search_path change is honored in the plpgsql body
+-- (bug in upstream and fixed in v9.3)
+
+CREATE OR REPLACE FUNCTION s1.test_searchpath() RETURNS integer
+LANGUAGE 'plpgsql' AS $BODY$
+BEGIN
+	RETURN 1;
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION s2.test_searchpath() RETURNS integer
+LANGUAGE 'plpgsql' AS $BODY$
+BEGIN
+	RETURN 2;
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION public.test_searchpath(p_search_path character varying) RETURNS integer
+LANGUAGE 'plpgsql' AS $BODY$
+DECLARE v_ret integer = -1;
+BEGIN
+	-- The EXECUTE statement will force replanning
+	EXECUTE 'SET search_path to ' || p_search_path;
+	SELECT test_searchpath() into v_ret;
+	RETURN v_ret;
+END;
+$BODY$;
+
+SELECT * FROM public.test_searchpath('s1');
+SELECT * FROM public.test_searchpath('s2');
+
+DROP FUNCTION s1.test_searchpath();
+DROP FUNCTION s2.test_searchpath();
+DROP FUNCTION public.test_searchpath(p_search_path character varying);
+
 drop schema s1 cascade;
 drop schema s2 cascade;
 
