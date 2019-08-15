cloudberrydb/cloudberrydb

commit b74f174b6cde1c2bf2e1bf09129d9397cccb1c7b

Implement TwoPhaseFileHeader details output in pg_xlogdump
Output the following in pg_xlogdump for prepare transaction records:
* Global transaction ID
* Prepare time
* Tablespace OIDs:
	* 'tablespace_oid_to_delete_on_abort'
	* 'tablespace_oid_to_delete_on_commit'

These data come from TwoPhaseFileHeader structure which is written in
xlog in GPDB.

Move TwoPhaseFileHeader to `twophase.h` in order for that structure to
be used by `xact_desc_prepare()` in `xactdesc.c`

Co-Authored-By: David Kimura <dkimura@pivotal.io>

src/backend/access/rmgrdesc/xactdesc.c
@@ -19,6 +19,7 @@
 #include "storage/dbdirnode.h"
 #include "storage/sinval.h"
 #include "utils/timestamp.h"
+#include "access/twophase.h"
 
 
 static char*
@@ -205,6 +206,25 @@ xact_desc_assignment(StringInfo buf, xl_xact_assignment *xlrec)
 		appendStringInfo(buf, " %u", xlrec->xsub[i]);
 }
 
+static void
+xact_desc_prepare(StringInfo buf, XLogRecord *record) {
+	uint8		info = record->xl_info & ~XLR_INFO_MASK;
+	char		*rec = XLogRecGetData(record);
+
+	Assert(info == XLOG_XACT_PREPARE);
+
+	TwoPhaseFileHeader *tpfh = (TwoPhaseFileHeader*) rec;
+
+	appendStringInfo(buf, "at = %s", timestamptz_to_str(tpfh->prepared_at));
+
+	appendStringInfo(buf, "; gid = %s", tpfh->gid);
+
+	if (tpfh->tablespace_oid_to_delete_on_commit != InvalidOid)
+		appendStringInfo(buf, "; tablespace_oid_to_delete_on_commit = %u", tpfh->tablespace_oid_to_delete_on_commit);
+	if (tpfh->tablespace_oid_to_delete_on_abort != InvalidOid)
+		appendStringInfo(buf, "; tablespace_oid_to_delete_on_abort = %u", tpfh->tablespace_oid_to_delete_on_abort);
+}
+
 void
 xact_desc(StringInfo buf, XLogRecord *record)
 {
@@ -234,7 +254,8 @@ xact_desc(StringInfo buf, XLogRecord *record)
 	}
 	else if (info == XLOG_XACT_PREPARE)
 	{
-		appendStringInfoString(buf, "prepare");
+		appendStringInfoString(buf, "prepare: ");
+		xact_desc_prepare(buf, record);
 	}
 	else if (info == XLOG_XACT_COMMIT_PREPARED)
 	{

src/backend/access/transam/twophase.c
@@ -113,7 +113,6 @@ int			max_prepared_xacts = 0;
  * typedef struct GlobalTransactionData *GlobalTransaction appears in
  * twophase.h
  */
-#define GIDSIZE 200
 
 typedef struct GlobalTransactionData
 {
@@ -961,26 +960,6 @@ TwoPhaseGetDummyProc(TransactionId xid)
  * Header for a 2PC state file
  */
 #define TWOPHASE_MAGIC	0x57F94532		/* format identifier */
-
-typedef struct TwoPhaseFileHeader
-{
-	uint32		magic;			/* format identifier */
-	uint32		total_len;		/* actual file length */
-	TransactionId xid;			/* original transaction XID */
-	Oid			database;		/* OID of database it was in */
-	TimestampTz prepared_at;	/* time of preparation */
-	Oid			owner;			/* user running the transaction */
-	int32		nsubxacts;		/* number of following subxact XIDs */
-	int32		ncommitrels;	/* number of delete-on-commit rels */
-	int32		nabortrels;		/* number of delete-on-abort rels */
-	int32		ncommitdbs;		/* number of delete-on-commit dbs */
-	int32		nabortdbs;		/* number of delete-on-abort dbs */
-	int32		ninvalmsgs;		/* number of cache invalidation messages */
-	bool		initfileinval;	/* does relcache init file need invalidation? */
-	Oid			tablespace_oid_to_delete_on_abort;
-	Oid			tablespace_oid_to_delete_on_commit;
-	char		gid[GIDSIZE];	/* GID for transaction */
-} TwoPhaseFileHeader;
 
 /*
  * Header for each record in a state file

src/include/access/twophase.h
@@ -54,6 +54,36 @@ typedef struct prepared_transaction_agg_state
  */
 typedef struct GlobalTransactionData *GlobalTransaction;
 
+/* GPDB-specific: GIDSIZE is defined in twophase.c in Postgres */
+
+#define GIDSIZE 200
+
+/* GPDB-specific: TwoPhaseFileHeader is defined in twophase.c in Postgres */
+/*
+ * Header for a 2PC state file
+ */
+typedef struct TwoPhaseFileHeader
+{
+	uint32		magic;			/* format identifier */
+	uint32		total_len;		/* actual file length */
+	TransactionId xid;			/* original transaction XID */
+	Oid			database;		/* OID of database it was in */
+	TimestampTz prepared_at;	/* time of preparation */
+	Oid			owner;			/* user running the transaction */
+	int32		nsubxacts;		/* number of following subxact XIDs */
+	int32		ncommitrels;	/* number of delete-on-commit rels */
+	int32		nabortrels;		/* number of delete-on-abort rels */
+	int32		ncommitdbs;		/* number of delete-on-commit dbs */
+	int32		nabortdbs;		/* number of delete-on-abort dbs */
+	int32		ninvalmsgs;		/* number of cache invalidation messages */
+	bool		initfileinval;	/* does relcache init file need invalidation? */
+	Oid			tablespace_oid_to_delete_on_abort;
+	Oid			tablespace_oid_to_delete_on_commit;
+	char		gid[GIDSIZE];	/* GID for transaction */
+} TwoPhaseFileHeader;
+
+/* GPDB-specific end */
+
 /* GUC variable */
 extern PGDLLIMPORT int max_prepared_xacts;
 

greenplum-db/gpdb

commit 9a2805c84353642362719c455ab126c87e6762b8

Fix compiler warning in xact_desc_prepare assertion
Commit b74f174b6cde1c2bf2e1bf09129d9397cccb1c7b introduced an Assert
backed by a variable, but since the variable was only used by the
assertion it caused a compiler warning in non-cassert enabled builds.
This fixes it by wrapping the variable declaration and assignment in
USE_ASSERT_CHECKING guards.

src/backend/access/rmgrdesc/xactdesc.c
@@ -208,7 +208,9 @@ xact_desc_assignment(StringInfo buf, xl_xact_assignment *xlrec)
 
 static void
 xact_desc_prepare(StringInfo buf, XLogRecord *record) {
+#ifdef USE_ASSERT_CHECKING
 	uint8		info = record->xl_info & ~XLR_INFO_MASK;
+#endif
 	char		*rec = XLogRecGetData(record);
 
 	Assert(info == XLOG_XACT_PREPARE);