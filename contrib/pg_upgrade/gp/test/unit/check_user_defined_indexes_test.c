d7a565e3a1ce367e2e650aa052320f9dff9aa627
Adam Berlin <aberlin@pivotal.io>
2019-08-29 10:31:06 -0400

    fixup: make memory sanitizer happy.

--- contrib/pg_upgrade/gp/test/unit/check_user_defined_indexes_test.c
+++ contrib/pg_upgrade/gp/test/unit/check_user_defined_indexes_test.c
@@ -5,44 +5,42 @@
 #include "cmockery.h"
 #include "stdlib.h"
 
-
 #include "../../checks.h"
 #include "libpq-int.h"
 #include "../../../pg_upgrade.h"
 
 static int stubbed_number_of_rows = 0;
 
-
-static struct UserDefinedIndexes query_for_indexes()
+static struct UserDefinedIndexes
+query_for_indexes()
 {
 	struct UserDefinedIndexes indexes;
 	indexes.number_of_user_defined_indexes = stubbed_number_of_rows;
 	return indexes;
 }
 
-
-static void 
+static void
 setup_rows_to_return(void)
 {
 	stubbed_number_of_rows = 1;
 }
 
-
-static void 
+static void
 setup_no_rows_to_return(void)
 {
 	stubbed_number_of_rows = 0;
 };
 
-
-static ClusterInfo *make_cluster()
+static ClusterInfo *
+make_cluster()
 {
 	ClusterInfo *cluster;
 	cluster = malloc(sizeof(ClusterInfo));
 	return cluster;
 }
 
-static Queries * make_queries()
+static Queries *
+make_queries()
 {
 	Queries *queries;
 	queries = malloc(sizeof(Queries));
@@ -50,6 +48,12 @@ static Queries * make_queries()
 	return queries;
 }
 
+void
+teardown(ClusterInfo *clusterInfo, Queries *queries)
+{
+	free(clusterInfo);
+	free(queries);
+};
 
 /*
  * Tests
@@ -58,38 +62,41 @@ static void
 test_it_returns_false_when_there_are_no_indexes_to_be_found(void **state)
 {
 	ClusterInfo *cluster = make_cluster();
-	Queries *queries = make_queries();
+	Queries     *queries = make_queries();
 
 	setup_rows_to_return();
 
 	bool result = check_user_defined_indexes(cluster, queries);
 
 	assert_false(result);
-}
 
+	teardown(cluster, queries);
+}
 
 static void
 test_it_returns_true(void **state)
 {
 	ClusterInfo *cluster = make_cluster();
-	Queries *queries = make_queries();
+	Queries     *queries = make_queries();
 
 	setup_no_rows_to_return();
 
 	bool result = check_user_defined_indexes(cluster, queries);
 
 	assert_true(result);
-}
 
+	teardown(cluster, queries);
+}
 
 /*
  * Runner
  */
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
 	cmockery_parse_arguments(argc, argv);
 
-	const		UnitTest tests[] = {
+	const UnitTest tests[] = {
 		unit_test(test_it_returns_true),
 		unit_test(test_it_returns_false_when_there_are_no_indexes_to_be_found)
 	};
