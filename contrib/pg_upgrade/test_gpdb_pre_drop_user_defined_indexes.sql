0ffb6471753fc15d7d1166f29e4d1a6bebdd56d9
Adam Berlin <aberlin@pivotal.io>
2019-09-18 11:05:51 -0400

    fixup: update drop script to correctly formulate drop constraint statement

--- contrib/pg_upgrade/test_gpdb_pre_drop_user_defined_indexes.sql
+++ contrib/pg_upgrade/test_gpdb_pre_drop_user_defined_indexes.sql
@@ -19,15 +19,13 @@ CREATE OR REPLACE FUNCTION drop_user_defined_indexes() RETURNS void AS $$
 DECLARE
     user_defined_indexes RECORD;
 BEGIN
-    FOR user_defined_indexes IN select * from get_user_defined_indexes() 
+    FOR user_defined_indexes IN select *, pg_class.relname as constraint_table_name from get_user_defined_indexes() 
         inner join pg_constraint on pg_constraint.conname = index_name
         inner join pg_namespace on pg_constraint.connamespace = pg_namespace.oid
         inner join pg_class on pg_constraint.conrelid = pg_class.oid
         where pg_namespace.nspname = namespace_name
     LOOP
-        execute 'alter table ' || quote_ident(user_defined_indexes.namespace_name) || '.' || quote_ident(pg_class.relname) || ' drop constraint if exists ' || 
-            quote_ident(user_defined_indexes.namespace_name) || 
-            '.' || 
+        execute 'alter table ' || quote_ident(user_defined_indexes.namespace_name) || '.' || quote_ident(user_defined_indexes.constraint_table_name) || ' drop constraint if exists ' || 
             quote_ident(user_defined_indexes.index_name) || ' cascade';
     END LOOP;
 
