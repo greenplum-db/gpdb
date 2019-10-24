45978f3798c23728036e97433919713b9c481d8e
David Kimura <dkimura@pivotal.io>
2019-10-24 22:56:05 +0000

    AddsAO upgrade tests:
    - Test for AO with dropped and updated tuples
    - Test for AO indexes created and dropped before upgrade then recreated
      after upgrade
    
    We're combining stories here to make it easier to avoid duplicatation
    and potentially have faster testing time.
    
    Co-authored-by: Bhuvnesh Chaudhary <bhuvnesh2703@gmail.com>

--- contrib/pg_upgrade/test/integration/scenarios/partitioned_ao_table.c
+++ contrib/pg_upgrade/test/integration/scenarios/partitioned_ao_table.c
@@ -41,8 +41,17 @@ partitionedAOTableShouldHaveDataOnMultipleSegfilesUpgradedToSixCluster()
 
 	executeQuery(connection, "SET search_path TO five_to_six_upgrade;");
 
+	executeQuery(connection, "CREATE INDEX name_index ON users(name);");
+	executeQuery(connection, "SET enable_seqscan=OFF");
+
 	result = executeQuery(connection, "SELECT * FROM users;");
 	assert_int_equal(5, PQntuples(result));
+
+	result = executeQuery(connection, "SET enable_seqscan=OFF; SELECT * FROM users WHERE name='Carolyn';");
+	assert_int_equal(1, PQntuples(result));
+
+	result = executeQuery(connection, "SET enable_seqscan=OFF; SELECT * FROM users WHERE name='Bob';");
+	assert_int_equal(0, PQntuples(result));
 
 	PQfinish(connection);
 }
@@ -84,6 +93,11 @@ createPartitionedAOTableWithDataOnMultipleSegfilesInFiveCluster(void)
 			"           DEFAULT SUBPARTITION other_names ) "
 			"(START (1) END (2) EVERY (1), "
 			"    DEFAULT PARTITION other_ids );");
+	/*
+	 * Table has indexes which will be dropped before upgrade and be re-created
+	 * after upgrade.
+	 */
+	executeQuery(connection1, "CREATE INDEX name_index ON users(name);");
 	executeQuery(connection1, "BEGIN;");
 	executeQuery(connection1, "INSERT INTO users VALUES (1, 'Jane')");
 	executeQuery(connection1, "INSERT INTO users VALUES (2, 'Jane')");
@@ -100,6 +114,24 @@ createPartitionedAOTableWithDataOnMultipleSegfilesInFiveCluster(void)
 
 	executeQuery(connection1, "END");
 	executeQuery(connection2, "END");
+
+	/*
+	 * Ensure that we can correctly upgrade tables with dropped or deleted
+	 * tuples.
+	 */
+	executeQuery(connection2, "UPDATE users SET name='Carolyn' WHERE name='Andy'");
+	executeQuery(connection2, "INSERT INTO users VALUES (5, 'Bob')");
+	executeQuery(connection2, "DELETE FROM users WHERE id=5");
+
+	executeQuery(connection1, "DROP INDEX name_index;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_2;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_other_ids;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_2_2_prt_jane;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_2_2_prt_john;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_2_2_prt_other_names;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_other_ids_2_prt_jane;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_other_ids_2_prt_john;");
+	executeQuery(connection1, "DROP INDEX name_index_1_prt_other_ids_2_prt_other_names;");
 	PQfinish(connection1);
 	PQfinish(connection2);
 }
