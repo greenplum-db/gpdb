cloudberrydb/cloudberrydb

commit 58c7833d7d76dd48dda1cf9a748f51ef5bfe9db6

Fix distributed transaction id issue
Previously, we generate a distributed transaction id and assign it to
'TMGXACT->gxid' later when the distributed transaction needs the two-phase
commit. This will cause invalid distributed snapshot sometimes. We need to make
the bump and set atomic. See the comments in currentDtxActivateTwoPhase().

src/backend/utils/gpmon/gpmon.c
@@ -159,10 +159,7 @@ void gpmon_record_update(int32 tmid, int32 ssid, int32 ccnt,
 
 void gpmon_gettmid(int32* tmid)
 {
-    char buff[TMGIDSIZE] = {0};
-    int32 xid;
-    getDistributedTransactionIdentifier(buff);
-    sscanf(buff, "%d-%d", tmid, &xid);
+	*tmid = (int32)getDtxStartTime();
 } 
 
 

src/backend/access/external/fileam.c
@@ -2233,11 +2233,13 @@ external_set_env_vars_ext(extvar_t *extvar, char *uri, bool csv, char *escape, c
 			1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday);
 	sprintf(extvar->GP_TIME, "%02d%02d%02d",
 			tm->tm_hour, tm->tm_min, tm->tm_sec);
+
+	/*
+	 * read-only query don't have a valid distributed transaction ID, use
+	 * "session id"-"command id" to identify the transaction.
+	 */
 	if (!getDistributedTransactionIdentifier(extvar->GP_XID))
-		ereport(ERROR,
-				(errcode_for_file_access(),
-				 errmsg("cannot get distributed transaction identifier for \"%s\"",
-						uri)));
+		sprintf(extvar->GP_XID, "%u-%.10u", gp_session_id, gp_command_count);
 
 	sprintf(extvar->GP_CID, "%x", QEDtxContextInfo.curcid);
 	sprintf(extvar->GP_SN, "%x", scancounter);

src/backend/access/transam/xact.c
@@ -272,9 +272,6 @@ static TransactionId unreportedXids[PGPROC_MAX_CACHED_SUBXIDS];
 
 static TransactionState CurrentTransactionState = &TopTransactionStateData;
 
-/* distributed transaction id of current transaction, if any. */
-static DistributedTransactionId currentDistribXid;
-
 /*
  * The subtransaction ID and command ID assignment counters are global
  * to a whole transaction, so we do not keep them in the state stack.
@@ -476,22 +473,11 @@ GetAllTransactionXids(
 {
 	TransactionState s = CurrentTransactionState;
 
-	*distribXid = currentDistribXid;
+	*distribXid = getDistributedTransactionId();
 	*localXid = s->transactionId;
 	*subXid = s->subTransactionId;
 }
 
-DistributedTransactionId
-GetCurrentDistributedTransactionId(void)
-{
-	return currentDistribXid;
-}
-
-void
-SetCurrentDistributedTransactionId(DistributedTransactionId gxid)
-{
-	currentDistribXid = gxid;
-}
 /*
  *	GetTopTransactionId
  *
@@ -1638,14 +1624,10 @@ RecordTransactionCommit(void)
 			 * to look it up in the DistributedLog.
 			 */
 			/* UNDONE: What are the locking issues here? */
-			if (isDtxPrepared)
+			if (isDtxPrepared || isOnePhaseQE)
 				DistributedLog_SetCommittedTree(xid, nchildren, children,
-												getDtxStartTime(),
+												getDistributedTransactionTimestamp(),
 												getDistributedTransactionId(),
-												/* isRedo */ false);
-			else if (isOnePhaseQE)
-				DistributedLog_SetCommittedTree(xid, nchildren, children,
-												MyTmGxact->distribTimeStamp, MyTmGxact->gxid,
 												/* isRedo */ false);
 
 			TransactionIdCommitTree(xid, nchildren, children);
@@ -2366,17 +2348,6 @@ StartTransaction(void)
 
 		case DTX_CONTEXT_QD_DISTRIBUTED_CAPABLE:
 		{
-			/*
-			 * FIXME: get rid of currentDistribXid and use MyTmGxact->gxid
-			 *
-			 * Generate the distributed transaction ID and save it.
-			 * it's not really needed by a select-only implicit transaction, but
-			 * currently gpfdist and pxf is using it.
-			 * We should probably replace xid with "session id + command id" in
-			 * identify a query in gpfdist and pxf.
-			 */
-			currentDistribXid = generateGID();
-
 			if (SharedLocalSnapshotSlot != NULL)
 			{
 				LWLockAcquire(SharedLocalSnapshotSlot->slotLock, LW_EXCLUSIVE);
@@ -2421,22 +2392,15 @@ StartTransaction(void)
 
 			/*
 			 * MPP: we're a QE Writer.
-			 *
-			 * For DTX_CONTEXT_QE_AUTO_COMMIT_IMPLICIT don't use the
-			 * distributed xid map since this may be one of funny distributed
-			 * queries the executor uses behind the scenes for estimation
-			 * work. We also don't need a local XID right now - we let it be
-			 * assigned lazily, as on a local transaction. This transaction
-			 * will auto-commit, and then we will follow it with the real user
-			 * command.
 			 */
+			MyTmGxact->gxid = QEDtxContextInfo.distributedXid;
+			MyTmGxact->distribTimeStamp = QEDtxContextInfo.distributedTimeStamp;
+
 			if (DistributedTransactionContext ==
 				DTX_CONTEXT_QE_TWO_PHASE_EXPLICIT_WRITER ||
 				DistributedTransactionContext ==
 				DTX_CONTEXT_QE_TWO_PHASE_IMPLICIT_WRITER)
 			{
-				currentDistribXid = QEDtxContextInfo.distributedXid;
-
 				Assert(QEDtxContextInfo.distributedTimeStamp != 0);
 				Assert(QEDtxContextInfo.distributedXid !=
 					   InvalidDistributedTransactionId);
@@ -2488,7 +2452,8 @@ StartTransaction(void)
 			 * MPP: we're a QE Reader.
 			 */
 			Assert (SharedLocalSnapshotSlot != NULL);
-			currentDistribXid = QEDtxContextInfo.distributedXid;
+			MyTmGxact->gxid = QEDtxContextInfo.distributedXid;
+			MyTmGxact->distribTimeStamp = QEDtxContextInfo.distributedTimeStamp;
 
 			/*
 			 * Snapshot must not be created before setting transaction
@@ -2940,7 +2905,6 @@ CommitTransaction(void)
 		LocalDistribXactCache_ShowStats("CommitTransaction");
 	}
 
-	currentDistribXid = InvalidDistributedTransactionId;
 	s->transactionId = InvalidTransactionId;
 	s->subTransactionId = InvalidSubTransactionId;
 	s->nestingLevel = 0;
@@ -3257,7 +3221,6 @@ PrepareTransaction(void)
 		LocalDistribXactCache_ShowStats("PrepareTransaction");
 	}
 
-	currentDistribXid = InvalidDistributedTransactionId;
 	s->transactionId = InvalidTransactionId;
 	s->subTransactionId = InvalidSubTransactionId;
 	s->nestingLevel = 0;
@@ -3556,7 +3519,6 @@ CleanupTransaction(void)
 
 	AtCleanup_Memory();			/* and transaction memory */
 
-	currentDistribXid = InvalidDistributedTransactionId;
 	s->transactionId = InvalidTransactionId;
 	s->subTransactionId = InvalidSubTransactionId;
 	s->nestingLevel = 0;
@@ -6523,8 +6485,8 @@ XactLogCommitRecord(TimestampTz commit_time,
 	if (isDtxPrepared || isOnePhaseQE)
 	{
 		xl_xinfo.xinfo |= XACT_XINFO_HAS_DISTRIB;
-		xl_distrib.distrib_timestamp = MyTmGxact->distribTimeStamp;
-		xl_distrib.distrib_xid = MyTmGxact->gxid;
+		xl_distrib.distrib_timestamp = getDistributedTransactionTimestamp();
+		xl_distrib.distrib_xid = getDistributedTransactionId();
 	}
 
 	if (xl_xinfo.xinfo != 0)

src/backend/cdb/cdbtm.c
@@ -23,6 +23,7 @@
 #include "cdb/cdbtm.h"
 #include "libpq/libpq-be.h"
 #include "miscadmin.h"
+#include "storage/s_lock.h"
 #include "storage/shmem.h"
 #include "storage/ipc.h"
 #include "cdb/cdbdisp.h"
@@ -52,6 +53,23 @@
 #include "utils/snapmgr.h"
 #include "utils/memutils.h"
 
+typedef struct TmControlBlock
+{
+	DistributedTransactionTimeStamp	distribTimeStamp;
+	DistributedTransactionId	seqno;
+	bool						DtmStarted;
+	uint32						NextSnapshotId;
+	int							num_committed_xacts;
+	slock_t						gxidGenLock;
+
+	/* Array [0..max_tm_gxacts-1] of TMGXACT_LOG ptrs is appended starting here */
+	TMGXACT_LOG  			    committed_gxact_array[1];
+}	TmControlBlock;
+
+
+#define TMCONTROLBLOCK_BYTES(num_gxacts) \
+	(offsetof(TmControlBlock, committed_gxact_array) + sizeof(TMGXACT_LOG) * (num_gxacts))
+
 extern bool Test_print_direct_dispatch_info;
 
 #define DTX_PHASE2_SLEEP_TIME_BETWEEN_RETRIES_MSECS 100
@@ -60,12 +78,14 @@ volatile DistributedTransactionTimeStamp *shmDistribTimeStamp;
 volatile DistributedTransactionId *shmGIDSeq;
 
 uint32 *shmNextSnapshotId;
+slock_t *shmGxidGenLock;
 
 int	max_tm_gxacts = 100;
 
 
 #define TM_ERRDETAIL (errdetail("gid=%u-%.10u, state=%s", \
-		MyTmGxact->distribTimeStamp, MyTmGxact->gxid, DtxStateToString(MyTmGxactLocal->state)))
+		getDistributedTransactionTimestamp(), getDistributedTransactionId(),\
+		DtxStateToString(MyTmGxactLocal ? MyTmGxactLocal->state : 0)))
 /* here are some flag options relationed to the txnOptions field of
  * PQsendGpQuery
  */
@@ -183,43 +203,35 @@ getDtxStartTime(void)
 DistributedTransactionId
 getDistributedTransactionId(void)
 {
-	if (isQDContext())
-	{
-		return GetCurrentDistributedTransactionId();
-	}
-	else if (isQEContext())
-	{
-		return QEDtxContextInfo.distributedXid;
-	}
+	if (isQDContext() || isQEContext())
+		return MyTmGxact->gxid;
 	else
-	{
 		return InvalidDistributedTransactionId;
-	}
+}
+
+DistributedTransactionTimeStamp
+getDistributedTransactionTimestamp(void)
+{
+	if (isQDContext() || isQEContext())
+		return MyTmGxact->distribTimeStamp;
+	else
+		return 0;
 }
 
 bool
 getDistributedTransactionIdentifier(char *id)
 {
-	if (isQDContext())
+	Assert(MyTmGxactLocal != NULL);
+
+	if ((isQDContext() || isQEContext()) &&
+		MyTmGxact->gxid != InvalidDistributedTransactionId)
 	{
-		DistributedTransactionId gxid = GetCurrentDistributedTransactionId();
-		if (gxid != InvalidDistributedTransactionId)
-		{
-			/*
-			 * The length check here requires the identifer have a trailing
-			 * NUL character.
-			 */
-			dtxFormGID(id, getDtxStartTime(), gxid);
-			return true;
-		}
-	}
-	else if (isQEContext())
-	{
-		if (QEDtxContextInfo.distributedXid != InvalidDistributedTransactionId)
-		{
-			dtxFormGID(id, QEDtxContextInfo.distributedTimeStamp, QEDtxContextInfo.distributedXid);
-			return true;
-		}
+		/*
+		 * The length check here requires the identifer have a trailing
+		 * NUL character.
+		 */
+		dtxFormGID(id, MyTmGxact->distribTimeStamp, MyTmGxact->gxid);
+		return true;
 	}
 
 	MemSet(id, 0, TMGIDSIZE);
@@ -249,28 +261,40 @@ isCurrentDtxTwoPhaseActivated(void)
 static void
 currentDtxActivateTwoPhase(void)
 {
-	DistributedTransactionId gxid;
+	/*
+	 * Bump 'shmGIDSeq' and assign it to 'MyTmGxact->gxid', this needs to be atomic.
+	 * Otherwise, another transaction might start and commit in between, which will
+	 * bump 'ShmemVariableCache->latestCompletedDxid'.  If someone else take a
+	 * snapshot now, it will consider this transaction has finished: it's not
+	 * in-progress (MyTmGxact->gxid is not set) and its transaction precedes the xmax.
+	 *
+	 * For example:
+	 * tx1: insert into t values(1), (2);
+	 * tx2: insert into t values(3), (4);
+	 * tx3: select * from t;
+	 *
+	 * It happens in the following order:
+	 * 1. tx1 generates a distributed transaction-id X1
+	 * 2. tx2 generates a distributed transaction-id X2 (X1 < X2)
+	 * 3. tx2 finished
+	 * 4. tx3 takes a distributed snapshot
+	 * 5. tx1 set 'TMGXACT->gxid'
+	 * 6. tx1 finish 'commit prepared' on segment 0 but not on segment 1 yet.
+	 * 7. tx3 will see the change of tx1 on segment 0 but not on segment 1, that's
+	 * because tx1 is considered finished according to the snapshot.
+	 */
+	SpinLockAcquire(shmGxidGenLock);
+	MyTmGxact->gxid = ++(*shmGIDSeq);
+	SpinLockRelease(shmGxidGenLock);
 
-	gxid = GetCurrentDistributedTransactionId();
-	if (gxid == InvalidDistributedTransactionId)
-	{
-		gxid = generateGID();
-		SetCurrentDistributedTransactionId(gxid);
-		Assert(gxid != InvalidDistributedTransactionId);
-	}
+	if (MyTmGxact->gxid == LastDistributedTransactionId)
+		ereport(PANIC,
+				(errmsg("reached the limit of %u global transactions per start",
+						LastDistributedTransactionId)));
 
 	MyTmGxact->distribTimeStamp = getDtxStartTime();
 	MyTmGxact->sessionId = gp_session_id;
 	setCurrentDtxState(DTX_STATE_ACTIVE_DISTRIBUTED);
-
-	/*
-	 * As addressed in access/transam/README, we have to hold ProcArrayLock
-	 * when cleaning MyPgXact->xid as well as MyTmGxact->gxid to make sure
-	 * someone else can take a consistent snapshot at the same time. But it's
-	 * fine to not hold ProcArrayLock when setting xid and gxid, it won't
-	 * affect the correctness of snapshot.
-	 */
-	MyTmGxact->gxid = gxid;
 }
 
 static void
@@ -377,7 +401,7 @@ doDispatchSubtransactionInternalCmd(DtxProtocolCommand cmdType)
 														 mppTxnOptions(true),
 														 "doDispatchSubtransactionInternalCmd");
 
-	dtxFormGID(gid, MyTmGxact->distribTimeStamp, MyTmGxact->gxid);
+	dtxFormGID(gid, getDistributedTransactionTimestamp(), getDistributedTransactionId());
 	succeeded = doDispatchDtxProtocolCommand(cmdType,
 											 gid,
 											 NULL, /* raiseError */ true,
@@ -457,13 +481,13 @@ doInsertForgetCommitted(void)
 
 	setCurrentDtxState(DTX_STATE_INSERTING_FORGET_COMMITTED);
 
-	dtxFormGID(gxact_log.gid, MyTmGxact->distribTimeStamp, MyTmGxact->gxid);
-	gxact_log.gxid = MyTmGxact->gxid;
+	dtxFormGID(gxact_log.gid, getDistributedTransactionTimestamp(), getDistributedTransactionId());
+	gxact_log.gxid = getDistributedTransactionId();
 
 	RecordDistributedForgetCommitted(&gxact_log);
 
 	setCurrentDtxState(DTX_STATE_INSERTED_FORGET_COMMITTED);
-	MyTmGxact->needIncludedInCkpt = false;
+	MyTmGxact->includeInCkpt = false;
 }
 
 void
@@ -833,13 +857,14 @@ prepareDtxTransaction(void)
 	{
 		elog(DTM_DEBUG5, "prepareDtxTransaction nothing to do (DistributedTransactionContext = '%s')",
 			 DtxContextToString(DistributedTransactionContext));
+		Assert(Gp_role != GP_ROLE_DISPATCH || MyTmGxact->gxid == InvalidDistributedTransactionId);
 		return;
 	}
 
 	if (!isCurrentDtxTwoPhaseActivated())
 	{
-		Assert(MyTmGxact->gxid == InvalidDistributedTransactionId);
 		Assert(MyTmGxactLocal->state == DTX_STATE_NONE);
+		Assert(Gp_role != GP_ROLE_DISPATCH || MyTmGxact->gxid == InvalidDistributedTransactionId);
 		resetGxact();
 		return;
 	}
@@ -1069,10 +1094,12 @@ tmShmemInit(void)
 
 		*shmGIDSeq = FirstDistributedTransactionId;
 		ShmemVariableCache->latestCompletedDxid = InvalidDistributedTransactionId;
+		SpinLockInit(&shared->gxidGenLock);
 	}
 	shmDtmStarted = &shared->DtmStarted;
 	shmNextSnapshotId = &shared->NextSnapshotId;
 	shmNumCommittedGxacts = &shared->num_committed_xacts;
+	shmGxidGenLock = &shared->gxidGenLock;
 	shmCommittedGxactArray = &shared->committed_gxact_array[0];
 
 	if (!IsUnderPostmaster)
@@ -1172,7 +1199,7 @@ currentDtxDispatchProtocolCommand(DtxProtocolCommand dtxProtocolCommand, bool ra
 	char gid[TMGIDSIZE];
 	bool *badgang = (MyTmGxactLocal->state == DTX_STATE_PREPARING) ? &MyTmGxactLocal->badPrepareGangs : NULL;
 
-	dtxFormGID(gid, MyTmGxact->distribTimeStamp, MyTmGxact->gxid);
+	dtxFormGID(gid, getDistributedTransactionTimestamp(), getDistributedTransactionId());
 	return doDispatchDtxProtocolCommand(dtxProtocolCommand, gid, badgang, raiseError,
 										MyTmGxactLocal->twophaseSegments, NULL, 0);
 }
@@ -1351,18 +1378,12 @@ dispatchDtxCommand(const char *cmd)
 void
 resetGxact()
 {
-	if (Gp_role == GP_ROLE_DISPATCH && MyTmGxact->gxid != InvalidDistributedTransactionId)
-	{
-		/* As QE doesn't take distributed snapshot, we don't have to hold ProcArrayLock to clear gxid */
-		Assert(LWLockHeldByMe(ProcArrayLock));
-		MyTmGxact->gxid = InvalidDistributedTransactionId;
-	}
-	else if (Gp_role == GP_ROLE_EXECUTE)
-		MyTmGxact->gxid = InvalidDistributedTransactionId;
-
+	AssertImply(Gp_role == GP_ROLE_DISPATCH && MyTmGxact->gxid != InvalidDistributedTransactionId,
+				LWLockHeldByMe(ProcArrayLock));
+	MyTmGxact->gxid = InvalidDistributedTransactionId;
 	MyTmGxact->distribTimeStamp = 0;
 	MyTmGxact->xminDistributedSnapshot = InvalidDistributedTransactionId;
-	MyTmGxact->needIncludedInCkpt = false;
+	MyTmGxact->includeInCkpt = false;
 	MyTmGxact->sessionId = 0;
 
 	MyTmGxactLocal->explicitBeginRemembered = false;
@@ -1432,22 +1453,7 @@ insertedDistributedCommitted(void)
 	 */
 	Assert(MyPgXact->delayChkpt);
 	if (IS_QUERY_DISPATCHER())
-		MyTmGxact->needIncludedInCkpt = true;
-}
-
-/* generate global transaction id */
-DistributedTransactionId
-generateGID(void)
-{
-	DistributedTransactionId gxid;
-
-	gxid = pg_atomic_add_fetch_u32((pg_atomic_uint32*)shmGIDSeq, 1);
-	if (gxid == LastDistributedTransactionId)
-		ereport(PANIC,
-				(errmsg("reached the limit of %u global transactions per start",
-						LastDistributedTransactionId)));
-
-	return gxid;
+		MyTmGxact->includeInCkpt = true;
 }
 
 /*
@@ -1987,11 +1993,14 @@ performDtxProtocolPrepare(const char *gid)
 static void
 performDtxProtocolCommitOnePhase(const char *gid)
 {
+	DistributedTransactionTimeStamp distribTimeStamp;
+	DistributedTransactionId gxid;
 	elog(DTM_DEBUG5,
 		 "performDtxProtocolCommitOnePhase going to call CommitTransaction for distributed transaction %s", gid);
 
-	/* MyTmGxact is now not used on QE for one-phase commit */
-	dtxCrackOpenGid(gid, &MyTmGxact->distribTimeStamp, &MyTmGxact->gxid);
+	dtxCrackOpenGid(gid, &distribTimeStamp, &gxid);
+	Assert(gxid == getDistributedTransactionId());
+	Assert(distribTimeStamp == getDistributedTransactionTimestamp());
 	MyTmGxactLocal->isOnePhaseCommit = true;
 
 	StartTransactionCommand();

src/backend/storage/ipc/procarray.c
@@ -494,7 +494,7 @@ ProcArrayEndTransaction(PGPROC *proc, TransactionId latestXid, bool lockHeld)
 	}
 
 	/* Clear distributed transaction status for one-phase commit transaction */
-	if (Gp_role == GP_ROLE_EXECUTE && MyTmGxactLocal->isOnePhaseCommit)
+	if (Gp_role == GP_ROLE_EXECUTE)
 		resetGxact();
 }
 
@@ -1972,7 +1972,7 @@ getDtxCheckPointInfo(char **result, int *result_size)
 		TMGXACT_LOG *gxact_log;
 		TMGXACT *gxact = &allTmGxact[arrayP->pgprocnos[i]];
 
-		if (!gxact->needIncludedInCkpt)
+		if (!gxact->includeInCkpt)
 			continue;
 
 		gxact_log = &gxact_log_array[actual];

src/backend/storage/ipc/test/procarray_test.c
@@ -9,21 +9,23 @@
 #define SIZE_OF_IN_PROGRESS_ARRAY (10 * sizeof(DistributedTransactionId))
 #define MAX_PROCS 100
 VariableCacheData vcdata;
+uint32 nextSnapshotId;
+DistributedTransactionTimeStamp distribTimeStamp;
+int num_committed_xacts;
 
 static void
-setup(TmControlBlock *controlBlock)
+setup(void)
 {
 	ShmemVariableCache = &vcdata;
-	shmNextSnapshotId = &controlBlock->NextSnapshotId;
-	shmDistribTimeStamp = &controlBlock->distribTimeStamp;
-	shmNumCommittedGxacts = &controlBlock->num_committed_xacts;
+	shmNextSnapshotId = &nextSnapshotId;
+	shmDistribTimeStamp = &distribTimeStamp;
+	shmNumCommittedGxacts = &num_committed_xacts;
 
 	/* Some imaginary LWLockId number */
 	*shmDistribTimeStamp = time(NULL);
 	*shmNumCommittedGxacts = 0;
 
 	allTmGxact = malloc(sizeof(TMGXACT)*MAX_PROCS);
-
 
 	procArray = malloc(sizeof(ProcArrayStruct) + sizeof(int) * (MAX_PROCS - 1));
 	procArray->pgprocnos[0] = 0;
@@ -38,13 +40,12 @@ setup(TmControlBlock *controlBlock)
 static void
 test__CreateDistributedSnapshot(void **state)
 {
-	TmControlBlock controlBlock;
 	DistributedSnapshot ds;
 
 	ds.inProgressXidArray =
 		(DistributedTransactionId*)malloc(SIZE_OF_IN_PROGRESS_ARRAY);
 
-	setup(&controlBlock);
+	setup();
 
 #ifdef USE_ASSERT_CHECKING
 	expect_value_count(LWLockHeldByMe, l, ProcArrayLock, -1);

src/include/access/xact.h
@@ -366,8 +366,6 @@ extern void GetAllTransactionXids(
 	DistributedTransactionId	*distribXid,
 	TransactionId				*localXid,
 	TransactionId				*subXid);
-extern DistributedTransactionId GetCurrentDistributedTransactionId(void);
-extern void SetCurrentDistributedTransactionId(DistributedTransactionId gxid);
 extern TransactionId GetTopTransactionId(void);
 extern TransactionId GetTopTransactionIdIfAny(void);
 extern TransactionId GetCurrentTransactionId(void);

src/include/cdb/cdbtm.h
@@ -214,7 +214,7 @@ typedef struct TMGXACT
 	 */
 	DistributedTransactionId	xminDistributedSnapshot;
 
-	bool						needIncludedInCkpt;
+	bool						includeInCkpt;
 	int							sessionId;
 }	TMGXACT;
 
@@ -256,22 +256,6 @@ typedef struct TMGALLXACTSTATUS
 } TMGALLXACTSTATUS;
 
 
-typedef struct TmControlBlock
-{
-	DistributedTransactionTimeStamp	distribTimeStamp;
-	DistributedTransactionId	seqno;
-	bool						DtmStarted;
-	uint32						NextSnapshotId;
-	int							num_committed_xacts;
-
-    /* Array [0..max_tm_gxacts-1] of TMGXACT_LOG ptrs is appended starting here */
-	TMGXACT_LOG  			    committed_gxact_array[1];
-}	TmControlBlock;
-
-
-#define TMCONTROLBLOCK_BYTES(num_gxacts) \
-	(offsetof(TmControlBlock, committed_gxact_array) + sizeof(TMGXACT_LOG) * (num_gxacts))
-
 #define DTM_DEBUG3 (Debug_print_full_dtm ? LOG : DEBUG3)
 #define DTM_DEBUG5 (Debug_print_full_dtm ? LOG : DEBUG5)
 
@@ -299,6 +283,7 @@ extern void dtxFormGID(char *gid,
 					   DistributedTransactionTimeStamp tstamp,
 					   DistributedTransactionId gxid);
 extern DistributedTransactionId getDistributedTransactionId(void);
+extern DistributedTransactionTimeStamp getDistributedTransactionTimestamp(void);
 extern bool getDistributedTransactionIdentifier(char *id);
 
 extern void resetGxact(void);
@@ -359,7 +344,6 @@ extern bool currentGxactWriterGangLost(void);
 
 extern void addToGxactTwophaseSegments(struct Gang* gp);
 
-extern DistributedTransactionId generateGID(void);
 extern void ClearTransactionState(TransactionId latestXid);
 
 extern void DtxRecoveryMain(Datum main_arg);

greenplum-db/gpdb

commit c717e88a63c13050fdeda0b4488b27f4c9fa1f4f

Fix gpperfmon tmid inconsistent across master and segments
The tmid should be of the same value among the cluster.
It increases only on gpdb cluster full restart.
Tmid, gp_session_id, and gp_command_count are put together to
uniquely identify a single query execution, monitoring agents
such as gpperfmon relies on this uniqueness.

In 58c7833d7d76dd48dda1cf9a748f51ef5bfe9db6 introduced a different
way for gpmon_gettmid(), it brings in a problem that on master
and segments the tmid may be different.
This commit fixes the problem.

Reviewed-by: Ning Yu <nyu@pivotal.io>

src/backend/utils/gpmon/gpmon.c
@@ -159,7 +159,12 @@ void gpmon_record_update(int32 tmid, int32 ssid, int32 ccnt,
 
 void gpmon_gettmid(int32* tmid)
 {
-	*tmid = (int32)getDtxStartTime();
+	if (QEDtxContextInfo.distributedSnapshot.distribTransactionTimeStamp > 0)
+		/* On QE */
+		*tmid = (int32)QEDtxContextInfo.distributedSnapshot.distribTransactionTimeStamp;
+	else
+		/* On QD */
+		*tmid = (int32)getDtxStartTime();
 } 
 
 

src/test/isolation2/expected/instr_in_shmem_terminate.out
@@ -117,12 +117,23 @@ SELECT count(*) FROM (SELECT 1 FROM gp_instrument_shmem_detail GROUP BY ssid, cc
 3&:SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a SELECT *, pg_sleep(1) FROM generate_series(1,10);  <waiting ...>
 
 -- validate plan nodes exist in instrument solts
+SELECT count(*) FROM pg_sleep(1);
+ count 
+-------
+ 1     
+(1 row)
 SELECT ro, CASE WHEN max(nid) > 2 THEN 'ok' ELSE 'wrong' END isok FROM ( SELECT CASE WHEN segid >= 0 THEN 's' ELSE 'm' END ro, nid FROM gp_instrument_shmem_detail WHERE ssid <> (SELECT setting FROM pg_settings WHERE name = 'gp_session_id')::int AND nid > 0 ) dt GROUP BY (ro) ORDER BY ro;
  ro | isok 
 ----+------
  m  | ok   
  s  | ok   
 (2 rows)
+-- validate no different tmid across segments
+SELECT count(*) FROM (SELECT DISTINCT tmid FROM gp_instrument_shmem_detail) t;
+ count 
+-------
+ 1     
+(1 row)
 -- cancel the query
 SELECT pg_cancel_backend(pid, 'test DML') FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a SELECT%' ORDER BY pid LIMIT 1;
  pg_cancel_backend 
@@ -163,12 +174,23 @@ SELECT count(*) FROM (SELECT 1 FROM gp_instrument_shmem_detail GROUP BY ssid, cc
 4&:SET OPTIMIZER TO off;SELECT a, b, array_dims(array_agg(x)) FROM QUERY_METRICS.mergeappend_test r GROUP BY a, b UNION ALL SELECT NULL, NULL, array_dims(array_agg(x)) FROM QUERY_METRICS.mergeappend_test r, pg_sleep(10) ORDER BY 1,2;  <waiting ...>
 
 -- validate plan nodes exist in instrument solts
+SELECT count(*) FROM pg_sleep(1);
+ count 
+-------
+ 1     
+(1 row)
 SELECT ro, CASE WHEN max(nid) > 5 THEN 'ok' ELSE 'wrong' END isok FROM ( SELECT CASE WHEN segid >= 0 THEN 's' ELSE 'm' END ro, nid FROM gp_instrument_shmem_detail WHERE ssid <> (SELECT setting FROM pg_settings WHERE name = 'gp_session_id')::int AND nid > 0 ) dt GROUP BY (ro) ORDER BY ro;
  ro | isok 
 ----+------
  m  | ok   
  s  | ok   
 (2 rows)
+-- validate no different tmid across segments
+SELECT count(*) FROM (SELECT DISTINCT tmid FROM gp_instrument_shmem_detail) t;
+ count 
+-------
+ 1     
+(1 row)
 -- cancel the query
 SELECT pg_cancel_backend(pid, 'test MergeAppend') FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;SELECT a, b, array_dims(array_agg(x)) FROM QUERY_METRICS.mergeappend_test%' ORDER BY pid LIMIT 1;
  pg_cancel_backend 
@@ -180,6 +202,55 @@ SELECT pg_cancel_backend(pid, 'test MergeAppend') FROM pg_stat_activity WHERE qu
 4<:  <... completed>
 ERROR:  canceling statement due to user request: "test MergeAppend"
 4q: ... <quitting>
+-- end_ignore
+
+-- query backend to ensure no PANIC on postmaster and wait cleanup done
+SELECT count(*) FROM foo, pg_sleep(2);
+ count 
+-------
+ 10    
+(1 row)
+
+-- test 5: entrydb
+-- Expected result is 1 row, means only current query in instrument slots,
+-- If more than one row returned, means previous test has leaked slots.
+SELECT count(*) FROM (SELECT 1 FROM gp_instrument_shmem_detail GROUP BY ssid, ccnt) t;
+ count 
+-------
+ 1     
+(1 row)
+
+-- this query will be cancelled by 'test pg_cancel_backend'
+5&:SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a(id) SELECT oid FROM pg_class, pg_sleep(10);  <waiting ...>
+
+-- validate QD and entrydb have instrumentations on each backend process
+SELECT count(*) FROM pg_sleep(1);
+ count 
+-------
+ 1     
+(1 row)
+SELECT count(DISTINCT pid) FROM gp_instrument_shmem_detail WHERE ssid <> (SELECT setting FROM pg_settings WHERE name = 'gp_session_id')::int AND segid < 0 AND nid >= 5;
+ count 
+-------
+ 2     
+(1 row)
+-- validate no different tmid across segments
+SELECT count(*) FROM (SELECT DISTINCT tmid FROM gp_instrument_shmem_detail) t;
+ count 
+-------
+ 1     
+(1 row)
+-- cancel the query
+SELECT pg_cancel_backend(pid, 'test entrydb') FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a(id) SELECT%' ORDER BY pid LIMIT 1;
+ pg_cancel_backend 
+-------------------
+ t                 
+(1 row)
+
+-- start_ignore
+5<:  <... completed>
+ERROR:  canceling statement due to user request: "test entrydb"
+5q: ... <quitting>
 -- end_ignore
 
 -- query backend to ensure no PANIC on postmaster and wait cleanup done

src/test/isolation2/sql/instr_in_shmem_terminate.sql
@@ -97,11 +97,14 @@ SELECT count(*) FROM (SELECT 1 FROM gp_instrument_shmem_detail GROUP BY ssid, cc
 3&:SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a SELECT *, pg_sleep(1) FROM generate_series(1,10);
 
 -- validate plan nodes exist in instrument solts
+SELECT count(*) FROM pg_sleep(1);
 SELECT ro, CASE WHEN max(nid) > 2 THEN 'ok' ELSE 'wrong' END isok FROM (
   SELECT CASE WHEN segid >= 0 THEN 's' ELSE 'm' END ro, nid FROM gp_instrument_shmem_detail
   WHERE ssid <> (SELECT setting FROM pg_settings WHERE name = 'gp_session_id')::int AND nid > 0
 ) dt
 GROUP BY (ro) ORDER BY ro;
+-- validate no different tmid across segments
+SELECT count(*) FROM (SELECT DISTINCT tmid FROM gp_instrument_shmem_detail) t;
 -- cancel the query
 SELECT pg_cancel_backend(pid, 'test DML')
 FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a SELECT%' ORDER BY pid LIMIT 1;
@@ -130,11 +133,14 @@ SELECT NULL, NULL, array_dims(array_agg(x)) FROM QUERY_METRICS.mergeappend_test 
 ORDER BY 1,2;
 
 -- validate plan nodes exist in instrument solts
+SELECT count(*) FROM pg_sleep(1);
 SELECT ro, CASE WHEN max(nid) > 5 THEN 'ok' ELSE 'wrong' END isok FROM (
   SELECT CASE WHEN segid >= 0 THEN 's' ELSE 'm' END ro, nid FROM gp_instrument_shmem_detail
   WHERE ssid <> (SELECT setting FROM pg_settings WHERE name = 'gp_session_id')::int AND nid > 0
 ) dt
 GROUP BY (ro) ORDER BY ro;
+-- validate no different tmid across segments
+SELECT count(*) FROM (SELECT DISTINCT tmid FROM gp_instrument_shmem_detail) t;
 -- cancel the query
 SELECT pg_cancel_backend(pid, 'test MergeAppend')
 FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;SELECT a, b, array_dims(array_agg(x)) FROM QUERY_METRICS.mergeappend_test%' ORDER BY pid LIMIT 1;
@@ -142,6 +148,33 @@ FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;SELECT a, b, array_
 -- start_ignore
 4<:
 4q:
+-- end_ignore
+
+-- query backend to ensure no PANIC on postmaster and wait cleanup done
+SELECT count(*) FROM foo, pg_sleep(2);
+
+-- test 5: entrydb
+-- Expected result is 1 row, means only current query in instrument slots,
+-- If more than one row returned, means previous test has leaked slots.
+SELECT count(*) FROM (SELECT 1 FROM gp_instrument_shmem_detail GROUP BY ssid, ccnt) t;
+
+-- this query will be cancelled by 'test pg_cancel_backend'
+5&:SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a(id) SELECT oid FROM pg_class, pg_sleep(10);
+
+-- validate QD and entrydb have instrumentations on each backend process
+SELECT count(*) FROM pg_sleep(1);
+SELECT count(DISTINCT pid) FROM gp_instrument_shmem_detail
+WHERE ssid <> (SELECT setting FROM pg_settings WHERE name = 'gp_session_id')::int
+ AND segid < 0 AND nid >= 5;
+-- validate no different tmid across segments
+SELECT count(*) FROM (SELECT DISTINCT tmid FROM gp_instrument_shmem_detail) t;
+-- cancel the query
+SELECT pg_cancel_backend(pid, 'test entrydb')
+FROM pg_stat_activity WHERE query LIKE 'SET OPTIMIZER TO off;EXPLAIN ANALYZE INSERT INTO QUERY_METRICS.a(id) SELECT%' ORDER BY pid LIMIT 1;
+
+-- start_ignore
+5<:
+5q:
 -- end_ignore
 
 -- query backend to ensure no PANIC on postmaster and wait cleanup done