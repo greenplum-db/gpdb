65c7c4a869b97cb3df9319100145ea373f4c0724
Daniel Gustafsson <dgustafsson@pivotal.io>
2020-02-10 17:04:05 +0100

    Fix incorrect spelling in docs/comments
    This fixes multiple occurrences of duplicated words in sentences, like
    "the the" and "is is" etc.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -15697,7 +15697,7 @@ ATExecSetDistributedBy(Relation rel, Node *node, AlterTableCmd *cmd)
 													 ldistro->numsegments);
 
 				/*
-				 * See if the the old policy is the same as the new one but
+				 * See if the old policy is the same as the new one but
 				 * remember, we still might have to rebuild if there are new
 				 * storage options.
 				 */
