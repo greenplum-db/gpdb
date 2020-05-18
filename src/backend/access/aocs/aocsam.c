50e6fe1b233e2c55a70bee92582d77a0cf6f481b
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-05-18 11:40:25 +0300

    Turn a couple of Asserts to elog()s.
    We're getting these assertions when running the regression tests. We need
    to fix the underlying cause, of course, but these seem reasonable to have
    as run-time checks anyway. They're not in a performance-critical path, and
    it seems quite possible that you could hit these with all kinds of bugs or
    catalog corruption.

--- src/backend/access/aocs/aocsam.c
+++ src/backend/access/aocs/aocsam.c
@@ -149,7 +149,8 @@ open_ds_write(Relation rel, DatumStreamWrite **ds, TupleDesc relationTupleDesc,
 		 * column of a column oriented table.  Note: checksum is a table level
 		 * attribute.
 		 */
-		Assert(opts[i]);
+		if (opts[i] == NULL || opts[i]->blocksize == 0)
+			elog(ERROR, "could not find blocksize option for AOCO column in pg_attribute_encoding");
 		ct = opts[i]->compresstype;
 		clvl = opts[i]->compresslevel;
 		blksz = opts[i]->blocksize;
@@ -200,11 +201,8 @@ open_ds_read(Relation rel, DatumStreamRead **ds, TupleDesc relationTupleDesc,
 		 * column of a column oriented table.  Note: checksum is a table level
 		 * attribute.
 		 */
-		if (opts[attno] == NULL)
-			elog(ERROR, "No relation attribute options for '%s', column #%d",
-							RelationGetRelationName(rel),
-							attno + 1);
-
+		if (opts[attno] == NULL || opts[attno]->blocksize == 0)
+			elog(ERROR, "could not find blocksize option for AOCO column in pg_attribute_encoding");
 		ct = opts[attno]->compresstype;
 		clvl = opts[attno]->compresslevel;
 		blksz = opts[attno]->blocksize;
