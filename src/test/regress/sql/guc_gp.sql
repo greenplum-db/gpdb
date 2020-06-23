66adfa19dbf576243a49bfd8753944c6afd38e16
Adam Lee <ali@pivotal.io>
2020-06-23 13:40:43 +0800

    Add cases

--- src/test/regress/sql/guc_gp.sql
+++ src/test/regress/sql/guc_gp.sql
@@ -237,3 +237,23 @@ RESET ROLE;
 DROP TABLE guc_gp_test_table;
 
 END;
+
+SET search_path to 'public';
+
+CREATE TABLE guc_gp_t1(c1 int, c2 int);
+INSERT INTO guc_gp_t1 SELECT generate_series(1,100),generate_series(51,150);
+CREATE TABLE guc_gp_t2 AS SELECT * FROM guc_gp_t1;
+
+BEGIN;
+CREATE SCHEMA guc_gp;
+CREATE TABLE guc_gp.guc_gp_t3 AS SELECT * FROM guc_gp_t1;
+CREATE TABLE guc_gp.guc_gp_t4 AS SELECT * FROM guc_gp_t1;
+
+SELECT sum(guc_gp_t1.c1) FROM guc_gp_t1,guc_gp_t2;
+SET search_path to 'guc_gp';
+SELECT sum(guc_gp_t3.c1) FROM guc_gp_t3,guc_gp_t4;
+ROLLBACK;
+
+SELECT sum(guc_gp_t1.c1) FROM guc_gp_t1,guc_gp_t2;
+DROP TABLE guc_gp_t1;
+DROP TABLE guc_gp_t2;
