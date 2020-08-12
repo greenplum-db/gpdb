d22d7a8f4866f9384674dd9fa5482d20b7e76ff3
Hao Wu <gfphoenix78@gmail.com>
2020-08-12 10:10:58 +0800

    Update pipeline yaml

--- src/test/regress/sql/check_status.sql
+++ src/test/regress/sql/check_status.sql
@@ -1,2 +1,2 @@
 select dbid, content, status, role, preferred_role, mode from gp_segment_configuration where status = 'd' or preferred_role != role;
-select gp_inject_fault('all', 'reset', dbid) from gp_segment_configuration where where status='u';
+select gp_inject_fault('all', 'reset', dbid) from gp_segment_configuration where status='u';
