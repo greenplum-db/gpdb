821673eff14cddffd08b04834ace5b232239236d
Fang Zheng <f.zheng@alibaba-inc.com>
2020-10-07 16:20:00 -0700

    Remove DTM_DEBUG3 macro as it's no longer used.

--- src/include/cdb/cdbtm.h
+++ src/include/cdb/cdbtm.h
@@ -258,7 +258,6 @@ typedef struct TMGALLXACTSTATUS
 } TMGALLXACTSTATUS;
 
 
-#define DTM_DEBUG3 (Debug_print_full_dtm ? LOG : DEBUG3)
 #define DTM_DEBUG5 (Debug_print_full_dtm ? LOG : DEBUG5)
 
 extern int max_tm_gxacts;
