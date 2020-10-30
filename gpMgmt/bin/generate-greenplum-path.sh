cloudberrydb/cloudberrydb

commit dc96f667891ef1ffcf67a534d9d7049d3bd96d29

Make greenplum-path.sh compatible with more shells (#11043)
The generated greenplum_path.sh env file contained bash specific syntax
previously, so it errors out if the user's shell is zsh.

zsh doesn't have BASH_SOURCE. "${(%):-%x}" is the similar replacement
for zsh.
Also try to support other shells with some command combinations.
Tested with bash/zsh/dash.

gpMgmt/bin/generate-greenplum-path.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
 cat <<"EOF"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+if test -n "$ZSH_VERSION"; then
+    # zsh
+    SCRIPT_PATH="${(%):-%x}"
+elif test -n "$BASH_VERSION"; then
+    # bash
+    SCRIPT_PATH="${BASH_SOURCE[0]}"
+else
+    # Unknown shell, hope below works.
+    # Tested with dash
+    result=$(lsof -p $$ -Fn | tail --lines=1 | xargs --max-args=2 | cut --delimiter=' ' --fields=2)
+    SCRIPT_PATH=${result#n}
+fi
+
+if test -z "$SCRIPT_PATH"; then
+    echo "The shell cannot be identified. \$GPHOME may not be set correctly." >&2
+fi
+SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+
 if [ ! -L "${SCRIPT_DIR}" ]; then
 	GPDB_DIR=$(basename "${SCRIPT_DIR}")
 else

greenplum-db/gpdb

commit 0d84ddb49c903bc971145690efc2bce061cc859d

Fix source greenplum_path.sh error with set -u
The error was introduced by dc96f66789.
If `set -u` was called before sourcing greenplum_path.sh with bash, an
error "ZSH_VERSION: unbound variable` would be reported.
To solve the issue, use shell syntax `{:-}` which will output an empty
value if the variable doesn't exist.

Tested with zsh, bash and dash.

gpMgmt/bin/generate-greenplum-path.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cat <<"EOF"
-if test -n "$ZSH_VERSION"; then
+if test -n "${ZSH_VERSION:-}"; then
     # zsh
     SCRIPT_PATH="${(%):-%x}"
-elif test -n "$BASH_VERSION"; then
+elif test -n "${BASH_VERSION:-}"; then
     # bash
     SCRIPT_PATH="${BASH_SOURCE[0]}"
 else