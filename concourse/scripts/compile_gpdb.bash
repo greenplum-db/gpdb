cloudberrydb/cloudberrydb

commit ee4961fc349aad16ae64e4fa10154d7996ce0401

Refactor compile_gpdb.bash
Build environment setup tasks removed from compile_gpdb.bash.
'pivotaldata/gpdb7-centos7-build' and 'pivotaldata/gpdb7-ubuntu18.04-build'
images now fully resolve build dependencies.

[GP-314]

Authored-by: Ning Wu <ningw@vmware.com>

concourse/scripts/compile_gpdb.bash
@@ -1,294 +1,203 @@
 #!/bin/bash
 set -exo pipefail
 
-GREENPLUM_INSTALL_DIR=/usr/local/greenplum-db-devel
-export GPDB_ARTIFACTS_DIR=$(pwd)/${OUTPUT_ARTIFACT_DIR}
-CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GPDB_SRC_PATH=${GPDB_SRC_PATH:=gpdb_src}
-
-GPDB_BIN_FILENAME=${GPDB_BIN_FILENAME:="bin_gpdb.tar.gz"}
-GREENPLUM_CL_INSTALL_DIR=/usr/local/greenplum-clients-devel
-GPDB_CL_FILENAME=${GPDB_CL_FILENAME:="gpdb-clients-${TARGET_OS}${TARGET_OS_VERSION}.tar.gz"}
-
+CWDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CWDIR}/common.bash"
 
-function expand_glob_ensure_exists() {
-  local -a glob=($*)
-  [ -e "${glob[0]}" ]
-  echo "${glob[0]}"
-}
-
 function prep_env() {
-  case "${TARGET_OS}" in
-  centos)
-    case "${TARGET_OS_VERSION}" in
-    6 | 7) BLD_ARCH=rhel${TARGET_OS_VERSION}_x86_64 ;;
-    *)
-      echo "TARGET_OS_VERSION not set or recognized for Centos/RHEL"
-      exit 1
-      ;;
-    esac
-    ;;
-  ubuntu)
-    case "${TARGET_OS_VERSION}" in
-    18.04) BLD_ARCH=ubuntu18.04_x86_64 ;;
-    *)
-      echo "TARGET_OS_VERSION not set or recognized for Ubuntu"
-      exit 1
-      ;;
-    esac
-    ;;
-  sles)
-    case "${TARGET_OS_VERSION}" in
-    12) export BLD_ARCH=sles12_x86_64 ;;
-    *)
-      echo "TARGET_OS_VERSION not set or recognized for SLES"
-      exit 1
-      ;;
-    esac
-    ;;
-  esac
-}
+    OS=$(os_id)
+    OS_VERSION=$(os_version)
+    OUTPUT_ARTIFACT_DIR=${OUTPUT_ARTIFACT_DIR:=gpdb_artifacts}
 
-function install_libuv() {
-  local includedir=/usr/include
-  local libdir
+    export CONFIGURE_FLAGS=${CONFIGURE_FLAGS}
+    export GPDB_ARTIFACTS_DIR=$(pwd)/${OUTPUT_ARTIFACT_DIR}
+    export BLD_ARCH=$(build_arch)
 
-  case "${TARGET_OS}" in
-    centos | sles) libdir=/usr/lib64 ;;
-    ubuntu) libdir=/usr/lib/x86_64-linux-gnu ;;
-    *) return ;;
-  esac
-  # provided by build container
-  cp -a /usr/local/include/uv* ${includedir}/
-  cp -a /usr/local/lib/libuv* ${libdir}/
-}
+    GPDB_SRC_PATH=${GPDB_SRC_PATH:=gpdb_src}
+    GPDB_EXT_PATH=${GPDB_SRC_PATH}/gpAux/ext/${BLD_ARCH}
+    GPDB_BIN_FILENAME=${GPDB_BIN_FILENAME:="bin_gpdb.tar.gz"}
+    GPDB_CL_FILENAME=${GPDB_CL_FILENAME:="bin_gpdb_clients.tar.gz"}
 
+    GREENPLUM_INSTALL_DIR=/usr/local/greenplum-db-devel
+    GREENPLUM_CL_INSTALL_DIR=/usr/local/greenplum-clients-devel
 
-function install_deps_for_centos_or_sles() {
-  rpm -i libquicklz-installer/libquicklz-*.rpm
-  rpm -i libquicklz-devel-installer/libquicklz-*.rpm
-}
+    mkdir -p "${GPDB_ARTIFACTS_DIR}"
 
-function install_deps_for_ubuntu() {
-  dpkg --install libquicklz-installer/libquicklz-*.deb
-}
-
-function install_deps() {
-  case "${TARGET_OS}" in
-    centos | sles) install_deps_for_centos_or_sles;;
-    ubuntu) install_deps_for_ubuntu;;
-  esac
-  install_libuv
+    # By default, only GPDB Server binary is build.
+    # Use BLD_TARGETS flag with appropriate value string to generate client, loaders
+    # connectors binaries
+    if [ -n "${BLD_TARGETS}" ]; then
+        BLD_TARGET_OPTION=("BLD_TARGETS=\"$BLD_TARGETS\"")
+    else
+        BLD_TARGET_OPTION=("")
+    fi
 }
 
 function generate_build_number() {
-  pushd ${GPDB_SRC_PATH}
-    #Only if its git repro, add commit SHA as build number
+    pushd ${GPDB_SRC_PATH}
+    # Only if its git repo, add commit SHA as build number
     # BUILD_NUMBER file is used by getversion file in GPDB to append to version
-    if [ -d .git ] ; then
-      echo "commit:`git rev-parse HEAD`" > BUILD_NUMBER
+    if [ -d .git ]; then
+        echo "commit: $(git rev-parse HEAD)" >BUILD_NUMBER
     fi
-  popd
+    popd
 }
 
 function build_gpdb() {
-  pushd ${GPDB_SRC_PATH}/gpAux
-    # Use -j4 to speed up the build. (Doesn't seem worth trying to guess a better
-    # value based on number of CPUs or anything like that. Going above -j4 wouldn't
-    # make it much faster, and -j4 is small enough to not hurt too badly even on
-    # a single-CPU system
+    pushd ${GPDB_SRC_PATH}/gpAux
     if [ -n "$1" ]; then
-      make "$1" GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j4 -s dist
+        make "$1" GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s dist
     else
-      make GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j4 -s dist
+        make GPROOT=/usr/local PARALLEL_MAKE_OPTS=-j"$(nproc)" -s dist
     fi
-  popd
+    popd
 }
 
 function git_info() {
-  pushd ${GPDB_SRC_PATH}
+    pushd ${GPDB_SRC_PATH}
+    if [[ -d .git ]]; then
+        "${CWDIR}/git_info.bash" | tee ${GREENPLUM_INSTALL_DIR}/etc/git-info.json
 
-  "${CWDIR}/git_info.bash" | tee ${GREENPLUM_INSTALL_DIR}/etc/git-info.json
+        PREV_TAG=$(git describe --tags --abbrev=0 HEAD^)
 
-  PREV_TAG=$(git describe --tags --abbrev=0 HEAD^)
+        cat >${GREENPLUM_INSTALL_DIR}/etc/git-current-changelog.txt <<-EOF
+			======================================================================
+			Git log since previous release tag (${PREV_TAG})
+			----------------------------------------------------------------------
+		EOF
 
-  cat > ${GREENPLUM_INSTALL_DIR}/etc/git-current-changelog.txt <<-EOF
-	======================================================================
-	Git log since previous release tag (${PREV_TAG})
-	----------------------------------------------------------------------
-
-	EOF
-
-  git log --abbrev-commit --date=relative "${PREV_TAG}..HEAD" >> ${GREENPLUM_INSTALL_DIR}/etc/git-current-changelog.txt
-
-  popd
+        git log --abbrev-commit --date=relative "${PREV_TAG}..HEAD" >>${GREENPLUM_INSTALL_DIR}/etc/git-current-changelog.txt
+    fi
+    popd
 }
 
 function unittest_check_gpdb() {
-  pushd ${GPDB_SRC_PATH}
+    pushd ${GPDB_SRC_PATH}
     source ${GREENPLUM_INSTALL_DIR}/greenplum_path.sh
     make GPROOT=/usr/local -s unittest-check
-  popd
+    popd
 }
 
 function include_zstd() {
-  local libdir
-  case "${TARGET_OS}" in
+    local libdir
+    case "${OS}" in
     centos | sles) libdir=/usr/lib64 ;;
     ubuntu) libdir=/usr/lib ;;
     *) return ;;
-  esac
-  pushd ${GREENPLUM_INSTALL_DIR}
+    esac
+    pushd ${GREENPLUM_INSTALL_DIR}
     cp ${libdir}/pkgconfig/libzstd.pc lib/pkgconfig
     cp -d ${libdir}/libzstd.so* lib
     cp /usr/include/zstd*.h include
-  popd
+    popd
 }
 
 function include_quicklz() {
-  local libdir
-  case "${TARGET_OS}" in
+    local libdir
+    case "${OS}" in
     centos | sles) libdir=/usr/lib64 ;;
     ubuntu) libdir=/usr/local/lib ;;
     *) return ;;
-  esac
-  pushd ${GREENPLUM_INSTALL_DIR}
+    esac
+    pushd ${GREENPLUM_INSTALL_DIR}
     cp -d ${libdir}/libquicklz.so* lib
-  popd
+    popd
 }
 
 function include_libuv() {
-  local includedir=/usr/include
-  local libdir
-  case "${TARGET_OS}" in
+    local includedir=/usr/include
+    local libdir
+    case "${OS}" in
     centos | sles) libdir=/usr/lib64 ;;
     ubuntu) libdir=/usr/lib/x86_64-linux-gnu ;;
     *) return ;;
-  esac
-  pushd ${GREENPLUM_INSTALL_DIR}
+    esac
+    pushd ${GREENPLUM_INSTALL_DIR}
     # need to include both uv.h and uv/*.h
     cp -a ${includedir}/uv* include
     cp -a ${libdir}/libuv.so* lib
-  popd
+    popd
 }
 
 function export_gpdb() {
-  TARBALL="${GPDB_ARTIFACTS_DIR}/${GPDB_BIN_FILENAME}"
-  local server_version
-  server_version="$("${GPDB_SRC_PATH}"/getversion --short)"
+    TARBALL="${GPDB_ARTIFACTS_DIR}/${GPDB_BIN_FILENAME}"
+    local server_version
+    server_version="$("${GPDB_SRC_PATH}"/getversion --short)"
 
-  local server_build
-  server_build="${GPDB_ARTIFACTS_DIR}/server-build-${server_version}-${BLD_ARCH}${RC_BUILD_TYPE_GCS}.tar.gz"
+    local server_build
+    server_build="${GPDB_ARTIFACTS_DIR}/server-build-${server_version}-${BLD_ARCH}${RC_BUILD_TYPE_GCS}.tar.gz"
 
-  pushd ${GREENPLUM_INSTALL_DIR}
+    pushd ${GREENPLUM_INSTALL_DIR}
     source greenplum_path.sh
     python3 -m compileall -q -x test .
     chmod -R 755 .
     tar -czf "${TARBALL}" ./*
-  popd
+    popd
 
-  cp "${TARBALL}" "${server_build}"
+    cp "${TARBALL}" "${server_build}"
 }
 
 function export_gpdb_extensions() {
-  pushd ${GPDB_SRC_PATH}/gpAux
-    if ls greenplum-*zip* 1>/dev/null 2>&1; then
-      chmod 755 greenplum-*zip*
-      cp greenplum-*zip* "${GPDB_ARTIFACTS_DIR}/"
-    fi
-  popd
-}
-
-function export_gpdb_win32_ccl() {
     pushd ${GPDB_SRC_PATH}/gpAux
-    if [ -f "$(find . -maxdepth 1 -name 'greenplum-*.msi' -print -quit)" ] ; then
-        cp greenplum-*.msi "${GPDB_ARTIFACTS_DIR}/"
+    if ls greenplum-*zip* 1>/dev/null 2>&1; then
+        chmod 755 greenplum-*zip*
+        cp greenplum-*zip* "${GPDB_ARTIFACTS_DIR}/"
     fi
     popd
 }
 
 function export_gpdb_clients() {
-  TARBALL="${GPDB_ARTIFACTS_DIR}/${GPDB_CL_FILENAME}"
-  pushd ${GREENPLUM_CL_INSTALL_DIR}
+    TARBALL="${GPDB_ARTIFACTS_DIR}/${GPDB_CL_FILENAME}"
+    pushd ${GREENPLUM_CL_INSTALL_DIR}
     source ./greenplum_clients_path.sh
     mkdir -p bin/ext/gppylib
     cp ${GREENPLUM_INSTALL_DIR}/lib/python/gppylib/__init__.py ./bin/ext/gppylib
-    cp  ${GREENPLUM_INSTALL_DIR}/lib/python/gppylib/gpversion.py ./bin/ext/gppylib
+    cp ${GREENPLUM_INSTALL_DIR}/lib/python/gppylib/gpversion.py ./bin/ext/gppylib
     # GPHOME_LOADERS and greenplum_loaders_path.sh are still requried by some users
     # So link greenplum_loaders_path.sh to greenplum_clients_path.sh for compatible
     ln -sf greenplum_clients_path.sh greenplum_loaders_path.sh
     chmod -R 755 .
     tar -czf "${TARBALL}" ./*
-  popd
+    popd
 }
 
-function build_xerces()
-{
-    OUTPUT_DIR="gpdb_src/gpAux/ext/${BLD_ARCH}"
+function build_xerces() {
+    OUTPUT_DIR="${GPDB_EXT_PATH}"
     mkdir -p xerces_patch/concourse
     cp -r gpdb_src/src/backend/gporca/concourse/xerces-c xerces_patch/concourse
-    /usr/bin/python xerces_patch/concourse/xerces-c/build_xerces.py --output_dir=${OUTPUT_DIR}
+    /usr/bin/python xerces_patch/concourse/xerces-c/build_xerces.py --output_dir="${OUTPUT_DIR}"
     rm -rf build
 }
 
 function _main() {
 
-  ## Add CCache Support (?)
-  add_ccache_support "${TARGET_OS}"
+    prep_env
 
-  mkdir gpdb_src/gpAux/ext
+    ## Add CCache Support (?)
+    add_ccache_support "${OS}"
 
-  case "${TARGET_OS}" in
-    centos|ubuntu|sles)
-      prep_env
-      build_xerces
-      install_deps
-      ;;
-    win32)
-        export BLD_ARCH=win32
-        ;;
-    *)
-        echo "only centos, ubuntu, sles and win32 are supported TARGET_OS'es"
-        false
-        ;;
-  esac
+    build_xerces
+    generate_build_number
+    build_gpdb "${BLD_TARGET_OPTION[@]}"
+    git_info
 
-  generate_build_number
+    if [[ -z "${SKIP_UNITTESTS}" ]]; then
+        unittest_check_gpdb
+    fi
 
-  # By default, only GPDB Server binary is build.
-  # Use BLD_TARGETS flag with appropriate value string to generate client, loaders
-  # connectors binaries
-  if [ -n "${BLD_TARGETS}" ]; then
-    BLD_TARGET_OPTION=("BLD_TARGETS=\"$BLD_TARGETS\"")
-  else
-    BLD_TARGET_OPTION=("")
-  fi
+    include_zstd
+    include_quicklz
+    include_libuv
 
-  export CONFIGURE_FLAGS=${CONFIGURE_FLAGS}
+    export_gpdb
+    export_gpdb_extensions
 
-  build_gpdb "${BLD_TARGET_OPTION[@]}"
-  git_info
+    if echo "${BLD_TARGETS}" | grep -qwi "clients"; then
+        export_gpdb_clients
+    fi
 
-  if [[ "${TARGET_OS}" != "win32" ]] && [[ -z "${SKIP_UNITTESTS}" ]]; then
-      # Don't unit test when cross compiling. Tests don't build because they
-      # require `./configure --with-zlib`.
-      unittest_check_gpdb
-  fi
-  include_zstd
-  include_quicklz
-  include_libuv
-  export_gpdb
-  export_gpdb_extensions
-  export_gpdb_win32_ccl
-
-  if echo "${BLD_TARGETS}" | grep -qwi "clients"
-  then
-      export_gpdb_clients
-  fi
-
-  ## Display CCache Stats
-  display_ccache_stats
+    ## Display CCache Stats
+    display_ccache_stats
 }
 
 _main "$@"

concourse/pipelines/gpdb_master-generated.yml
@@ -12,7 +12,7 @@
 ## file (example: templates/gpdb-tpl.yml) and regenerate the pipeline
 ## using appropriate tool (example: gen_pipeline.py -t prod).
 ## ----------------------------------------------------------------------
-## Generated by gen_pipeline.py at: 2021-04-08 15:20:18.615552
+## Generated by gen_pipeline.py at: 2021-04-17 14:43:30.563248
 ## Template file: gpdb-tpl.yml
 ## OS Types: ['centos6', 'centos7', 'ubuntu18.04', 'win']
 ## Test Sections: ['ICW', 'Replication', 'ResourceGroups', 'Interconnect', 'CLI', 'UD', 'Extensions']
@@ -418,27 +418,6 @@ resources:
     - gpdb-doc/*
     - README*
 
-- name: libquicklz-centos7
-  type: gcs
-  source:
-    bucket: ((gcs-bucket))
-    json_key: ((concourse-gcs-resources-service-account-key))
-    regexp: gp-internal-artifacts/centos7/libquicklz-(1\.5\.0-.*)-1.el7.x86_64.rpm
-
-- name: libquicklz-devel-centos7
-  type: gcs
-  source:
-    bucket: ((gcs-bucket))
-    json_key: ((concourse-gcs-resources-service-account-key))
-    regexp: gp-internal-artifacts/centos7/libquicklz-devel-(1\.5\.0-.*)-1.el7.x86_64.rpm
-
-- name: libquicklz-ubuntu18.04
-  type: gcs
-  source:
-    bucket: ((gcs-bucket))
-    json_key: ((concourse-gcs-resources-service-account-key))
-    regexp: gp-internal-artifacts/ubuntu18.04/libquicklz-dev_(1\.5\.0-.*)-1_amd64.deb
-
 - name: gpdb7-centos7-build
   type: registry-image
   source:
@@ -635,17 +614,11 @@ jobs:
           - check_format
         trigger: ((gpdb_src-trigger-flag))
       - get: gpdb7-centos7-build
-      - get: libquicklz-installer
-        resource: libquicklz-centos7
-      - get: libquicklz-devel-installer
-        resource: libquicklz-devel-centos7
   - task: compile_gpdb
     image: gpdb7-centos7-build
     file: gpdb_src/concourse/tasks/compile_gpdb.yml
     params:
       CONFIGURE_FLAGS: ((configure_flags_with_extensions))
-      TARGET_OS: centos
-      TARGET_OS_VERSION: 7
       BLD_TARGETS: "clients"
       RC_BUILD_TYPE_GCS: ((rc-build-type-gcs))
       USE_CCACHE: ((use_ccache))
@@ -656,7 +629,7 @@ jobs:
           file: gpdb_artifacts/bin_gpdb.tar.gz
       - put: bin_gpdb_clients_centos7
         params:
-          file: gpdb_artifacts/gpdb-clients-centos7.tar.gz
+          file: gpdb_artifacts/bin_gpdb_clients.tar.gz
 
 - name: compile_gpdb_ubuntu18.04
   plan:
@@ -669,15 +642,11 @@ jobs:
             - check_format
           trigger: ((gpdb_src-trigger-flag))
         - get: gpdb7-ubuntu18.04-build
-        - get: libquicklz-installer
-          resource: libquicklz-ubuntu18.04
     - task: compile_gpdb
       image: gpdb7-ubuntu18.04-build
       file: gpdb_src/concourse/tasks/compile_gpdb.yml
       params:
         CONFIGURE_FLAGS: ((configure_flags_with_extensions))
-        TARGET_OS: ubuntu
-        TARGET_OS_VERSION: "18.04"
         BLD_TARGETS: "clients"
         RC_BUILD_TYPE_GCS: ((rc-build-type-gcs))
     - in_parallel:
@@ -687,7 +656,7 @@ jobs:
             file: gpdb_artifacts/bin_gpdb.tar.gz
         - put: bin_gpdb_clients_ubuntu18.04
           params:
-            file: gpdb_artifacts/gpdb-clients-ubuntu18.04.tar.gz
+            file: gpdb_artifacts/bin_gpdb_clients.tar.gz
 
 - name: compile_gpdb_clients_windows
   serial: true

concourse/pipelines/templates/gpdb-tpl.yml
@@ -475,31 +475,6 @@ resources:
     - README*
 
 {% if "centos7" in os_types %}
-- name: libquicklz-centos7
-  type: gcs
-  source:
-    bucket: ((gcs-bucket))
-    json_key: ((concourse-gcs-resources-service-account-key))
-    regexp: gp-internal-artifacts/centos7/libquicklz-(1\.5\.0-.*)-1.el7.x86_64.rpm
-
-- name: libquicklz-devel-centos7
-  type: gcs
-  source:
-    bucket: ((gcs-bucket))
-    json_key: ((concourse-gcs-resources-service-account-key))
-    regexp: gp-internal-artifacts/centos7/libquicklz-devel-(1\.5\.0-.*)-1.el7.x86_64.rpm
-
-{% endif %}
-{% if "ubuntu18.04" in os_types %}
-- name: libquicklz-ubuntu18.04
-  type: gcs
-  source:
-    bucket: ((gcs-bucket))
-    json_key: ((concourse-gcs-resources-service-account-key))
-    regexp: gp-internal-artifacts/ubuntu18.04/libquicklz-dev_(1\.5\.0-.*)-1_amd64.deb
-
-{% endif %}
-{% if "centos7" in os_types %}
 - name: gpdb7-centos7-build
   type: registry-image
   source:
@@ -763,17 +738,11 @@ jobs:
           - check_format
         trigger: ((gpdb_src-trigger-flag))
       - get: gpdb7-centos7-build
-      - get: libquicklz-installer
-        resource: libquicklz-centos7
-      - get: libquicklz-devel-installer
-        resource: libquicklz-devel-centos7
   - task: compile_gpdb
     image: gpdb7-centos7-build
     file: gpdb_src/concourse/tasks/compile_gpdb.yml
     params:
       CONFIGURE_FLAGS: ((configure_flags_with_extensions))
-      TARGET_OS: centos
-      TARGET_OS_VERSION: 7
       BLD_TARGETS: "clients"
       RC_BUILD_TYPE_GCS: ((rc-build-type-gcs))
       USE_CCACHE: ((use_ccache))
@@ -784,7 +753,7 @@ jobs:
           file: gpdb_artifacts/bin_gpdb.tar.gz
       - put: bin_gpdb_clients_centos7
         params:
-          file: gpdb_artifacts/gpdb-clients-centos7.tar.gz
+          file: gpdb_artifacts/bin_gpdb_clients.tar.gz
 
 {% endif %}
 {% if "ubuntu18.04" in os_types %}
@@ -799,15 +768,11 @@ jobs:
             - check_format
           trigger: ((gpdb_src-trigger-flag))
         - get: gpdb7-ubuntu18.04-build
-        - get: libquicklz-installer
-          resource: libquicklz-ubuntu18.04
     - task: compile_gpdb
       image: gpdb7-ubuntu18.04-build
       file: gpdb_src/concourse/tasks/compile_gpdb.yml
       params:
         CONFIGURE_FLAGS: ((configure_flags_with_extensions))
-        TARGET_OS: ubuntu
-        TARGET_OS_VERSION: "18.04"
         BLD_TARGETS: "clients"
         RC_BUILD_TYPE_GCS: ((rc-build-type-gcs))
     - in_parallel:
@@ -817,7 +782,7 @@ jobs:
             file: gpdb_artifacts/bin_gpdb.tar.gz
         - put: bin_gpdb_clients_ubuntu18.04
           params:
-            file: gpdb_artifacts/gpdb-clients-ubuntu18.04.tar.gz
+            file: gpdb_artifacts/bin_gpdb_clients.tar.gz
 
 {% endif %}
 {% if "win" in os_types %}

concourse/scripts/common.bash
@@ -6,7 +6,42 @@
 
 function set_env() {
     export TERM=xterm-256color
-    export TIMEFORMAT=$'\e[4;33mIt took %R seconds to complete this step\e[0m';
+    export TIMEFORMAT=$'\e[4;33mIt took %R seconds to complete this step\e[0m'
+}
+
+function os_id() {
+    if [[ -f "/etc/redhat-release" ]]; then
+        echo "centos"
+    else
+        echo "$(
+            . /etc/os-release
+            echo "${ID}"
+        )"
+    fi
+}
+
+function os_version() {
+    if [[ -f "/etc/redhat-release" ]]; then
+        echo "$(sed </etc/redhat-release 's/.*release *//' | cut -d. -f1)"
+    else
+        echo "$(
+            . /etc/os-release
+            echo "${VERSION_ID}"
+        )"
+    fi
+}
+
+build_arch() {
+    local id=$(os_id)
+    local version=$(os_version)
+    # BLD_ARCH expects rhel{6,7,8}_x86_64 || photon3_x86_64 || sles12_x86_64 || ubuntu18.04_x86_64
+    case ${id} in
+    photon | sles) version=$(os_version | cut -d. -f1) ;;
+    centos) id="rhel" ;;
+    *) ;;
+    esac
+
+    echo "${id}${version}_x86_64"
 }
 
 ## ----------------------------------------------------------------------
@@ -27,31 +62,31 @@ function setup_configure_vars() {
 }
 
 function configure() {
-  pushd gpdb_src
-      # The full set of configure options which were used for building the
-      # tree must be used here as well since the toplevel Makefile depends
-      # on these options for deciding what to test. Since we don't ship
-      ./configure --prefix=/usr/local/greenplum-db-devel --disable-orca --enable-gpcloud --enable-mapreduce --enable-orafce --enable-tap-tests --with-gssapi --with-libxml --with-openssl --with-perl --with-python PYTHON=python3 PKG_CONFIG_PATH="${GPHOME}/lib/pkgconfig" ${CONFIGURE_FLAGS}
+    pushd gpdb_src
+    # The full set of configure options which were used for building the
+    # tree must be used here as well since the toplevel Makefile depends
+    # on these options for deciding what to test. Since we don't ship
+    ./configure --prefix=/usr/local/greenplum-db-devel --disable-orca --enable-gpcloud --enable-mapreduce --enable-orafce --enable-tap-tests --with-gssapi --with-libxml --with-openssl --with-perl --with-python PYTHON=python3 PKG_CONFIG_PATH="${GPHOME}/lib/pkgconfig" ${CONFIGURE_FLAGS}
 
-  popd
+    popd
 }
 
 function install_and_configure_gpdb() {
-  install_gpdb
-  setup_configure_vars
-  configure
+    install_gpdb
+    setup_configure_vars
+    configure
 }
 
 function make_cluster() {
-  source /usr/local/greenplum-db-devel/greenplum_path.sh
-  export BLDWRAP_POSTGRES_CONF_ADDONS=${BLDWRAP_POSTGRES_CONF_ADDONS}
-  export STATEMENT_MEM=250MB
-  pushd gpdb_src/gpAux/gpdemo
-  su gpadmin -c "source /usr/local/greenplum-db-devel/greenplum_path.sh; LANG=en_US.utf8 make create-demo-cluster"
+    source /usr/local/greenplum-db-devel/greenplum_path.sh
+    export BLDWRAP_POSTGRES_CONF_ADDONS=${BLDWRAP_POSTGRES_CONF_ADDONS}
+    export STATEMENT_MEM=250MB
+    pushd gpdb_src/gpAux/gpdemo
+    su gpadmin -c "source /usr/local/greenplum-db-devel/greenplum_path.sh; LANG=en_US.utf8 make create-demo-cluster"
 
-  if [[ "$MAKE_TEST_COMMAND" =~ gp_interconnect_type=proxy ]]; then
-    # generate the addresses for proxy mode
-    su gpadmin -c bash -- -e <<EOF
+    if [[ "$MAKE_TEST_COMMAND" =~ gp_interconnect_type=proxy ]]; then
+        # generate the addresses for proxy mode
+        su gpadmin -c bash -- -e <<EOF
       source /usr/local/greenplum-db-devel/greenplum_path.sh
       source $PWD/gpdemo-env.sh
 
@@ -69,13 +104,13 @@ function make_cluster() {
 
       gpstop -u
 EOF
-  fi
+    fi
 
-  popd
+    popd
 }
 
 function run_test() {
-  su gpadmin -c "bash /opt/run_test.sh $(pwd)"
+    su gpadmin -c "bash /opt/run_test.sh $(pwd)"
 }
 
 function install_python_requirements_on_single_host() {
@@ -99,9 +134,9 @@ function install_python_requirements_on_multi_host() {
 
     pip3 --retries 10 install --user -r ${requirements_txt}
     while read -r host; do
-       scp ${requirements_txt} "$host":/tmp/requirements.txt
-       ssh $host PIP_CACHE_DIR=${PIP_CACHE_DIR} pip3 --retries 10 install --user -r /tmp/requirements.txt
-    done < /tmp/hostfile_all
+        scp ${requirements_txt} "$host":/tmp/requirements.txt
+        ssh $host PIP_CACHE_DIR=${PIP_CACHE_DIR} pip3 --retries 10 install --user -r /tmp/requirements.txt
+    done </tmp/hostfile_all
 }
 
 function setup_coverage() {
@@ -114,13 +149,13 @@ function setup_coverage() {
 
     # This file will be copied into GPDB's PYTHONPATH; it sets up the coverage
     # hook for all Python source files that are executed.
-    cat > /tmp/sitecustomize.py <<SITEEOF
+    cat >/tmp/sitecustomize.py <<SITEEOF
 import coverage
 coverage.process_startup()
 SITEEOF
 
     # Set up coverage.py to handle analysis from multiple parallel processes.
-    cat > /tmp/coveragerc <<COVEOF
+    cat >/tmp/coveragerc <<COVEOF
 [run]
 branch = True
 data_file = $coverage_path/coverage
@@ -131,11 +166,11 @@ COVEOF
     while read -r host; do
         scp /tmp/sitecustomize.py "$host":/usr/local/greenplum-db-devel/lib/python
         scp /tmp/coveragerc "$host":/usr/local/greenplum-db-devel
-        ssh "$host" "mkdir -p $coverage_path" < /dev/null
+        ssh "$host" "mkdir -p $coverage_path" </dev/null
 
         # Enable coverage instrumentation after sourcing greenplum_path.
-        ssh "$host" "echo 'export COVERAGE_PROCESS_START=/usr/local/greenplum-db-devel/coveragerc' >> /usr/local/greenplum-db-devel/greenplum_path.sh" < /dev/null
-    done < /tmp/hostfile_all
+        ssh "$host" "echo 'export COVERAGE_PROCESS_START=/usr/local/greenplum-db-devel/coveragerc' >> /usr/local/greenplum-db-devel/greenplum_path.sh" </dev/null
+    done </tmp/hostfile_all
 }
 
 function tar_coverage() {
@@ -146,16 +181,16 @@ function tar_coverage() {
 
     # Uniquify the coverage files a little bit with the supplied prefix.
     pushd ./coverage/*
-        for f in *; do
-            mv "$f" "$prefix.$f"
-        done
+    for f in *; do
+        mv "$f" "$prefix.$f"
+    done
 
-        # Compress coverage files and remove the originals
-        tar --remove-files -cf "$prefix.tar" *
+    # Compress coverage files and remove the originals
+    tar --remove-files -cf "$prefix.tar" *
     popd
 }
 
-function add_ccache_support(){
+function add_ccache_support() {
 
     _TARGET_OS=$1
 
@@ -177,7 +212,7 @@ function add_ccache_support(){
     fi
 }
 
-function display_ccache_stats(){
+function display_ccache_stats() {
     if [[ "${USE_CCACHE}" = "true" ]]; then
         cat <<EOF
 
@@ -185,7 +220,7 @@ function display_ccache_stats(){
                             CCACHE STATS
 ----------------------------------------------------------------------
 
-$( ccache --show-stats)
+$(ccache --show-stats)
 
 ======================================================================
 

concourse/tasks/compile_gpdb.yml
@@ -3,9 +3,6 @@ image_resource:
   type: registry-image
 inputs:
   - name: gpdb_src
-  - name: libquicklz-installer
-  - name: libquicklz-devel-installer
-    optional: true
 outputs:
   - name: gpdb_artifacts
 run:
@@ -13,8 +10,6 @@ run:
 caches:
   - path: ccache_dir
 params:
-  TARGET_OS:
-  TARGET_OS_VERSION:
   BLD_TARGETS:
   OUTPUT_ARTIFACT_DIR: gpdb_artifacts
   CONFIGURE_FLAGS:

greenplum-db/gpdb

commit 06d0391ce735b946441cfa8083b0110820b68c39

Fix postgres --gp-version
Previous commit: ee4961fc349aad16ae64e4fa10154d7996ce0401 accidentally
changed postgres --gp-version output, which add an extra
space in `commit: XXXX`, which should be `commit:XXXX`

Co-authored-by: Ning Wu <ningw@vmware.com>
Co-authored-by: Shaoqi Bai <bshaoqi@vmware.com>

concourse/scripts/compile_gpdb.bash
@@ -38,7 +38,7 @@ function generate_build_number() {
     # Only if its git repo, add commit SHA as build number
     # BUILD_NUMBER file is used by getversion file in GPDB to append to version
     if [ -d .git ]; then
-        echo "commit: $(git rev-parse HEAD)" >BUILD_NUMBER
+        echo "commit:$(git rev-parse HEAD)" >BUILD_NUMBER
     fi
     popd
 }