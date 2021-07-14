greenplum-db/gpdb

commit 6ae9179dca7e408727ec0cf8d92b77b4a90701a9

Correct locus type for gather motion node for replicated table (#12103)
Set correctlocus type for gather motion since if we operate(join, set
operation, etc.) replicated table with catalog table, we may scan the
replicated table on entry DB, but entry DB does not contain data for
replicated table.
The fix logic is to set the correct locus type for the motion node,
so when we try to optimize out motion node in apply_motion_mutator,
it shouldn't remove it.
This fixed the issue: https://github.com/greenplum-db/gpdb/issues/11945

src/backend/optimizer/plan/planagg.c
@@ -553,6 +553,7 @@ make_agg_subplan(PlannerInfo *root, MinMaxAggInfo *mminfo)
 	PlannerInfo *subroot = mminfo->subroot;
 	Query	   *subparse = subroot->parse;
 	Plan	   *plan;
+	bool        needGather = false;
 
 	/*
 	 * Generate the plan for the subquery. We already have a Path, but we have
@@ -567,13 +568,15 @@ make_agg_subplan(PlannerInfo *root, MinMaxAggInfo *mminfo)
 	 */
 	plan = plan_pushdown_tlist(root, plan, copyObject(subparse->targetList));
 
-	if (plan->flow->flotype == FLOW_SINGLETON)
+	/* check whether to gather subplan */
+	if (plan->flow->flotype == FLOW_PARTITIONED ||
+		(plan->flow->flotype == FLOW_SINGLETON &&
+		 plan->flow->locustype == CdbLocusType_SegmentGeneral))
+		needGather = true;
+
+	if (needGather)
 	{
-		/* ok */
-	}
-	else if (plan->flow->flotype == FLOW_PARTITIONED)
-	{
-		plan = (Plan *) make_motion_gather(subroot, plan, subroot->sort_pathkeys);
+		plan = (Plan *) make_motion_gather(subroot, plan, subroot->sort_pathkeys, CdbLocusType_SingleQE);
 	}
 	else
 		elog(ERROR, "MIN/MAX subplan has unexpected flowtype: %d", plan->flow->type);

src/backend/cdb/cdbmutate.c
@@ -99,7 +99,7 @@ static void assignMotionID(Node *newnode, ApplyMotionState *context, Node *oldno
 static void add_slice_to_motion(Motion *motion,
 					MotionType motionType,
 					List *hashExprs, List *hashOpfamilies, int numsegments,
-					bool isBroadcast);
+					bool isBroadcast, CdbLocusType targetLocus);
 
 static Node *apply_motion_mutator(Node *node, ApplyMotionState *context);
 
@@ -847,7 +847,9 @@ apply_motion_mutator(Node *node, ApplyMotionState *context)
 				context->sliceDepth == 0)
 				flow->segindex = -1;
 
-			newnode = (Node *) make_union_motion(plan, true, flow->numsegments);
+			newnode = (Node *) make_union_motion(plan, true, flow->numsegments,
+												 flow->segindex == -1 ?
+													CdbLocusType_Entry : CdbLocusType_SingleQE);
 			break;
 
 		case MOVEMENT_BROADCAST:
@@ -860,8 +862,7 @@ apply_motion_mutator(Node *node, ApplyMotionState *context)
 												  flow->hashExprs,
 												  flow->hashOpfamilies,
 												  true	/* useExecutorVarFormat */,
-												  flow->numsegments
-				);
+												  flow->numsegments);
 			break;
 
 		case MOVEMENT_EXPLICIT:
@@ -980,7 +981,7 @@ static void
 add_slice_to_motion(Motion *motion,
 					MotionType motionType,
 					List *hashExprs, List *hashOpfamilies, int numsegments,
-					bool isBroadcast)
+					bool isBroadcast, CdbLocusType targetLocus)
 {
 	Oid		   *hashFuncs;
 	ListCell   *expr_cell;
@@ -1025,6 +1026,8 @@ add_slice_to_motion(Motion *motion,
 
 			break;
 		case MOTIONTYPE_FIXED:
+			Assert(targetLocus == CdbLocusType_Null || targetLocus == CdbLocusType_Entry
+				   || targetLocus == CdbLocusType_SingleQE);
 			if (motion->isBroadcast)
 			{
 				/* broadcast */
@@ -1036,10 +1039,13 @@ add_slice_to_motion(Motion *motion,
 			{
 				/* Focus motion */
 				motion->plan.flow = makeFlow(FLOW_SINGLETON, numsegments);
-				motion->plan.flow->locustype = (motion->plan.flow->segindex < 0) ?
-					CdbLocusType_Entry :
-					CdbLocusType_SingleQE;
-
+				if (targetLocus == CdbLocusType_Entry)
+				{
+					motion->plan.flow->locustype = CdbLocusType_Entry;
+					motion->plan.flow->segindex = -1;
+				}
+				else
+					motion->plan.flow->locustype = CdbLocusType_SingleQE;
 			}
 
 			break;
@@ -1062,14 +1068,15 @@ add_slice_to_motion(Motion *motion,
 }
 
 Motion *
-make_union_motion(Plan *lefttree, bool useExecutorVarFormat, int numsegments)
+make_union_motion(Plan *lefttree, bool useExecutorVarFormat, int numsegments,
+				  CdbLocusType targetLocus)
 {
 	Motion	   *motion;
 
 	motion = make_motion(NULL, lefttree,
 						 0, NULL, NULL, NULL, NULL, /* no ordering */
 						 useExecutorVarFormat);
-	add_slice_to_motion(motion, MOTIONTYPE_FIXED, NIL, NIL, numsegments, false);
+	add_slice_to_motion(motion, MOTIONTYPE_FIXED, NIL, NIL, numsegments, false, targetLocus);
 	return motion;
 }
 
@@ -1077,14 +1084,15 @@ Motion *
 make_sorted_union_motion(PlannerInfo *root, Plan *lefttree, int numSortCols,
 						 AttrNumber *sortColIdx, Oid *sortOperators,
 						 Oid *collations, bool *nullsFirst,
-						 bool useExecutorVarFormat, int numsegments)
+						 bool useExecutorVarFormat, int numsegments,
+						 CdbLocusType targetLocus)
 {
 	Motion	   *motion;
 
 	motion = make_motion(root, lefttree,
 						 numSortCols, sortColIdx, sortOperators, collations, nullsFirst,
 						 useExecutorVarFormat);
-	add_slice_to_motion(motion, MOTIONTYPE_FIXED, NIL, NIL, numsegments, false);
+	add_slice_to_motion(motion, MOTIONTYPE_FIXED, NIL, NIL, numsegments, false, targetLocus);
 	return motion;
 }
 
@@ -1104,7 +1112,7 @@ make_hashed_motion(Plan *lefttree,
 						 useExecutorVarFormat);
 	add_slice_to_motion(motion, MOTIONTYPE_HASH,
 						hashExprs, hashOpfamilies, numsegments,
-						false);
+						false, CdbLocusType_Null);
 	return motion;
 }
 
@@ -1120,7 +1128,7 @@ make_broadcast_motion(Plan *lefttree, bool useExecutorVarFormat,
 
 	add_slice_to_motion(motion, MOTIONTYPE_FIXED,
 						NIL, NIL, numsegments,
-						true);
+						true, CdbLocusType_Null);
 	return motion;
 }
 
@@ -1146,7 +1154,7 @@ make_explicit_motion(Plan *lefttree, AttrNumber segidColIdx, bool useExecutorVar
 
 	add_slice_to_motion(motion, MOTIONTYPE_EXPLICIT,
 						NIL, NIL, numsegments,
-						false);
+						false, CdbLocusType_Null);
 	return motion;
 }
 

src/backend/cdb/cdbsetop.c
@@ -296,7 +296,7 @@ copyFlow(Flow *model_flow, bool withExprs, bool withSort)
 Motion *
 make_motion_gather_to_QD(PlannerInfo *root, Plan *subplan, List *sortPathKeys)
 {
-	return make_motion_gather(root, subplan, sortPathKeys);
+	return make_motion_gather(root, subplan, sortPathKeys, CdbLocusType_Entry);
 }
 
 /*
@@ -308,7 +308,7 @@ make_motion_gather_to_QD(PlannerInfo *root, Plan *subplan, List *sortPathKeys)
 Motion *
 make_motion_gather_to_QE(PlannerInfo *root, Plan *subplan, List *sortPathKeys)
 {
-	return make_motion_gather(root, subplan, sortPathKeys);
+	return make_motion_gather(root, subplan, sortPathKeys, CdbLocusType_SingleQE);
 }
 
 /*
@@ -318,7 +318,7 @@ make_motion_gather_to_QE(PlannerInfo *root, Plan *subplan, List *sortPathKeys)
  *      subplan.
  */
 Motion *
-make_motion_gather(PlannerInfo *root, Plan *subplan, List *sortPathKeys)
+make_motion_gather(PlannerInfo *root, Plan *subplan, List *sortPathKeys, CdbLocusType targetLocus)
 {
 	Motion	   *motion;
 
@@ -350,14 +350,15 @@ make_motion_gather(PlannerInfo *root, Plan *subplan, List *sortPathKeys)
 										  sort->sortOperators,
 										  sort->collations,sort->nullsFirst,
 										  false,
-										  subplan->flow->numsegments);
+										  subplan->flow->numsegments,
+										  targetLocus);
 
 		/* throw away the Sort */
 		pfree(sort);
 	}
 	else
 	{
-		motion = make_union_motion(subplan, false, subplan->flow->numsegments);
+		motion = make_union_motion(subplan, false, subplan->flow->numsegments, targetLocus);
 	}
 
 	return motion;
@@ -415,7 +416,7 @@ make_motion_hash_all_targets(PlannerInfo *root, Plan *subplan)
 		 * produce a different plan, with Sorts in the segments, and an
 		 * order-preserving gather on the top.)
 		 */
-		return make_motion_gather(root, subplan, NIL);
+		return make_motion_gather(root, subplan, NIL, CdbLocusType_SingleQE);
 	}
 }
 

src/backend/optimizer/path/allpaths.c
@@ -1691,7 +1691,7 @@ set_subquery_pathlist(PlannerInfo *root, RelOptInfo *rel,
 		(contain_volatile_functions((Node *) rel->subplan->targetlist) ||
 		 contain_volatile_functions(subquery->havingQual)))
 	{
-		rel->subplan = (Plan *) make_motion_gather(subroot, rel->subplan, NIL);
+		rel->subplan = (Plan *) make_motion_gather(subroot, rel->subplan, NIL, CdbLocusType_SingleQE);
 	}
 
 	rel->subroot = subroot;

src/backend/optimizer/plan/createplan.c
@@ -7205,18 +7205,18 @@ cdbpathtoplan_create_motion_plan(PlannerInfo *root,
 				 */
 				subplan = prep;
 				motion = make_sorted_union_motion(root, subplan, numSortCols, sortColIdx, sortOperators, collations,
-												  nullsFirst, false, numsegments);
+												  nullsFirst, false, numsegments, path->path.locus.locustype);
 			}
 			else
 			{
 				/* Degenerate ordering... build unordered Union Receive */
-				motion = make_union_motion(subplan, false, numsegments);
+				motion = make_union_motion(subplan, false, numsegments, path->path.locus.locustype);
 			}
 		}
 
 		/* Unordered Union Receive */
 		else
-			motion = make_union_motion(subplan, false, numsegments);
+			motion = make_union_motion(subplan, false, numsegments, path->path.locus.locustype);
 	}
 
 	/* Send all of the tuples to all of the QEs in gang above... */

src/backend/optimizer/plan/planner.c
@@ -2981,7 +2981,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 				}
 				else
 				{
-					result_plan = (Plan *) make_motion_gather(root, result_plan, current_pathkeys);
+					result_plan = (Plan *) make_motion_gather(root, result_plan, current_pathkeys, CdbLocusType_SingleQE);
 				}
 				result_plan->total_cost += motion_cost_per_row * result_plan->plan_rows;
 			}
@@ -3070,7 +3070,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 				result_plan = (Plan *) make_unique(result_plan, parse->distinctClause);
 				result_plan->flow = pull_up_Flow(result_plan, result_plan->lefttree);
 
-				result_plan = (Plan *) make_motion_gather(root, result_plan, current_pathkeys);
+				result_plan = (Plan *) make_motion_gather(root, result_plan, current_pathkeys, CdbLocusType_SingleQE);
 			}
 
 			result_plan = (Plan *) make_unique(result_plan,
@@ -3109,7 +3109,7 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 			 * handling below.
 			 */
 			current_pathkeys = root->sort_pathkeys;
-			result_plan = (Plan *) make_motion_gather(root, result_plan, current_pathkeys);
+			result_plan = (Plan *) make_motion_gather(root, result_plan, current_pathkeys, CdbLocusType_SingleQE);
 		}
 	}
 

src/backend/optimizer/prep/prepunion.c
@@ -474,7 +474,7 @@ generate_recursion_plan(SetOperationStmt *setOp, PlannerInfo *root,
 			/* do nothing, will set flow correct at the end of the function */
 		}
 		else if (lplan->flow->locustype == CdbLocusType_SegmentGeneral)
-			lplan = (Plan *) make_motion_gather(root, lplan, NIL);
+			lplan = (Plan *) make_motion_gather(root, lplan, NIL, rplan->flow->locustype);
 		else
 		{
 			elog(ERROR,

src/include/cdb/cdbmutate.h
@@ -23,9 +23,20 @@
 
 extern Plan *apply_motion(struct PlannerInfo *root, Plan *plan, Query *query);
 
-extern Motion *make_union_motion(Plan *lefttree, bool useExecutorVarFormat, int numsegments);
-extern Motion *make_sorted_union_motion(PlannerInfo *root, Plan *lefttree, int numSortCols, AttrNumber *sortColIdx, Oid *sortOperators,
-										Oid *collations, bool *nullsFirst, bool useExecutorVarFormat, int numsegments);
+extern Motion *make_union_motion(Plan *lefttree,
+								 bool useExecutorVarFormat,
+								 int numsegments,
+								 CdbLocusType targetLocus);
+extern Motion *make_sorted_union_motion(PlannerInfo *root,
+										Plan *lefttree,
+										int numSortCols,
+										AttrNumber *sortColIdx,
+										Oid *sortOperators,
+										Oid *collations,
+										bool *nullsFirst,
+										bool useExecutorVarFormat,
+										int numsegments,
+										CdbLocusType targetLocus);
 extern Motion *make_hashed_motion(Plan *lefttree,
 								  List *hashExpr,
 								  List *hashOpfamilies,

commit c5829966472723626e443b420e6541325a241dc7

Agg on entryDB shouldn't raise error
This is a regression caused by commit: 6ae9179dca7.
The refactor forget to consider agg on entryDB.
So fix the issue and add a test case to cover it.

src/backend/optimizer/plan/planagg.c
@@ -578,6 +578,10 @@ make_agg_subplan(PlannerInfo *root, MinMaxAggInfo *mminfo)
 	{
 		plan = (Plan *) make_motion_gather(subroot, plan, subroot->sort_pathkeys, CdbLocusType_SingleQE);
 	}
+	else if (plan->flow->flotype == FLOW_SINGLETON && plan->flow->locustype == CdbLocusType_Entry)
+	{
+		/* No need to gather if the plan is executed on entryDB. */
+	}
 	else
 		elog(ERROR, "MIN/MAX subplan has unexpected flowtype: %d", plan->flow->type);
 

src/test/regress/expected/gp_aggregates.out
@@ -62,6 +62,13 @@ SELECT array_agg(a order by a) over (order by a) FROM aggtest; -- window derived
 ERROR:  aggregate ORDER BY is not implemented for window functions
 LINE 1: SELECT array_agg(a order by a) over (order by a) FROM aggtes...
                ^
+-- agg on entryDB, this used to raise error "MIN/MAX subplan has unexpected flowtype"
+SELECT min(attnum) min_attnum FROM pg_catalog.pg_attribute WHERE attrelid = 'pg_proc'::regclass AND attnum > 0;
+ min_attnum 
+------------
+          1
+(1 row)
+
 -- check for mpp-2687
 CREATE TEMPORARY TABLE mpp2687t (
     dk text,

src/test/regress/sql/gp_aggregates.sql
@@ -18,6 +18,9 @@ SELECT lag(a order by a) over (order by a) FROM aggtest;  -- window function
 SELECT count(a order by a) over (order by a) FROM aggtest;  -- window derived aggregate
 SELECT array_agg(a order by a) over (order by a) FROM aggtest; -- window derived ordered aggregate
 
+-- agg on entryDB, this used to raise error "MIN/MAX subplan has unexpected flowtype"
+SELECT min(attnum) min_attnum FROM pg_catalog.pg_attribute WHERE attrelid = 'pg_proc'::regclass AND attnum > 0;
+
 -- check for mpp-2687
 CREATE TEMPORARY TABLE mpp2687t (
     dk text,