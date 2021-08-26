9357a1fb8ca46666a9d86b153ec2fd136d7a396f
Hao Wu <gfphoenix78@gmail.com>
2021-08-26 12:40:43 +0000

    use libuv file event to monitor create file

--- src/test/regress/file_monitor.c
+++ src/test/regress/file_monitor.c
@@ -1,68 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <unistd.h>
+#include <uv.h>
 
-#ifdef __linux__
-#include <sys/inotify.h>
-
-static void sig_handler(int signo)
+static void fs_cb(uv_fs_event_t *handle, const char *filename, int events, int status)
 {
-	fflush(stdout);
-  exit(0);
-}
-/* av[1]: dirpath */
-int main(int ac, const char *av[])
-{
-	char buffer[16 << 10];
-	const char *path;
-	int fd, wd, n;
+  if (status != 0 || (events & UV_RENAME) == 0)
+    return;
 
-	path = av[1];
-	if (ac < 2 || !path || path[0] == '\0') {
-		fprintf(stderr, "Need argument: <location>\n");
-		exit(1);
-	}
-
-	fd = inotify_init();
-	if (fd < 0) {
-		fprintf(stderr, "inotify_init1 failed: %m\n");
-		exit(1);
-	}
-	wd = inotify_add_watch(fd, path, IN_CREATE);
-	if (wd < 0) {
-		fprintf(stderr, "inotify_add_watch failed: %m\n");
-		exit(1);
-	}
-	signal(SIGINT, sig_handler);
-	signal(SIGTERM, sig_handler);
-	while ((n = read(fd, buffer, sizeof(buffer))) > 0) {
-		const struct inotify_event *ev;
-		int count = 0;
-		for (char *ptr = &buffer[0];
-				ptr < buffer + n;
-				ptr += sizeof(*ev) + ev->len) {
-			ev = (const struct inotify_event *)ptr;
-			if (ev->wd != wd) {
-				fprintf(stderr, "Unexpected wd = %d, expected wd = %d\n", ev->wd, wd);
-				continue;
-			}
-			printf("%s/%s\n", path, ev->name);
-			count++;
-		}
-		if (count > 0)
-			fflush(stdout);
-	}
-	close(fd);
-
-	return 0;
+  printf("%s/%s\n", (char*)handle->data, filename);
 }
 
-#else
+static void signal_cb(uv_signal_t *handle, int signo)
+{
+  uv_signal_stop(handle);
+  uv_stop(uv_default_loop());
+  fflush(stdout);
+}
 
 int main(int ac, const char *av[])
 {
-  fprintf(stderr, "No Support for non-Linux\n");
-  return -1;
+  uv_loop_t *loop = uv_default_loop();
+  uv_fs_event_t fse[32];
+  uv_signal_t sig[2];
+  int i, n;
+  unsigned flags = 0;
+  n = ac - 1;
+  if (n > 32) {
+    fprintf(stderr, "Too many directories to monitor\n");
+    exit(1);
+  }
+  for (i = 0; i < n; i++) {
+    uv_fs_event_init(loop, &fse[i]);
+    fse[i].data = (void*)av[i+1];
+    uv_fs_event_start(&fse[i], fs_cb, av[i+1], flags);
+  }
+  uv_signal_init(loop, &sig[0]);
+  uv_signal_init(loop, &sig[1]);
+  uv_signal_start(&sig[0], signal_cb, SIGINT);
+  uv_signal_start(&sig[1], signal_cb, SIGTERM);
+
+  uv_run(loop, UV_RUN_DEFAULT);
+
+  fflush(stdout);
+  return 0;
+
 }
-#endif
