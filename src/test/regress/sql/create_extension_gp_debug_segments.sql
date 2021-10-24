154ddae91775c92983fd0cc0251c6a515772296a
ZhaoRui <zhaoru@vmware.com>
2021-10-24 05:35:55 -0700

    Fix cannot drop table after creating extension failure with normal user.
    Issue: https://github.com/greenplum-db/gpdb/issues/12713
    
    The problem is first found in gpload. And after multiple retries one reproduce prodedure was found:
    1. First use a normal user (not super user) to create extension and hit an error
    with "permission denied"
    2. Then create a table within the same psql connection.
    3. Then quit the psql connection and start a new one and try to drop the table
    created in step 2.
    ```
    gpadmin=# set role test;
    SET
    gpadmin=> create extension gp_inject_fault ;
    ERROR:  permission denied to create extension "gp_inject_fault"
    HINT:  Must be superuser to create this extension.
    gpadmin=> create table t(a int);
    CREATE TABLE
    gpadmin=> quit;
    gpadmin@zlv-ubuntu:~/gpdb$ psql
    psql (12beta2)
    Type "help" for help.
    
    gpadmin=# set role test;
    SET
    gpadmin=> drop table t;
    ERROR:  cache lookup failed for extension 16415 (objectaddress.c:3570)  (seg1 127.0.1.1:7003 pid=16991) (objectaddress.c:3570)
    ```
    The reason for this is that the user acl judgement code in "execute_extension_script" fails to catch the error throw
    inside the user acl judgement which makes the QD fails to call CdbDispatchUtilityStatement to QEs to set the "creating_extension" to false:
    (a. Before the QD runs at the user acl judgement, the QD has call CdbDispatchUtilityStatement to start createextension.
    b. Then QE executes the "CreateExtension" function in "CREATE_EXTENSION_BEGIN" switch branch, and set the "creating_extension" to true in QEs.
    c. If everything goes fine, the QD will call CdbDispatchUtilityStatement at end of the function "execute_extension_script"
    d. Then QEs will execute "CREATE_EXTENSION_BEGIN" again and set the  "creating_extension" to false.)
    But in this issue, the value of "creating_extension" in QD is false, and in QEs to be true.
    
    In later session of creating table will call the function "recordDependencyOnCurrentExtension" and will add
    dependance of the failed extension which will make the catalogs in QEs and QD incompatible.
    
    The fix is quite straight forward:
    Move the user acl judgement code into the try{...}catch{} block.
    
    Take a look at issue:12713 to see more details.

--- src/test/regress/sql/create_extension_gp_debug_segments.sql
+++ src/test/regress/sql/create_extension_gp_debug_segments.sql
@@ -13,3 +13,25 @@ create extension gp_debug_numsegments;
 
 select gp_inject_fault('create_function_fail', 'reset', 2);
 
+--
+-- Tests for cannot drop table after create extension with normal user
+-- The issue: https://github.com/greenplum-db/gpdb/issues/12713
+--
+--1. use normal user to create extension
+--2. create a table
+--3. quit psql
+--4. reconnect
+--5. try to drop the table
+
+--start_ignore
+drop table if exists t_12713;
+drop user if exists normal_user_12713;
+create user normal_user_12713;
+--end_ignore
+
+set role normal_user_12713;
+create extension postgres_fdw;
+create table t_12713(name text);
+\c
+drop table t_12713;
+drop user normal_user_12713;
