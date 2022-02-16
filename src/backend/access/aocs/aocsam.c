b2744328cf4db1ee26db1313662dd4c9da1e6862
Andrey M. Borodin <x4mmm@flight.local>
2022-02-16 11:01:37 +0100

    More detailed error message

--- src/backend/access/aocs/aocsam.c
+++ src/backend/access/aocs/aocsam.c
@@ -149,8 +149,10 @@ open_ds_write(Relation rel, DatumStreamWrite **ds, TupleDesc relationTupleDesc,
 		 * column of a column oriented table.  Note: checksum is a table level
 		 * attribute.
 		 */
-		if (opts[i] == NULL || opts[i]->blocksize == 0)
-			elog(ERROR, "could not find blocksize option for AOCO column in pg_attribute_encoding");
+		if (opts[attno] == NULL || opts[attno]->blocksize == 0)
+			elog(ERROR, "No relation attribute options for '%s', column #%d  in pg_attribute_encoding",
+							RelationGetRelationName(rel),
+							attno + 1);
 		ct = opts[i]->compresstype;
 		clvl = opts[i]->compresslevel;
 		blksz = opts[i]->blocksize;
@@ -202,7 +204,10 @@ open_ds_read(Relation rel, DatumStreamRead **ds, TupleDesc relationTupleDesc,
 		 * attribute.
 		 */
 		if (opts[attno] == NULL || opts[attno]->blocksize == 0)
-			elog(ERROR, "could not find blocksize option for AOCO column in pg_attribute_encoding");
+			elog(ERROR, "No relation attribute options for '%s', column #%d  in pg_attribute_encoding",
+							RelationGetRelationName(rel),
+							attno + 1);
+
 		ct = opts[attno]->compresstype;
 		clvl = opts[attno]->compresslevel;
 		blksz = opts[attno]->blocksize;
