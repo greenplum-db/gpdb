4bdd94999a5ee8403e124cdb262c2fb214aa1b23
maqing <maqing833@outlook.com>
2022-03-09 18:51:21 +0800

    Apply suggestions from code review
    Co-authored-by: Zhenghua Lyu <kainwen@gmail.com>

--- src/backend/commands/collationcmds.c
+++ src/backend/commands/collationcmds.c
@@ -653,6 +653,12 @@ pg_import_system_collations(PG_FUNCTION_ARGS)
 				continue;		/* ignore locales for client-only encodings */
 			if (enc == PG_SQL_ASCII)
 				continue;		/* C/POSIX are already in the catalog */
+            /*
+             * Greenplum specific behavior: this function in Greenplum can only be called after a full cluster is
+             * built, this is different from Postgres which might call this function during initdb. When reaching
+             * here, it must be in a database session, we can just ignore the collations not match current database's
+             * encoding because they cannot be used in this database.
+             */
 			if (enc != GetDatabaseEncoding())
 				continue;       /* Ignore collations incompatible with database encoding */ 
 
