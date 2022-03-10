415d6d08d4a914ad4bf46cea7d3845978b3ac1fd
Chris Hajas <chajas@vmware.com>
2022-03-10 13:45:27 -0800

    Fallback to planner for functions returning composite types
    Orca doesn't support composite types (also called "whole-row
    variables"). While we had a fallback for this if we project the
    composite type, we didn't have this logic for functions returning
    composite types, which led to wrong results.

--- src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
+++ src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
@@ -3619,6 +3619,12 @@ CTranslatorQueryToDXL::TranslateTVFToDXL(const RangeTblEntry *rte,
 	// if this is a folded function expression, generate a project over a CTG
 	if (!IsA(funcexpr, FuncExpr))
 	{
+		if (gpdb::IsCompositeType(funcexpr->funcid))
+		{
+			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
+					   GPOS_WSZ_LIT("Whole-row variable"));
+		}
+
 		CDXLNode *const_tbl_get_dxlnode = DXLDummyConstTableGet();
 
 		CDXLNode *project_list_dxlnode = GPOS_NEW(m_mp)
