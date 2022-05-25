greenplum-db/gpdb

commit cb120281af122ed9a2841945abd559162ee770c3

[6X] recalculate QE's query_mem proportionally (#13369)
This is the backport of #13160.

In the current resource group implementation, query_mem in the plan tree is calculated using
QD's system memory and the number of primary segments, not QE's own system memory and
the number of primary segments. This can result in the wrong memory being allocated at the
execution stage eventually, which can lead to various problems such as OOM, underutilization of
QE resources, etc.

Only total_memory and nsegments differ between QD and QE, so we can dispatch these two
parameters to QE, and then calculate QE's own query_mem proportionally.

At the same time, we use the GUC gp_resource_group_enable_recalculate_query_mem to let the
client decide whether to recalculate the query_mem proportionally on QE and repopulate the
operatorMemKB in the plan tree according to this value.

More information can be found at https://github.com/greenplum-db/gpdb/pull/13160.

Author: lzhenglong@vmware.com, zlyu@vmware.com

src/backend/nodes/copyfuncs.c
@@ -130,6 +130,9 @@ _copyPlannedStmt(const PlannedStmt *from)
 
 	COPY_SCALAR_FIELD(query_mem);
 
+	COPY_SCALAR_FIELD(total_memory_master);
+	COPY_SCALAR_FIELD(nsegments_master);
+
 	COPY_NODE_FIELD(intoClause);
 	COPY_NODE_FIELD(copyIntoClause);
 	COPY_NODE_FIELD(refreshClause);

src/backend/nodes/outfast.c
@@ -377,6 +377,10 @@ _outPlannedStmt(StringInfo str, PlannedStmt *node)
 	WRITE_INT_FIELD(nInitPlans);
 	WRITE_NODE_FIELD(intoPolicy);
 	WRITE_UINT64_FIELD(query_mem);
+
+	WRITE_INT_FIELD(total_memory_master);
+	WRITE_INT_FIELD(nsegments_master);
+
 	WRITE_NODE_FIELD(intoClause);
 	WRITE_NODE_FIELD(copyIntoClause);
 	WRITE_NODE_FIELD(refreshClause);

src/backend/nodes/outfuncs.c
@@ -322,6 +322,10 @@ _outPlannedStmt(StringInfo str, const PlannedStmt *node)
 	WRITE_NODE_FIELD(intoPolicy);
 
 	WRITE_UINT64_FIELD(query_mem);
+
+	WRITE_INT_FIELD(total_memory_master);
+	WRITE_INT_FIELD(nsegments_master);
+
 	WRITE_NODE_FIELD(intoClause);
 	WRITE_NODE_FIELD(copyIntoClause);
 	WRITE_NODE_FIELD(refreshClause);

src/backend/nodes/readfast.c
@@ -1379,6 +1379,10 @@ _readPlannedStmt(void)
 	READ_NODE_FIELD(intoPolicy);
 
 	READ_UINT64_FIELD(query_mem);
+
+	READ_INT_FIELD(total_memory_master);
+	READ_INT_FIELD(nsegments_master);
+
 	READ_NODE_FIELD(intoClause);
 	READ_NODE_FIELD(copyIntoClause);
 	READ_NODE_FIELD(refreshClause);

src/include/nodes/plannodes.h
@@ -138,6 +138,9 @@ typedef struct PlannedStmt
 	/* What is the memory reserved for this query's execution? */
 	uint64		query_mem;
 
+	int			total_memory_master;	/* GPDB: The total usable virtual memory on master node in MB */
+	int			nsegments_master;		/* GPDB: The number of primary segments on master node  */
+
 	/*
 	 * GPDB: Used to keep target information for CTAS and it is needed
 	 * to be dispatched to QEs.

src/backend/cdb/dispatcher/cdbdisp_query.c
@@ -36,6 +36,7 @@
 #include "utils/faultinjector.h"
 #include "utils/resgroup.h"
 #include "utils/resource_manager.h"
+#include "utils/resgroup-ops.h"
 #include "utils/session_state.h"
 #include "utils/typcache.h"
 #include "miscadmin.h"
@@ -230,6 +231,21 @@ CdbDispatchPlan(struct QueryDesc *queryDesc,
 	if (queryDesc->extended_query)
 	{
 		verify_shared_snapshot_ready(gp_command_count);
+	}
+
+	/* In the final stage, add the resource information needed for QE by the resource group */
+	stmt->total_memory_master = 0;
+	stmt->nsegments_master = 0;
+
+	if (IsResGroupEnabled() && gp_resource_group_enable_recalculate_query_mem &&
+		memory_spill_ratio != RESGROUP_FALLBACK_MEMORY_SPILL_RATIO)
+	{
+		/*
+		 * We enable resource group re-calculate the query_mem on QE, and we are not in
+		 * fall back mode (use statement_mem).
+		 */
+		stmt->total_memory_master = ResGroupOps_GetTotalMemory();
+		stmt->nsegments_master = ResGroupGetSegmentNum();
 	}
 
 	cdbdisp_dispatchX(queryDesc, planRequiresTxn, cancelOnError);

src/backend/executor/execMain.c
@@ -90,6 +90,7 @@
 #include "utils/workfile_mgr.h"
 #include "utils/faultinjector.h"
 #include "utils/resource_manager.h"
+#include "utils/resgroup-ops.h"
 
 #include "catalog/pg_inherits_fn.h"
 #include "catalog/pg_statistic.h"
@@ -305,9 +306,6 @@ standard_ExecutorStart(QueryDesc *queryDesc, int eflags)
 	if (query_info_collect_hook)
 		(*query_info_collect_hook)(METRICS_QUERY_START, queryDesc);
 
-	/**
-	 * Distribute memory to operators.
-	 */
 	if (Gp_role == GP_ROLE_DISPATCH)
 	{
 		if (!IsResManagerMemoryPolicyNone() &&
@@ -316,12 +314,75 @@ standard_ExecutorStart(QueryDesc *queryDesc, int eflags)
 			elog(GP_RESMANAGER_MEMORY_LOG_LEVEL, "query requested %.0fKB of memory",
 				 (double) queryDesc->plannedstmt->query_mem / 1024.0);
 		}
+	}
 
-		/**
-		 * There are some statements that do not go through the resource queue, so we cannot
-		 * put in a strong assert here. Someday, we should fix resource queues.
+	/*
+	 * Distribute memory to operators.
+	 *
+	 * There are some statements that do not go through the resource queue, so we cannot
+	 * put in a strong assert here. Someday, we should fix resource queues.
+	 */
+	if (queryDesc->plannedstmt->query_mem > 0)
+	{
+		/*
+		 * Whether we should skip operator memory assignment
+		 * - We should never skip operator memory assignment on QD.
+		 * - On QE, not skip in case of resource group enabled, and customer allow QE re-calculate query_mem,
+		 * as the GUC `gp_resource_group_enable_recalculate_query_mem` set to on.
 		 */
-		if (queryDesc->plannedstmt->query_mem > 0)
+		bool	should_skip_operator_memory_assign = true;
+
+		if (Gp_role == GP_ROLE_EXECUTE)
+		{
+			/*
+			 * If resource group is enabled, we should re-calculate query_mem on QE, because the memory
+			 * of the coordinator and segment nodes or the number of instance could be different.
+			 *
+			 * On QE, we only try to recalculate query_mem if resource group enabled. Otherwise, we will skip this
+			 * and the next operator memory assignment if resource queue enabled
+			 */
+			if (IsResGroupEnabled())
+			{
+				int 	total_memory_master = queryDesc->plannedstmt->total_memory_master;
+				int    	nsegments_master = queryDesc->plannedstmt->nsegments_master;
+
+				/*
+				 * memSpill is not in fallback mode, and we enable resource group re-calculate the query_mem on QE,
+				 * then re-calculate the query_mem and re-compute operatorMemKB using this new value
+				 */
+				if (total_memory_master != 0 && nsegments_master != 0)
+				{
+					should_skip_operator_memory_assign = false;
+
+					/* Get total system memory on the QE in MB */
+					int 	total_memory_segment = ResGroupOps_GetTotalMemory();
+					int 	nsegments_segment = ResGroupGetSegmentNum();
+					uint64	master_query_mem = queryDesc->plannedstmt->query_mem;
+
+					/*
+					 * In the resource group environment, when we calculate query_mem, we can roughly use the following
+					 * formula:
+					 *
+					 * 	query_mem = (total_memory * gp_resource_group_memory_limit * memory_limit / nsegments) * memory_spill_ratio / concurrency
+					 *
+					 * Only total_memory and nsegments could differ between QD and QE, so query_mem is proportional to
+					 * the system's available virtual memory and inversely proportional to the number of instances.
+					 */
+					queryDesc->plannedstmt->query_mem *= (total_memory_segment * 1.0 / nsegments_segment) /
+							(total_memory_master * 1.0 / nsegments_master);
+
+					elog(DEBUG1, "re-calculate query_mem, original QD's query_mem: %.0fKB, after recalculation QE's query_mem: %.0fKB",
+						 (double) master_query_mem / 1024.0  , (double) queryDesc->plannedstmt->query_mem / 1024.0);
+				}
+			}
+		}
+		else
+		{
+			/* On QD, we always traverse the plan tree and compute operatorMemKB */
+			should_skip_operator_memory_assign = false;
+		}
+
+		if (!should_skip_operator_memory_assign)
 		{
 			switch(*gp_resmanager_memory_policy)
 			{

src/backend/executor/nodeSort.c
@@ -269,6 +269,16 @@ ExecInitSort(Sort *node, EState *estate, int eflags)
 			   "initializing sort node");
 
 	/*
+	 * GPDB
+	 */
+#ifdef FAULT_INJECTOR
+	if (SIMPLE_FAULT_INJECTOR("rg_qmem_qd_qe") == FaultInjectorTypeSkip)
+	{
+		elog(NOTICE, "op_mem=%d", (int) (((Plan *) node)->operatorMemKB));
+	}
+#endif
+
+	/*
 	 * create state structure
 	 */
 	sortstate = makeNode(SortState);

commit 514c0cbf0792e87b4a6ce183fb4132e625605b1e

Fix API broken introduced by cb120281a.
Commit cb120281a introduces two fields of struct PlannedStmt and not
put them at the end of the struct. In Greenplum 6X, some extensions,
like metrics_collector will use fields of PlannedStmt, and the
extension is created in a previous version of Greenplum 6, when users
upgrade to later version of Greenplum 6, the extension's .so file is
not recompiled, however offset of fields changed.

This commit fixes the ABI broken by putting the new-added fields at
the end of the struct.

src/backend/nodes/copyfuncs.c
@@ -130,13 +130,13 @@ _copyPlannedStmt(const PlannedStmt *from)
 
 	COPY_SCALAR_FIELD(query_mem);
 
-	COPY_SCALAR_FIELD(total_memory_master);
-	COPY_SCALAR_FIELD(nsegments_master);
-
 	COPY_NODE_FIELD(intoClause);
 	COPY_NODE_FIELD(copyIntoClause);
 	COPY_NODE_FIELD(refreshClause);
 	COPY_SCALAR_FIELD(metricsQueryType);
+
+	COPY_SCALAR_FIELD(total_memory_master);
+	COPY_SCALAR_FIELD(nsegments_master);
 
 	return newnode;
 }

src/backend/nodes/outfast.c
@@ -378,13 +378,13 @@ _outPlannedStmt(StringInfo str, PlannedStmt *node)
 	WRITE_NODE_FIELD(intoPolicy);
 	WRITE_UINT64_FIELD(query_mem);
 
-	WRITE_INT_FIELD(total_memory_master);
-	WRITE_INT_FIELD(nsegments_master);
-
 	WRITE_NODE_FIELD(intoClause);
 	WRITE_NODE_FIELD(copyIntoClause);
 	WRITE_NODE_FIELD(refreshClause);
 	WRITE_INT8_FIELD(metricsQueryType);
+
+	WRITE_INT_FIELD(total_memory_master);
+	WRITE_INT_FIELD(nsegments_master);
 }
 
 static void

src/backend/nodes/outfuncs.c
@@ -323,13 +323,13 @@ _outPlannedStmt(StringInfo str, const PlannedStmt *node)
 
 	WRITE_UINT64_FIELD(query_mem);
 
-	WRITE_INT_FIELD(total_memory_master);
-	WRITE_INT_FIELD(nsegments_master);
-
 	WRITE_NODE_FIELD(intoClause);
 	WRITE_NODE_FIELD(copyIntoClause);
 	WRITE_NODE_FIELD(refreshClause);
 	WRITE_INT_FIELD(metricsQueryType);
+
+	WRITE_INT_FIELD(total_memory_master);
+	WRITE_INT_FIELD(nsegments_master);
 }
 #endif /* COMPILING_BINARY_FUNCS */
 

src/backend/nodes/readfast.c
@@ -1380,13 +1380,14 @@ _readPlannedStmt(void)
 
 	READ_UINT64_FIELD(query_mem);
 
-	READ_INT_FIELD(total_memory_master);
-	READ_INT_FIELD(nsegments_master);
-
 	READ_NODE_FIELD(intoClause);
 	READ_NODE_FIELD(copyIntoClause);
 	READ_NODE_FIELD(refreshClause);
 	READ_INT8_FIELD(metricsQueryType);
+
+	READ_INT_FIELD(total_memory_master);
+	READ_INT_FIELD(nsegments_master);
+
 	READ_DONE();
 }
 

src/include/nodes/plannodes.h
@@ -138,9 +138,6 @@ typedef struct PlannedStmt
 	/* What is the memory reserved for this query's execution? */
 	uint64		query_mem;
 
-	int			total_memory_master;	/* GPDB: The total usable virtual memory on master node in MB */
-	int			nsegments_master;		/* GPDB: The number of primary segments on master node  */
-
 	/*
 	 * GPDB: Used to keep target information for CTAS and it is needed
 	 * to be dispatched to QEs.
@@ -153,6 +150,9 @@ typedef struct PlannedStmt
  	 * GPDB: whether a query is a SPI inner query for extension usage 
  	 */
 	int8		metricsQueryType;
+
+	int			total_memory_master;	/* GPDB: The total usable virtual memory on master node in MB */
+	int			nsegments_master;		/* GPDB: The number of primary segments on master node  */
 } PlannedStmt;
 
 /*