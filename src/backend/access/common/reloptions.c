45476f8f523a7a45b7e02ac34dda6587eae35b62
QingMa <maqi@vmware.com>
2022-09-23 14:08:36 +0800

    fix memory leaks

--- src/backend/access/common/reloptions.c
+++ src/backend/access/common/reloptions.c
@@ -1239,7 +1239,7 @@ default_reloptions(Datum reloptions, bool validate, relopt_kind kind)
 
 	validate_and_refill_options(rdopts, options, numoptions, kind, validate);
 
-	pfree(options);
+	free_options_deep(options, numoptions);
 
 	return (bytea *) rdopts;
 }
@@ -1271,7 +1271,7 @@ view_reloptions(Datum reloptions, bool validate)
 	fillRelOptions((void *) vopts, sizeof(ViewOptions), options, numoptions,
 				   validate, tab, lengthof(tab));
 
-	pfree(options);
+	free_options_deep(options, numoptions);
 
 	return (bytea *) vopts;
 }
@@ -1371,8 +1371,7 @@ attribute_reloptions(Datum reloptions, bool validate)
 	fillRelOptions((void *) aopts, sizeof(AttributeOpts), options, numoptions,
 				   validate, tab, lengthof(tab));
 
-	pfree(options);
-
+	free_options_deep(options, numoptions);
 	return (bytea *) aopts;
 }
 
@@ -1402,7 +1401,7 @@ tablespace_reloptions(Datum reloptions, bool validate)
 	fillRelOptions((void *) tsopts, sizeof(TableSpaceOpts), options, numoptions,
 				   validate, tab, lengthof(tab));
 
-	pfree(options);
+	free_options_deep(options, numoptions);
 
 	return (bytea *) tsopts;
 }
