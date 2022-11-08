2c096cb4d524164382ab9f97cc494ec8bb6f748f
liuxueyang <liuxueyang457@gmail.com>
2022-11-08 10:30:28 +0800

    Keep version 1.0 on stable branch

--- contrib/uuid-ossp/uuid-ossp--1.0--1.1.sql
+++
@@ -1,4 +0,0 @@
-/* contrib/uuid-ossp/uuid-ossp--1.0--1.1.sql */
-
--- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION uuid-ossp UPDATE TO '1.1'" to load this file. \quit
