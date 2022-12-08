greenplum-db/gpdb

commit 99dc08ff24a2ca5a67a8cef7a4cb92f68562bc47

pg_dump: Define macros for GPDB Postgres versions
Two conditionals in getTableSchema were testing remote version <= 90400
for GPDB5/6 specific code, which is a bug because it omits GPDB6 (90424
or 90426) altogether. This commit fixes the issue by defining and using a macro
for the major Postgres versions for GPDB5-7. These are then used in GPDB specific version
checks. Also cleans up some now unused functions.

Also check tbinfo->partclause before trying to dereference the pointer.

Authored-by: Brent Doil <bdoil@vmware.com>

src/bin/pg_dump/pg_dumpall.c
@@ -1451,7 +1451,7 @@ dumpTablespaces(PGconn *conn)
 	 * Greenplum, and the dump format should vary depending on if the dump is
 	 * --gp-syntax or --no-gp-syntax.
 	 */
-	if (server_version <= 80323)
+	if (server_version == GPDB5_MAJOR_PGVERSION)
 	{
 		filespace_to_tablespace = true;
 		/*
@@ -1925,9 +1925,9 @@ dumpDatabaseConfig(PGconn *conn, const char *dbname)
 	}
 
 	/*
-	 * If we're upgrading from GPDB 5 or below, use the legacy hash ops.
+	 * If we're upgrading from GPDB 5, use the legacy hash ops.
 	 */
-	if (binary_upgrade && server_version < 90400)
+	if (binary_upgrade && server_version < GPDB6_MAJOR_PGVERSION)
 	{
 		makeAlterConfigCommand(conn, "gp_use_legacy_hashops=on",
 							   "DATABASE", dbname, NULL, NULL);
@@ -2422,7 +2422,7 @@ connectDatabase(const char *dbname, const char *connection_string,
 	 * our own major version.  (See also version check in pg_dump.c.)
 	 */
 	if (my_version != server_version
-		&& (server_version < 80300 ||		/* we can handle back to 8.3 */
+		&& (server_version < GPDB5_MAJOR_PGVERSION ||		/* we can handle back to 8.3 */
 			(server_version / 100) > (my_version / 100)))
 	{
 		fprintf(stderr, _("server version: %s; %s version: %s\n"),

src/bin/pg_dump/pg_backup.h
@@ -38,6 +38,9 @@
 #define oidge(x,y) ( (x) >= (y) )
 #define oidzero(x) ( (x) == 0 )
 
+#define GPDB5_MAJOR_PGVERSION 80300
+#define GPDB6_MAJOR_PGVERSION 90400
+
 typedef enum trivalue
 {
 	TRI_DEFAULT,

src/bin/pg_dump/pg_dump.c
@@ -335,48 +335,8 @@ static bool testGPbackend(Archive *fout);
 static char *nextToken(register char **stringp, register const char *delim);
 static void addDistributedBy(Archive *fout, PQExpBuffer q, TableInfo *tbinfo, int actual_atts);
 static void addDistributedByOld(Archive *fout, PQExpBuffer q, TableInfo *tbinfo, int actual_atts);
-static bool isGPDB(Archive *fout);
-static bool isGPDB6000OrLater(Archive *fout);
 
 /* END MPP ADDITION */
-
-/*
- * Check if we are talking to GPDB
- */
-static bool
-isGPDB(Archive *fout)
-{
-	static int	value = -1;		/* -1 = not known yet, 0 = no, 1 = yes */
-
-	/* Query the server on first call, and cache the result */
-	if (value == -1)
-	{
-		const char *query = "select pg_catalog.version()";
-		PGresult   *res;
-		char	   *ver;
-
-		res = ExecuteSqlQuery(fout, query, PGRES_TUPLES_OK);
-
-		ver = (PQgetvalue(res, 0, 0));
-		if (strstr(ver, "Greenplum") != NULL)
-			value = 1;
-		else
-			value = 0;
-
-		PQclear(res);
-	}
-	return (value == 1) ? true : false;
-}
-
-static bool
-isGPDB6000OrLater(Archive *fout)
-{
-	if (!isGPDB(fout))
-		return false;		/* Not Greenplum at all. */
-
-	/* GPDB 6 is based on PostgreSQL 8.4 */
-	return fout->remoteVersion >= 80400;
-}
 
 int
 main(int argc, char **argv)
@@ -811,7 +771,7 @@ main(int argc, char **argv)
 	 * We allow the server to be back to 8.3, and up to any minor release of
 	 * our own major version.  (See also version check in pg_dumpall.c.)
 	 */
-	fout->minRemoteVersion = 80300;	/* we can handle back to 8.3 */
+	fout->minRemoteVersion = GPDB5_MAJOR_PGVERSION;	/* we can handle back to 8.3 */
 	fout->maxRemoteVersion = (PG_VERSION_NUM / 100) * 100 + 99;
 
 	fout->numWorkers = numWorkers;
@@ -4736,7 +4696,7 @@ getTables(Archive *fout, int *numTables)
 	 * If there is more than 1 entry in the policy table for an
 	 * oid the scalar subquery will fail as intended.
 	 */
-	if (isGPDB6000OrLater(fout))
+	if (fout->remoteVersion > GPDB6_MAJOR_PGVERSION)
 	 		appendPQExpBufferStr(query,
 							"pg_catalog.pg_get_table_distributedby(c.oid) as distclause, ");
 	else
@@ -4824,7 +4784,7 @@ getTables(Archive *fout, int *numTables)
 
 	appendPQExpBufferStr(query,
 						  "LEFT JOIN pg_class tc ON (c.reltoastrelid = tc.oid"
-						  " AND tc.relkind = " CppAsString2(RELKIND_TOASTVALUE) ")\n"
+						  " AND tc.relkind = " CppAsString2(RELKIND_TOASTVALUE) ")\n"\
 						  "LEFT JOIN pg_partition_rule pr ON c.oid = pr.parchildrelid\n"
 						  "LEFT JOIN pg_partition p ON pr.paroid = p.oid\n"
 						  "LEFT JOIN pg_partition pl ON (c.oid = pl.parrelid AND pl.parlevel = 0)\n");
@@ -5179,7 +5139,7 @@ getBMIndxInfo(Archive *fout)
 	Oid bitmap_index_namespace;
 
 	/* On GPDB5 pg_bitmapindex OID is 3012 */
-	bitmap_index_namespace = fout->remoteVersion >= 90400 ? PG_BITMAPINDEX_NAMESPACE : 3012;
+	bitmap_index_namespace = fout->remoteVersion > GPDB6_MAJOR_PGVERSION ? PG_BITMAPINDEX_NAMESPACE : 3012;
 
 	resetPQExpBuffer(query);
 
@@ -9842,7 +9802,7 @@ dumpFunc(Archive *fout, FuncInfo *finfo)
 								"proiswin as proiswindow,\n");
 
 		/* GPDB6 added proexeclocation */
-		if (fout->remoteVersion >= 90400)
+		if (fout->remoteVersion > GPDB6_MAJOR_PGVERSION)
 				appendPQExpBuffer(query,
 								"proexeclocation\n");
 		else
@@ -12980,7 +12940,6 @@ dumpExternal(Archive *fout, TableInfo *tbinfo, PQExpBuffer q, PQExpBuffer delq)
 		bool		isweb = false;
 		bool		iswritable = false;
 		char	   *options;
-		bool		gpdb6OrLater = isGPDB6000OrLater(fout);
 		char	   *logerrors = NULL;
 		char	   *on_clause;
 		char	   *qualrelname;
@@ -12997,7 +12956,7 @@ dumpExternal(Archive *fout, TableInfo *tbinfo, PQExpBuffer q, PQExpBuffer delq)
 						  qualrelname);
 
 		/* Now get required information from pg_exttable */
-		if (gpdb6OrLater)
+		if (fout->remoteVersion > GPDB6_MAJOR_PGVERSION)
 		{
 			appendPQExpBuffer(query,
 					"SELECT x.urilocation, x.execlocation, x.fmttype, x.fmtopts, x.command, "
@@ -16100,7 +16059,7 @@ testGPbackend(Archive *fout)
 static void
 addDistributedBy(Archive *fout, PQExpBuffer q, TableInfo *tbinfo, int actual_atts)
 {
-	if (isGPDB6000OrLater(fout))
+	if (fout->remoteVersion > GPDB6_MAJOR_PGVERSION)
 	{
 		if (strcmp(tbinfo->distclause, "") != 0)
 			appendPQExpBuffer(q, " %s", tbinfo->distclause);

src/bin/pg_dump/pg_dump.h
@@ -62,7 +62,6 @@ typedef struct SimpleOidList
 	SimpleOidListCell *tail;
 } SimpleOidList;
 
-
 /*
  * The data structures used to store system catalog information.  Every
  * dumpable object is a subclass of DumpableObject.

commit 5592441d768327a82efb816fa1c3b011a620618b

Fix pg_dumpall to dump 5X tablespaces correctly
Commit 99dc08f introduced a regression where dumpTablespaces will no
longer choose the correct SQL to dump tablespaces on 5X.

src/bin/pg_dump/pg_dumpall.c
@@ -1451,7 +1451,7 @@ dumpTablespaces(PGconn *conn)
 	 * Greenplum, and the dump format should vary depending on if the dump is
 	 * --gp-syntax or --no-gp-syntax.
 	 */
-	if (server_version == GPDB5_MAJOR_PGVERSION)
+	if (server_version < GPDB6_MAJOR_PGVERSION)
 	{
 		filespace_to_tablespace = true;
 		/*