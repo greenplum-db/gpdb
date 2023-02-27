7a7af409b6fb343cec8b03fffe6d65c964820cf4
Yongtao Huang <yongtaoh@vmware.com>
2023-02-27 21:57:27 +0800

    fix comment

--- src/backend/cdb/cdbcopy.c
+++ src/backend/cdb/cdbcopy.c
@@ -434,10 +434,10 @@ cdbCopyEndInternal(CdbCopy *c, char *abort_msg,
 											 * QEs */
 	int64		total_rows_rejected = 0;	/* total num rows rejected by all
 											 * QEs */
-	int64		first_segment_rows_completed = 0;	/* total num rows completed by first
-											 		* QE, mainly for replicated table*/
-	int64		first_segment_rows_rejected = 0;	/* total num rows rejected by first
-													* QE, mainly for replicated table*/
+	int64		first_segment_rows_completed = -1;	/* total num rows completed by first QE,
+											 		 * mainly for replicated table */
+	int64		first_segment_rows_rejected = -1;	/* total num rows rejected by first QE,
+													 * mainly for replicated table */
 	ErrorData *first_error = NULL;
 	int			seg;
 	struct pollfd	*pollRead;
@@ -653,7 +653,7 @@ cdbCopyEndInternal(CdbCopy *c, char *abort_msg,
 				 */
 				if (c->is_replicated)
 				{
-					if (seg == 0)
+					if (first_segment_rows_rejected == -1) 
 						first_segment_rows_rejected = res->numRejected;
 					else
 					{
@@ -677,7 +677,7 @@ cdbCopyEndInternal(CdbCopy *c, char *abort_msg,
 				 */
 				if (c->is_replicated)
 				{
-					if (seg == 0)
+					if (first_segment_rows_completed == -1) 
 						first_segment_rows_completed = res->numCompleted;
 					else
 					{
