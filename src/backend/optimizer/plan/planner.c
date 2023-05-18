31bdeac0ea2161cace29256d1f196d5257fd0535
Xing Guo <higuoxing@gmail.com>
2023-05-18 10:40:34 +0800

    Add comments and an assertion.

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -373,6 +373,9 @@ standard_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 #ifdef USE_ORCA
 		result = optimize_query(parse, cursorOptions, boundParams);
 #else
+		/* Make sure this branch is not taken in builds using --disable-orca. */
+		Assert(false);
+		/* Keep compilers quiet in case the build used --disable-orca. */
 		result = NULL;
 #endif
 
