bd4a0d168a2d3136fd8a572e4f8758285d537f27
beeender <chenmulong@gmail.com>
2023-06-07 11:02:40 +0800

    remove re & time

--- src/test/isolation2/sql/resgroup/resgroup_auxiliary_tools_v1.sql
+++ src/test/isolation2/sql/resgroup/resgroup_auxiliary_tools_v1.sql
@@ -246,10 +246,6 @@ $$ LANGUAGE plpython3u;
 
 0: CREATE OR REPLACE FUNCTION is_session_in_group(pid integer, groupname text) RETURNS BOOL AS $$
     import subprocess
-    import time
-    import re
-
-    pt = re.compile(r'con(\d+)')
 
     sql = "select sess_id from pg_stat_activity where pid = '%d'" % pid
     result = plpy.execute(sql)
