946eb607d3519c592426a73def9e8ad5e18e75b9
TonyHou <hlinbit@yeah.net>
2023-07-14 13:12:16 +0800

    cancel CA file check when verify_gpfdists_cert=off

--- src/bin/gpfdist/regress/change_cert.bash
+++ src/bin/gpfdist/regress/change_cert.bash
@@ -10,8 +10,33 @@ if [ "$1" = "client" ]; then
 	done
 fi
 
+# Create a new CA direrctory for gpfdist with a random root.crt
 if [ "$1" = "gpfdist" ]; then
 	mkdir data/gpfdist_ssl/certs_server_no_verify
 	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Palo Alto/O=Pivotal/CN=root" -out data/gpfdist_ssl/certs_server_no_verify/root.crt
 	cp data/gpfdist_ssl/certs_matching/server.* data/gpfdist_ssl/certs_server_no_verify
+fi
+
+# Clear gpdb client and root CA certification.
+if [ "$1" = "clear" ]; then
+	for dir in $(find $COORDINATOR_DATA_DIRECTORY/../../.. -name pg_hba.conf)
+		do
+		if [ -d $(dirname $dir)/gpfdists ]; then
+			mv $(dirname $dir)/gpfdists/client.crt $(dirname $dir)/gpfdists/client.crt.bak
+			mv $(dirname $dir)/gpfdists/client.key $(dirname $dir)/gpfdists/client.key.bak
+			mv $(dirname $dir)/gpfdists/root.crt $(dirname $dir)/gpfdists/root.crt.bak
+		fi
+	done
+fi
+
+# Reset gpdb client and root CA certification.
+if [ "$1" = "recover" ]; then
+	for dir in $(find $COORDINATOR_DATA_DIRECTORY/../../.. -name pg_hba.conf)
+		do
+		if [ -d $(dirname $dir)/gpfdists ]; then
+			mv $(dirname $dir)/gpfdists/client.crt.bak $(dirname $dir)/gpfdists/client.crt
+			mv $(dirname $dir)/gpfdists/client.key.bak $(dirname $dir)/gpfdists/client.key
+			mv $(dirname $dir)/gpfdists/root.crt.bak $(dirname $dir)/gpfdists/root.crt
+		fi
+	done
 fi
