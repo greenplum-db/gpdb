greenplum-db/gpdb

commit e0cca545510322805731d5eb723f512df6b25f29

(6X) Refactor motion deadlock handler.
The idea that IC-UDP motion deadlock is related to join plan is not
accurate, the pattern of IC-UDP motion deadlock should be from the
whole plan's perspective, that is:

  An MPP slice's algorithm is
    1. fetch one tuple from motion
    2. fetch tuples from another motion

Because the step 1 fetching one tuple from motion is not a sync
operation, here by sync we mean each QE in this slice operates in the
same beat. Like fetching all tuples from a motion until finishing is a
sync operation, because each QE in the sending slice must have send
out End-Of-Stream to each QE in the receving slice, and there are no
more calls to this motion node.

Previous idea was the step 2 only from: inner plan of join and
SubPlans in Join's Qual or Join's joinqual. This is not
correct. SubPlans can come from any expression not related to join:
target entry, plan qual, joinqual, withcheck option...

SubPlans in Greenplum, if containing a motion, the motion has to be
under a material node to make the SubPlan rescannable. Previous
prefetch inner (for nestloop), prefetch qual, prefetch joinqual is all
based on this. Also note in 6X, HashSubPlan also will have a
material node over the motion.

This commit fixes the motion deadlock involving SubPlans. The fix
method is before actually executing the main plan, at the first time,
always pre-execute the material over motion node in the main plan
node's SubPlans. I also remove the old test `deadlock2.sql` which is
all covered in the new test file `motion_deadlock.sql`.

This is 6X version of main branch PR
https://github.com/greenplum-db/gpdb/pull/15842.

Fixes Issue: https://github.com/greenplum-db/gpdb/issues/15719

Refer to gpdb-dev mailing list discussion for more details:
https://groups.google.com/a/greenplum.org/g/gpdb-dev/c/Y4ajINeKeUw

src/backend/executor/execProcnode.c
@@ -157,6 +157,9 @@ static CdbVisitOpt planstate_walk_kids(PlanState *planstate,
 					void *context,
 					int flags);
 
+static List *find_all_mat_nodes(PlanState *ps);
+static void  prefetch_subplans(PlanState *node);
+
 /*
  * saveExecutorMemoryAccount saves an operator specific memory account
  * into the PlanState of that operator
@@ -959,6 +962,31 @@ ExecProcNode(PlanState *node)
 		if (query_info_collect_hook)
 			(*query_info_collect_hook)(METRICS_PLAN_NODE_EXECUTING, node);
 		node->fHadSentNodeStart = true;
+	}
+
+	/*
+	 * Greenplum specific code.
+	 * For historical background, please refer to the comments of
+	 * motion_sanity_walker to understand interconnect UDP deadlock.
+	 * Previous methods focus on joinqual and join node's plan qual's
+	 * motion node. In fact, motion nodes from SubPlan might also exist
+	 * in target list and qual (or join qual) from upper level nodes.
+	 * See Github Issue 15719 for examples of motion deadlock involving
+	 * target list subplan.
+	 *
+	 * For SubPlan in Greenplum, most of the cases it contains material
+	 * over broadcast (or gather motion) to make it rescannable. So if
+	 * there is data returned by plan node, anyway, the SubPlan will
+	 * fetch all data from motion and materialize it.
+	 *
+	 * To get rid of interconnect UDP deadlock involving SubPlan, we
+	 * prefetch them before actually execute the plannode at the first
+	 * time.
+	 */
+	if (!node->prefetch_subplans_done && node->subPlan != NIL)
+	{
+		prefetch_subplans(node);
+		node->prefetch_subplans_done = true;
 	}
 
 	switch (nodeTag(node))
@@ -1803,3 +1831,90 @@ planstate_walk_kids(PlanState *planstate,
 
 	return v;
 }	/* planstate_walk_kids */
+
+/*
+ * Greenplum specific code.
+ * Interconnect UDP deadlock might happen between outer plan and SubPlans.
+ * This kind of deadlock in fact is not directly related to Join Plan. To
+ * Get rid of this kind of deadlock, the method here is to prefetch all
+ * SubPlans for a node at the first time.
+ *
+ * For more details please refer to the thread in gpdb-dev mailing list:
+ * https://groups.google.com/a/greenplum.org/g/gpdb-dev/c/Y4ajINeKeUw
+ */
+static void
+prefetch_subplans(PlanState *node)
+{
+	ListCell *lc        = NULL;
+	List     *mat_nodes = NIL;
+
+	if (node->subPlan == NIL)
+		return;
+
+	foreach(lc, node->subPlan)
+	{
+		PlanState *ps = ((SubPlanState *) lfirst(lc))->planstate;
+		mat_nodes = list_concat(mat_nodes, find_all_mat_nodes(ps));
+	}
+
+	if (mat_nodes != NIL)
+	{
+		foreach(lc, mat_nodes)
+		{
+			PlanState *ps = (PlanState *) lfirst(lc);
+			Assert(IsA(ps, MaterialState));
+
+			((MaterialState *) ps)->cdb_strict = true;
+			((MaterialState *) ps)->eflags |= EXEC_FLAG_REWIND;
+
+			(void) ExecProcNode(ps);
+			ExecReScan(ps);
+		}
+	}
+}
+
+/*
+ * Greenplum specific code.
+ * InitPlans are separately dispatched before the main plan,
+ * we don't want walk into them.
+ */
+static List *
+find_all_mat_nodes(PlanState *ps)
+{
+	List     *mat_nodes = NIL;
+	ListCell *lc        = NULL;
+
+	if (ps == NULL)
+		return NIL;
+
+	if (IsA(ps, MotionState))
+	{
+		/*
+		 * This function is only designed to search in a SubPlan.
+		 * In SubPlan, motion nodes should be wrapped by a MaterialState
+		 * to make it rescannable. Only exception case I can think of,
+		 * is HashSubPlan, which will be handled below. So if we meet a
+		 * motion here, just return not recursively walk into other slice.
+		 */
+		return NIL;
+	}
+
+	if (IsA(ps, MaterialState) &&
+		IsA(outerPlanState(ps), MotionState))
+		return list_make1(ps);
+
+	if (outerPlanState(ps))
+		mat_nodes = list_concat(mat_nodes, find_all_mat_nodes(outerPlanState(ps)));
+	if (innerPlanState(ps))
+		mat_nodes = list_concat(mat_nodes, find_all_mat_nodes(innerPlanState(ps)));
+	if (ps->subPlan)
+	{
+		foreach(lc, ps->subPlan)
+		{
+			PlanState *sps = ((SubPlanState *) lfirst(lc))->planstate;
+			mat_nodes = list_concat(mat_nodes, find_all_mat_nodes(sps));
+		}
+	}
+
+	return mat_nodes;
+}

src/include/nodes/execnodes.h
@@ -1432,6 +1432,8 @@ typedef struct PlanState
 
 	/* MemoryAccount to use for recording the memory usage of different plan nodes. */
 	MemoryAccountIdType memoryAccountId;
+
+	bool		prefetch_subplans_done;
 } PlanState;
 
 /* Gpperfmon helper functions defined in execGpmon.c */
@@ -2467,6 +2469,7 @@ typedef struct MaterialState
 	void	   *ts_markpos;
 	void	   *share_lk_ctxt;
 	char	   *share_bufname_prefix;
+	bool        cdb_strict;
 } MaterialState;
 
 /* ----------------

src/backend/executor/execUtils.c
@@ -89,8 +89,6 @@ static bool index_recheck_constraint(Relation index, Oid *constr_procs,
 						 Datum *existing_values, bool *existing_isnull,
 						 Datum *new_values);
 static void ShutdownExprContext(ExprContext *econtext, bool isCommit);
-static List *flatten_logic_exprs(Node *node);
-
 
 /* ----------------------------------------------------------------
  *				 Executor state and memory management functions
@@ -1686,49 +1684,6 @@ ExecGetShareNodeEntry(EState* estate, int shareidx, bool fCreate)
 }
 
 /*
- * flatten_logic_exprs
- * This function is only used by ExecPrefetchJoinQual.
- * ExecPrefetchJoinQual need to prefetch subplan in join
- * qual that contains motion to materialize it to avoid
- * motion deadlock. This function is going to flatten
- * the bool exprs to avoid shortcut of bool logic.
- * An example is:
- * (a and b or c) or (d or e and f or g) and (h and i or j)
- * will be transformed to
- * (a, b, c, d, e, f, g, h, i, j).
- */
-static List *
-flatten_logic_exprs(Node *node)
-{
-	if (node == NULL)
-		return NIL;
-
-	if (IsA(node, BoolExprState))
-	{
-		BoolExprState *be = (BoolExprState *) node;
-		return flatten_logic_exprs((Node *) (be->args));
-	}
-
-	if (IsA(node, List))
-	{
-		List     *es = (List *) node;
-		List     *result = NIL;
-		ListCell *lc = NULL;
-
-		foreach(lc, es)
-		{
-			Node *n = (Node *) lfirst(lc);
-			result = list_concat(result,
-								 flatten_logic_exprs(n));
-		}
-
-		return result;
-	}
-
-	return list_make1(node);
-}
-
-/*
  * fake_outer_params
  *   helper function to fake the nestloop's nestParams
  *   so that prefetch inner or prefetch joinqual will
@@ -1775,86 +1730,13 @@ fake_outer_params(JoinState *node)
 }
 
 /*
- * Prefetch JoinQual or NonJoinQual to prevent motion hazard.
- *
- * A motion hazard is a deadlock between motions, a classic motion hazard in a
- * join executor is formed by its inner and outer motions, it can be prevented
- * by prefetching the inner plan, refer to motion_sanity_check() for details.
- *
- * A similar motion hazard can be formed by the outer motion and the join qual
- * motion(or non join qual motion).  A join executor fetches a outer tuple,
- * filters it with the qual, then repeat the process on all the outer tuples.
- * When there are motions in both outer plan and the join qual then below state
- * is possible:
- *
- * 0. processes A and B belong to the join slice, process C belongs to the
- *    outer slice, process D belongs to the JoinQual(NonJoinQual) slice;
- * 1. A has read the first outer tuple and is fetching tuples from D;
- * 2. D is waiting for ACK from B;
- * 3. B is fetching the first outer tuple from C;
- * 4. C is waiting for ACK from A;
- *
- * So a deadlock is formed A->D->B->C->A.  We can prevent it also by
- * prefetching the join qual or non join qual
- *
- * An example is demonstrated and explained in test case
- * src/test/regress/sql/deadlock2.sql.
- *
- * Return true if the JoinQual or NonJoinQual is prefetched.
+ * Leave a dummy Function here to make ABI not broken
+ * for 6X STABLE version. See comments in ExecInitNestLoop.
  */
 void
 ExecPrefetchQual(JoinState *node, bool isJoinQual)
 {
-	EState	   *estate = node->ps.state;
-	ExprContext *econtext = node->ps.ps_ExprContext;
-	PlanState  *inner = innerPlanState(node);
-	PlanState  *outer = outerPlanState(node);
-	TupleTableSlot *innertuple = econtext->ecxt_innertuple;
-
-	ListCell   *lc = NULL;
-	List       *quals = NIL;
-
-	List   *qual;
-
-	if (isJoinQual)
-		qual = node->joinqual;
-	else
-		qual = node->ps.qual;
-
-	Assert(qual);
-
-	/* Outer tuples should not be fetched before us */
-	Assert(econtext->ecxt_outertuple == NULL);
-
-	/* Build fake inner & outer tuples */
-	econtext->ecxt_innertuple = ExecInitNullTupleSlot(estate,
-													  ExecGetResultType(inner));
-	econtext->ecxt_outertuple = ExecInitNullTupleSlot(estate,
-													  ExecGetResultType(outer));
-
-	if (IsA(node->ps.plan, NestLoop))
-	{
-		NestLoop *nl = (NestLoop *) (node->ps.plan);
-		if (nl->nestParams)
-			fake_outer_params(node);
-	}
-
-	quals = flatten_logic_exprs((Node *) qual);
-
-	/* Fetch subplan with the fake inner & outer tuples */
-	foreach(lc, quals)
-	{
-		/*
-		 * Force every qual is prefech because
-		 * our target is to materialize motion node.
-		 */
-		ExprState  *clause = (ExprState *) lfirst(lc);
-		(void) ExecQual(list_make1(clause), econtext, false);
-	}
-
-	/* Restore previous state */
-	econtext->ecxt_innertuple = innertuple;
-	econtext->ecxt_outertuple = NULL;
+	return;
 }
 
 /* ----------------------------------------------------------------

src/backend/executor/nodeHashjoin.c
@@ -237,23 +237,6 @@ ExecHashJoin_guts(HashJoinState *node)
 					return NULL;
 
 				/*
-				 * Prefetch JoinQual or NonJoinQual to prevent motion hazard.
-				 *
-				 * See ExecPrefetchQual() for details.
-				 */
-				if (node->prefetch_joinqual)
-				{
-					ExecPrefetchQual(&node->js, true);
-					node->prefetch_joinqual = false;
-				}
-
-				if (node->prefetch_qual)
-				{
-					ExecPrefetchQual(&node->js, false);
-					node->prefetch_qual = false;
-				}
-
-				/*
 				 * We just scanned the entire inner side and built the hashtable
 				 * (and its overflow batches). Check here and remember if the inner
 				 * side is empty.
@@ -611,22 +594,10 @@ ExecInitHashJoin(HashJoin *node, EState *estate, int eflags)
 	 * the fix to MPP-989)
 	 */
 	hjstate->prefetch_inner = node->join.prefetch_inner;
-	hjstate->prefetch_joinqual = node->join.prefetch_joinqual;
-	hjstate->prefetch_qual = node->join.prefetch_qual;
 
-	if (Test_print_prefetch_joinqual && hjstate->prefetch_joinqual)
-		elog(NOTICE,
-			 "prefetch join qual in slice %d of plannode %d",
-			 currentSliceId, ((Plan *) node)->plan_node_id);
-
-	/*
-	 * reuse GUC Test_print_prefetch_joinqual to output debug information for
-	 * prefetching non join qual
-	 */
-	if (Test_print_prefetch_joinqual && hjstate->prefetch_qual)
-		elog(NOTICE,
-			 "prefetch non join qual in slice %d of plannode %d",
-			 currentSliceId, ((Plan *) node)->plan_node_id);
+	/* Greenplum specific: see comments in ExecInitNestLoop */	
+	hjstate->prefetch_joinqual = false;
+	hjstate->prefetch_qual = false;
 
 	/*
 	 * initialize child nodes

src/backend/executor/nodeMaterial.c
@@ -128,8 +128,7 @@ ExecMaterial(MaterialState *node)
 		 * is used to share input, we will need to fetch all rows and put
 		 * them in tuple store
 		 */
-		while (((Material *) node->ss.ps.plan)->cdb_strict
-				|| ma->share_type != SHARE_NOTSHARED)
+		while (node->cdb_strict || ma->share_type != SHARE_NOTSHARED)
 		{
 			TupleTableSlot *outerslot = ExecProcNode(outerPlanState(node));
 
@@ -261,7 +260,10 @@ ExecInitMaterial(Material *node, EState *estate, int eflags)
 	matstate->ss.ps.state = estate;
 
 	if (node->cdb_strict)
+	{
 		eflags |= EXEC_FLAG_REWIND;
+		matstate->cdb_strict = true;
+	}
 
 	/*
 	 * If the Material node was inserted to protect the child node from rescanning, don't

src/backend/executor/nodeMergejoin.c
@@ -682,23 +682,6 @@ ExecMergeJoin_guts(MergeJoinState *node)
 	}
 
 	/*
-	 * Prefetch JoinQual or NonJoinQual to prevent motion hazard.
-	 *
-	 * See ExecPrefetchQual() for details.
-	 */
-	if (node->prefetch_joinqual)
-	{
-		ExecPrefetchQual(&node->js, true);
-		node->prefetch_joinqual = false;
-	}
-
-	if (node->prefetch_qual)
-	{
-		ExecPrefetchQual(&node->js, false);
-		node->prefetch_qual = false;
-	}
-
-	/*
 	 * ok, everything is setup.. let's go to work
 	 */
 	for (;;)
@@ -1582,22 +1565,10 @@ ExecInitMergeJoin(MergeJoin *node, EState *estate, int eflags)
 	mergestate->mj_ConstFalseJoin = false;
 
 	mergestate->prefetch_inner = node->join.prefetch_inner;
-	mergestate->prefetch_joinqual = node->join.prefetch_joinqual;
-	mergestate->prefetch_qual = node->join.prefetch_qual;
 
-	if (Test_print_prefetch_joinqual && mergestate->prefetch_joinqual)
-		elog(NOTICE,
-			 "prefetch join qual in slice %d of plannode %d",
-			 currentSliceId, ((Plan *) node)->plan_node_id);
-
-	/*
-	 * reuse GUC Test_print_prefetch_joinqual to output debug information for
-	 * prefetching non join qual
-	 */
-	if (Test_print_prefetch_joinqual && mergestate->prefetch_qual)
-		elog(NOTICE,
-			 "prefetch non join qual in slice %d of plannode %d",
-			 currentSliceId, ((Plan *) node)->plan_node_id);
+	/* Greenplum specific: see comments in ExecInitNestLoop */
+	mergestate->prefetch_joinqual = false;
+	mergestate->prefetch_qual = false;
 
 	/* Prepare inner operators for rewind after the prefetch */
 	rewindflag = mergestate->prefetch_inner ? EXEC_FLAG_REWIND : 0;

src/backend/executor/nodeNestloop.c
@@ -166,23 +166,6 @@ ExecNestLoop_guts(NestLoopState *node)
 	}
 
 	/*
-	 * Prefetch JoinQual or NonJoinQual to prevent motion hazard.
-	 *
-	 * See ExecPrefetchQual() for details.
-	 */
-	if (node->prefetch_joinqual)
-	{
-		ExecPrefetchQual(&node->js, true);
-		node->prefetch_joinqual = false;
-	}
-
-	if (node->prefetch_qual)
-	{
-		ExecPrefetchQual(&node->js, false);
-		node->prefetch_qual = false;
-	}
-
-	/*
 	 * Ok, everything is setup for the join so now loop until we return a
 	 * qualifying join tuple.
 	 */
@@ -422,22 +405,21 @@ ExecInitNestLoop(NestLoop *node, EState *estate, int eflags)
 	nlstate->shared_outer = node->shared_outer;
 
 	nlstate->prefetch_inner = node->join.prefetch_inner;
-	nlstate->prefetch_joinqual = node->join.prefetch_joinqual;
-	nlstate->prefetch_qual = node->join.prefetch_qual;
-
-	if (Test_print_prefetch_joinqual && nlstate->prefetch_joinqual)
-		elog(NOTICE,
-			 "prefetch join qual in slice %d of plannode %d",
-			 currentSliceId, ((Plan *) node)->plan_node_id);
 
 	/*
-	 * reuse GUC Test_print_prefetch_joinqual to output debug information for
-	 * prefetching non join qual
+	 * Greeplum specific:
+	 * prefetch joinqual and prefetch qual are old methods
+	 * to get rid of motion deadlock. Motion nodes in joinqual
+	 * or planqual are in SubPlan expressions. Thus the motion
+	 * can also appear in any TargetList which means old ways
+	 * do not consider all cases and motion dealocks are not
+	 * only limited JOIN. For 6X Stable version, we have to make
+	 * sure for ABI compatible thus we have to keep these fields
+	 * introduced by previous fix, just set them to false. This
+	 * logic also exists in ExecInitHashJoin & ExecInitMergeJoin.
 	 */
-	if (Test_print_prefetch_joinqual && nlstate->prefetch_qual)
-		elog(NOTICE,
-			 "prefetch non join qual in slice %d of plannode %d",
-			 currentSliceId, ((Plan *) node)->plan_node_id);
+	nlstate->prefetch_joinqual = false;
+	nlstate->prefetch_qual = false;
 
 	/*CDB-OLAP*/
 	nlstate->reset_inner = false;

src/backend/optimizer/plan/createplan.c
@@ -64,13 +64,6 @@
 #include "cdb/cdbsreh.h"
 #include "cdb/cdbvars.h"
 
-typedef struct
-{
-	plan_tree_base_prefix base; /* Required prefix for
-								 * plan_tree_walker/mutator */
-	Bitmapset            *seen_subplans;
-	bool                  result;
-} contain_motion_walk_context;
 
 static Plan *create_subplan(PlannerInfo *root, Path *best_path);		/* CDB */
 static Plan *create_scan_plan(PlannerInfo *root, Path *best_path);
@@ -196,8 +189,6 @@ static Motion *cdbpathtoplan_create_motion_plan(PlannerInfo *root,
 								 CdbMotionPath *path,
 								 Plan *subplan);
 static void append_initplan_for_function_scan(PlannerInfo *root, Path *best_path, Plan *plan);
-static bool contain_motion(PlannerInfo *root, Node *node);
-static bool contain_motion_walk(Node *node, contain_motion_walk_context *ctx);
 
 /*
  * GPDB_92_MERGE_FIXME: The following functions have been removed in PG 9.2
@@ -807,32 +798,6 @@ create_join_plan(PlannerInfo *root, JoinPath *best_path)
 			best_path->path.parent ? best_path->path.parent->relids
 					: NULL,
 					  plan);
-
-	/*
-	 * We may set prefetch_joinqual to true if there is
-	 * potential risk when create_xxxjoin_plan. Here, we
-	 * have all the information at hand, this is the final
-	 * logic to set prefetch_joinqual.
-	 */
-	if (((Join *) plan)->prefetch_joinqual)
-	{
-		List *joinqual = ((Join *) plan)->joinqual;
-
-		((Join *) plan)->prefetch_joinqual = contain_motion(root,
-															(Node *) joinqual);
-	}
-
-	/*
-	 * Similar for non join qual. If it contains a motion and outer relation
-	 * also contains a motion, then we should set prefetch_qual to true.
-	 */
-	if (((Join *) plan)->prefetch_qual)
-	{
-		List *qual = ((Join *) plan)->plan.qual;
-
-		((Join *) plan)->prefetch_qual = contain_motion(root,
-															(Node *) qual);
-	}
 
 	/*
 	 * If there are any pseudoconstant clauses attached to this node, insert a
@@ -3329,27 +3294,6 @@ create_nestloop_plan(PlannerInfo *root,
 	if (prefetch)
 		join_plan->join.prefetch_inner = true;
 
-	/*
-	 * A motion deadlock can also happen when outer and joinqual both contain
-	 * motions.  It is not easy to check for joinqual here, so we set the
-	 * prefetch_joinqual mark only according to outer motion, and check for
-	 * joinqual later in the executor.
-	 *
-	 * See ExecPrefetchJoinQual() for details.
-	 */
-	if (best_path->outerjoinpath &&
-		best_path->outerjoinpath->motionHazard &&
-		join_plan->join.joinqual != NIL)
-		join_plan->join.prefetch_joinqual = true;
-
-	/*
-	 * Similar for non join qual.
-	 */
-	if (best_path->outerjoinpath &&
-		best_path->outerjoinpath->motionHazard &&
-		join_plan->join.plan.qual != NIL)
-		join_plan->join.prefetch_qual = true;
-
 	return join_plan;
 }
 
@@ -3675,35 +3619,6 @@ create_mergejoin_plan(PlannerInfo *root,
 
 	join_plan->join.prefetch_inner = prefetch;
 
-	/*
-	 * A motion deadlock can also happen when outer and joinqual both contain
-	 * motions.  It is not easy to check for joinqual here, so we set the
-	 * prefetch_joinqual mark only according to outer motion, and check for
-	 * joinqual later in the executor.
-	 *
-	 * See ExecPrefetchJoinQual() for details.
-	 */
-	if (best_path->jpath.outerjoinpath &&
-		best_path->jpath.outerjoinpath->motionHazard &&
-		join_plan->join.joinqual != NIL)
-		join_plan->join.prefetch_joinqual = true;
-	/*
-	 * If inner motion is not under a Material or Sort node then there could
-	 * also be motion deadlock between inner and joinqual in mergejoin.
-	 */
-	if (best_path->jpath.innerjoinpath &&
-		best_path->jpath.innerjoinpath->motionHazard &&
-		join_plan->join.joinqual != NIL)
-		join_plan->join.prefetch_joinqual = true;
-
-	/*
-	 * Similar for non join qual.
-	 */
-	if (best_path->jpath.innerjoinpath &&
-		best_path->jpath.innerjoinpath->motionHazard &&
-		join_plan->join.plan.qual != NIL)
-		join_plan->join.prefetch_qual = true;
-
 	/* Costs of sort and material steps are included in path cost already */
 	copy_path_costsize(root, &join_plan->join.plan, &best_path->jpath.path);
 
@@ -3857,27 +3772,6 @@ create_hashjoin_plan(PlannerInfo *root,
 	{
 		join_plan->join.prefetch_inner = true;
 	}
-
-	/*
-	 * A motion deadlock can also happen when outer and joinqual both contain
-	 * motions.  It is not easy to check for joinqual here, so we set the
-	 * prefetch_joinqual mark only according to outer motion, and check for
-	 * joinqual later in the executor.
-	 *
-	 * See ExecPrefetchJoinQual() for details.
-	 */
-	if (best_path->jpath.outerjoinpath &&
-		best_path->jpath.outerjoinpath->motionHazard &&
-		join_plan->join.joinqual != NIL)
-		join_plan->join.prefetch_joinqual = true;
-
-	/*
-	 * Similar for non join qual.
-	 */
-	if (best_path->jpath.outerjoinpath &&
-		best_path->jpath.outerjoinpath->motionHazard &&
-		join_plan->join.plan.qual != NIL)
-		join_plan->join.prefetch_qual = true;
 
 	copy_path_costsize(root, &join_plan->join.plan, &best_path->jpath.path);
 
@@ -7377,56 +7271,3 @@ append_initplan_for_function_scan(PlannerInfo *root, Path *best_path, Plan *plan
 														 &initplan->scan.plan);
 }
 
-/*
- * contain_motion
- * This function walks the joinqual list to  see there is
- * any motion node in it. The only case a qual contains motion
- * is that it is a SubPlan and the SubPlan contains motion.
- * XXX: an over kill method is used here, for details please
- * refer to the following function `contain_motion_walk`.
- */
-static bool
-contain_motion(PlannerInfo *root, Node *node)
-{
-	contain_motion_walk_context ctx;
-	planner_init_plan_tree_base(&ctx.base, root);
-	ctx.result = false;
-	ctx.seen_subplans = NULL;
-
-	(void) contain_motion_walk(node, &ctx);
-
-	return ctx.result;
-}
-
-static bool
-contain_motion_walk(Node *node, contain_motion_walk_context *ctx)
-{
-	if (ctx->result)
-		return true;
-
-	if (node == NULL)
-		return false;
-
-	if (IsA(node, SubPlan))
-	{
-		/*
-		 * In 6X we add motions for SubPlan after we have gotten
-		 * the plan. So at the time of this function calling,
-		 * we do not know if the SubPlan contains motion. We might
-		 * walk the plannedstmt to accurately determine this, but
-		 * that makes the code not that clean. So, we adopt an over
-		 * kill method here: any SubPlan contains motion. At least,
-		 * this can avoid motion deadlock.
-		 */
-		ctx->result = true;
-		return true;
-	}
-
-	if (IsA(node, Motion))
-	{
-		ctx->result = true;
-		return true;
-	}
-
-	return plan_tree_walker((Node *) node, contain_motion_walk, ctx);
-}

commit 74eb71f0251e87f1085e8dc5a6b4fc4745c3d472

Fix ABI broken of 6X introduced by e0cca54551.
Commit e0cca54551 add a new field at the end of struct PlanState,
however PlanState is many other execute Nodes' base. So the previous
commit breaks ABI of stable version. This commit fixes it by adding
the field in SubPlanState, which is a leaf struct in GPDB 6 (no other
struct include it as base).

src/backend/executor/execProcnode.c
@@ -981,13 +981,13 @@ ExecProcNode(PlanState *node)
 	 *
 	 * To get rid of interconnect UDP deadlock involving SubPlan, we
 	 * prefetch them before actually execute the plannode at the first
-	 * time.
+	 * time. Due to 6X stable ABI limit, we cannot add any new fields to
+	 * struct PlanState, because it is the base of many other StateNodes.
+	 * In 6X, we add a new field at the end of SubPlanState to mark if
+	 * the SubPlan has already been prefetched.
 	 */
-	if (!node->prefetch_subplans_done && node->subPlan != NIL)
-	{
+	if (node->subPlan != NIL)
 		prefetch_subplans(node);
-		node->prefetch_subplans_done = true;
-	}
 
 	switch (nodeTag(node))
 	{
@@ -1854,6 +1854,11 @@ prefetch_subplans(PlanState *node)
 	foreach(lc, node->subPlan)
 	{
 		PlanState *ps = ((SubPlanState *) lfirst(lc))->planstate;
+
+		if (((SubPlanState *) lfirst(lc))->prefetch_subplan_done)
+			continue;
+
+		((SubPlanState *) lfirst(lc))->prefetch_subplan_done = true;
 		mat_nodes = list_concat(mat_nodes, find_all_mat_nodes(ps));
 	}
 

src/include/nodes/execnodes.h
@@ -1142,6 +1142,7 @@ typedef struct SubPlanState
 	FmgrInfo   *cur_eq_funcs;	/* equality functions for LHS vs. table */
 	void	   *ts_pos;
 	GenericTupStore *ts_state;
+	bool        prefetch_subplan_done; /* Greenplum specific */
 } SubPlanState;
 
 /* ----------------
@@ -1432,8 +1433,6 @@ typedef struct PlanState
 
 	/* MemoryAccount to use for recording the memory usage of different plan nodes. */
 	MemoryAccountIdType memoryAccountId;
-
-	bool		prefetch_subplans_done;
 } PlanState;
 
 /* Gpperfmon helper functions defined in execGpmon.c */