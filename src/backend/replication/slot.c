cloudberrydb/cloudberrydb

commit 67d4843a149cd9eafef7b9a46a75408e01137137

Add sync_error field to gp_stat_replication view
The sync_error field stores WalSndError exposed by function
gp_replication_error().

In order to report incremental recovery failure due to xlog missing, we
add this enum and stored the error state in the WalSndCtl, so that it
won't go away if the wal sender disappeared.

FIXME: see if we can get a better name for
pg_stat_gp_execution_segment()

Author: Xin Zhang <xzhang@pivotal.io>
Author: Ashwin Agrawal <aagrawal@pivotal.io>

src/include/replication/walsender_private.h
@@ -88,6 +88,12 @@ typedef struct WalSnd
 
 extern WalSnd *MyWalSnd;
 
+typedef enum
+{
+	WALSNDERROR_NONE = 0,
+	WALSNDERROR_WALREAD
+} WalSndError;
+
 /* There is one WalSndCtl struct for the whole database cluster */
 typedef struct
 {
@@ -123,6 +129,19 @@ typedef struct
 	 * Note:- Valid only when atleast one walsender is alive
 	 */
 	XLogRecPtr	walsnd_xlogCleanUpTo;
+
+	/*
+	 * Indicate error state of WalSender, for example, missing XLOG for mirror
+	 * to stream.
+	 *
+	 * Note: If we want to support multiple mirrors, this data structure
+	 * need to be redesigned (e.g. using WalSndError[]). We cannot store this
+	 * field in the walsnds[] array below, because the walsnds[] only
+	 * tracks the live wal senders. Hence, if the wal sender goes away
+	 * with certain error, the error state will go away with it.
+	 *
+	 */
+	WalSndError error;
 
 	WalSnd		walsnds[1];		/* VARIABLE LENGTH ARRAY */
 } WalSndCtlData;

src/backend/catalog/system_views.sql
@@ -392,34 +392,57 @@ CREATE VIEW pg_stat_replication AS
     WHERE S.usesysid = U.oid AND
             S.procpid = W.pid;
 
-CREATE FUNCTION pg_stat_get_master_replication() RETURNS SETOF RECORD AS
+CREATE FUNCTION gp_stat_get_master_replication() RETURNS SETOF RECORD AS
 $$
     SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, *
     FROM pg_catalog.pg_stat_replication
 $$
 LANGUAGE SQL EXECUTE ON MASTER;
 
-CREATE FUNCTION pg_stat_get_segment_replication() RETURNS SETOF RECORD AS
+CREATE FUNCTION gp_stat_get_segment_replication() RETURNS SETOF RECORD AS
 $$
     SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, *
     FROM pg_catalog.pg_stat_replication
 $$
 LANGUAGE SQL EXECUTE ON ALL SEGMENTS;
 
+CREATE FUNCTION gp_stat_get_segment_replication_error() RETURNS SETOF RECORD AS
+$$
+    SELECT pg_catalog.gp_execution_segment() AS gp_segment_id, pg_catalog.gp_replication_error() as sync_error
+$$
+LANGUAGE SQL EXECUTE ON ALL SEGMENTS;
+
 CREATE VIEW gp_stat_replication AS
-    SELECT * FROM pg_stat_get_master_replication() AS R
+    SELECT *, pg_catalog.gp_replication_error() AS sync_error
+    FROM pg_catalog.gp_stat_get_master_replication() AS R
     (gp_segment_id integer, procpid integer, usesysid oid,
      usename name, application_name text, client_addr inet,
      client_port integer, backend_start timestamptz, state text,
      sent_location text, write_location text, flush_location text,
      replay_location text, sync_priority integer, sync_state text)
     UNION ALL
-    SELECT * FROM pg_stat_get_segment_replication() AS R
-    (gp_segment_id integer, procpid integer, usesysid oid,
-     usename name, application_name text, client_addr inet,
-     client_port integer, backend_start timestamptz, state text,
-     sent_location text, write_location text, flush_location text,
-     replay_location text, sync_priority integer, sync_state text);
+    (
+        SELECT G.gp_segment_id
+            , R.procpid, R.usesysid, R.usename, R.application_name, R.client_addr
+            , R.client_port, R.backend_start, R.state, R.sent_location
+            , R.write_location, R.flush_location, R.replay_location
+            , R.sync_priority, R.sync_state
+            , G.sync_error
+        FROM (
+            SELECT E.*
+            FROM pg_catalog.gp_segment_configuration C
+            JOIN pg_catalog.gp_stat_get_segment_replication_error() AS E (gp_segment_id integer, sync_error text)
+            ON c.content = E.gp_segment_id
+            WHERE C.role = 'm'
+        ) G
+        LEFT OUTER JOIN pg_catalog.gp_stat_get_segment_replication() AS R
+        (gp_segment_id integer, procpid integer, usesysid oid,
+         usename name, application_name text, client_addr inet,
+         client_port integer, backend_start timestamptz, state text,
+         sent_location text, write_location text, flush_location text,
+         replay_location text, sync_priority integer, sync_state text)
+         ON G.gp_segment_id = R.gp_segment_id
+    );
 
 CREATE VIEW pg_stat_database AS 
     SELECT 

src/backend/replication/Makefile
@@ -18,11 +18,7 @@ override CPPFLAGS := -I$(libpq_srcdir) $(CPPFLAGS)
 #OBJS = walsender.o walreceiverfuncs.o walreceiver.o basebackup.o \
 #	repl_gram.o syncrep.o
 OBJS = basebackup.o gp_libpqwalreceiver.o repl_gram.o walreceiver.o \
-	   walreceiverfuncs.o walsender.o syncrep.o
-
-ifeq ($(enable_segwalrep), yes)
-OBJS += gp_replication.o
-endif
+	   walreceiverfuncs.o walsender.o syncrep.o gp_replication.o
 
 include $(top_srcdir)/src/backend/common.mk
 

src/backend/replication/gp_replication.c
@@ -12,10 +12,8 @@
  */
 
 #include "replication/gp_replication.h"
-#include "utils/guc.h"
-
-#include "replication/walsender.h"
 #include "replication/walsender_private.h"
+#include "utils/builtins.h"
 
 /*
  * Checking the WalSndCtl to figure out whether the mirror is up or not.
@@ -59,6 +57,7 @@ void GetMirrorStatus(bool *IsMirrorUp, bool *IsInSync)
 	LWLockRelease(SyncRepLock);
 }
 
+#ifdef USE_SEGWALREP
 /*
  * Set WalSndCtl->sync_standbys_defined to true to enable synchronous segment
  * WAL replication and insert synchronous_standby_names="*" into the
@@ -77,4 +76,11 @@ SetSyncStandbysDefined(void)
 	}
 
 	LWLockRelease(SyncRepLock);
+}
+#endif
+
+Datum
+gp_replication_error(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_TEXT_P(cstring_to_text(WalSndCtl->error == WALSNDERROR_WALREAD ? "walread" : "none"));
 }

src/backend/replication/walsender.c
@@ -887,6 +887,7 @@ XLogRead(char *buf, XLogRecPtr startptr, Size count)
 			sendFile = BasicOpenFile(path, O_RDONLY | PG_BINARY, 0);
 			if (sendFile < 0)
 			{
+				WalSndCtl->error = WALSNDERROR_WALREAD;
 				/*
 				 * If the file is not found, assume it's because the standby
 				 * asked for a too old WAL segment that has already been
@@ -915,10 +916,15 @@ XLogRead(char *buf, XLogRecPtr startptr, Size count)
 		if (sendOff != startoff)
 		{
 			if (lseek(sendFile, (off_t) startoff, SEEK_SET) < 0)
+			{
+				WalSndCtl->error = WALSNDERROR_WALREAD;
+
 				ereport(ERROR,
 						(errcode_for_file_access(),
 						 errmsg("could not seek in log file %u, segment %u to offset %u: %m",
 								sendId, sendSeg, startoff)));
+			}
+
 			sendOff = startoff;
 		}
 
@@ -930,11 +936,14 @@ XLogRead(char *buf, XLogRecPtr startptr, Size count)
 
 		readbytes = read(sendFile, p, segbytes);
 		if (readbytes <= 0)
+		{
+			WalSndCtl->error = WALSNDERROR_WALREAD;
 			ereport(ERROR,
 					(errcode_for_file_access(),
 			errmsg("could not read from log file %u, segment %u, offset %u, "
 				   "length %lu: %m",
 				   sendId, sendSeg, sendOff, (unsigned long) segbytes)));
+		}
 
 		/* Update state for read */
 		XLByteAdvance(recptr, readbytes);
@@ -958,12 +967,16 @@ XLogRead(char *buf, XLogRecPtr startptr, Size count)
 	{
 		char		filename[MAXFNAMELEN];
 
+		WalSndCtl->error = WALSNDERROR_WALREAD;
+
 		XLogFileName(filename, ThisTimeLineID, log, seg);
 		ereport(ERROR,
 				(errcode_for_file_access(),
 				 errmsg("requested WAL segment %s has already been removed",
 						filename)));
 	}
+
+	WalSndCtl->error = WALSNDERROR_NONE;
 }
 
 /*

src/include/catalog/pg_proc_gp.h
@@ -249,6 +249,10 @@ DESCR("Statistics: Greenplum session id of backend");
 DATA(insert OID = 6042 ( pg_renice_session  PGNSP PGUID 12 1 0 0 f f f t f v 2 0 23 "23 23" _null_ _null_ _null_ _null_ pg_renice_session _null_ _null_ _null_ n a ));
 DESCR("change priority of all the backends for a given session id");
 
+/* gp_replication_error() => text */
+DATA(insert OID = 3098 ( gp_replication_error PGNSP PGUID 12 1 0 0 f f f t f v 0 0 25 "" _null_ _null_ _null_ _null_ gp_replication_error _null_ _null_ _null_ ));
+DESCR("get replication error");
+
 /* pg_stat_get_wal_senders(OUT pid int4, OUT state text, OUT sent_location text, OUT write_location text, OUT flush_location text, OUT replay_location text, OUT sync_priority int4, OUT sync_state text) => SETOF pg_catalog.record */
 DATA(insert OID = 3099 ( pg_stat_get_wal_senders  PGNSP PGUID 12 1 1000 0 f f f f t s 0 0 2249 "" "{23,25,25,25,25,25,23,25}" "{o,o,o,o,o,o,o,o}" "{pid,state,sent_location,write_location,flush_location,replay_location,sync_priority,sync_state}" _null_ pg_stat_get_wal_senders _null_ _null_ _null_ n a ));
 DESCR("statistics: information about currently active replication");

src/include/replication/gp_replication.h
@@ -15,8 +15,11 @@
 #define GPDB_GP_REPLICATION_H
 
 #include "postgres.h"
+#include "fmgr.h"
 
 extern void GetMirrorStatus(bool *IsMirrorUp, bool *IsInSync);
 extern void SetSyncStandbysDefined(void);
+
+extern Datum gp_replication_error(PG_FUNCTION_ARGS __attribute__((unused)) );
 
 #endif

src/test/walrep/Makefile
@@ -15,7 +15,7 @@ ifeq ($(enable_segwalrep), yes)
 ifeq ($(WITH_MIRRORS), false)
 REGRESS += generate_ao_xlog generate_aoco_xlog replication_views_mirrorless
 else
-REGRESS += replication_views_mirrored
+REGRESS += replication_views_mirrored missing_xlog
 endif
 endif
 REGRESS_OPTS = --dbname="walrep_regression"

greenplum-db/gpdb

commit 6b1dffc8e636f053060a1938c8932fc8d7f13419

Flag persistent WalSndError on repslot invalidation
67d4843a149 introduced WalSndError as a means for having a persistent
error state, exposed through the sync_error field in
gp_stat_replication. This state was meant to signal
walsender<->walreceiver connection failures, even when the walsender
died (the traditional upstream mechanism depends on the walsender being
alive to report issues).

We should set this state when the replication slot is invalidated as
well. This was done correctly in 6X_STABLE commit: ea69506b407.

This would prevent flakes in the associated max_slot_wal_keep_size test
such as:

--- expected/segwalrep/max_slot_wal_keep_size.out
+++ results/segwalrep/max_slot_wal_keep_size.out
@@ -289,7 +450,7 @@
 1: SELECT sync_error FROM gp_stat_replication WHERE gp_segment_id = 0;
  sync_error
 ------------
- walread
+ none
 (1 row)

PS: The max_slot_wal_keep_size GUC was first backported to 6X and then
later backported to 7X from PG13.

Co-authored-by: Alexandra Wang <walexandra@vmware.com>
Reviewed-by: Huansong Fu <fuhuansong@gmail.com>

src/backend/replication/slot.c
@@ -45,6 +45,7 @@
 #include "miscadmin.h"
 #include "pgstat.h"
 #include "replication/slot.h"
+#include "replication/walsender_private.h"
 #include "storage/fd.h"
 #include "storage/proc.h"
 #include "storage/procarray.h"
@@ -1344,6 +1345,9 @@ restart:
 	 */
 	if (invalidated)
 	{
+		/* GPDB: Set WalSndCtl state to indicate persistent sync error state */
+		WalSndCtl->error = WALSNDERROR_WALREAD;
+
 		ReplicationSlotsComputeRequiredXmin(false);
 		ReplicationSlotsComputeRequiredLSN();
 	}

src/include/replication/walsender_private.h
@@ -108,7 +108,12 @@ typedef struct WalSnd
 
 extern WalSnd *MyWalSnd;
 
+/*
+ * GPDB: Meant to hold persistent state about a walsender<->walreceiver
+ * connection, on the walsender side, even if the walsender has died.
+ */
 typedef enum
+
 {
 	WALSNDERROR_NONE = 0,
 	WALSNDERROR_WALREAD