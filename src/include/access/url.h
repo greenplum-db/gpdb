greenplum-db/gpdb-archive

commit e358573762ee35a40f95ba96630f176b0cd795cc

Enable GUC `log_directory` to be modifiable
This commit enables the flexibility of `log_directory`.
There are two scenarios to consider.
- Absolute path
- Relative path to the data directory
- Relative path but goes outside the data directory

When the GUC change is an absolute path (or outside the data directory),
we will append a unique identifier (DBID) to the `log_directory` GUC. If
we do not do this, the segment logs will overwrite one another and cause issues.
    For example:
        `log_directory: /tmp/foosball` will internally change to
        coordinator: /tmp/foosball/1, seg0: /tmp/foosball/2, seg1:
        /tmp/foosball/3, and so on.

    The points above are all internal, so when a user queries the
    `log_directory` GUC, it will not have the DBID.
    For example:
        $ gpconfig -s log_directory
        Values on all segments are consistent
        GUC              : log_directory
        Coordinator value: /tmp/foosball
        Segment     value: /tmp/foosball

The change will work with reload `gpstop -u` and immediately takes effect
once the reload is done.

COMMIT NOTES

- Add `GP_SEG_LOGDIR` environment variable for gp_toolkit
  In order to know the segment log directory per segment, introduce
  `GP_SEG_LOGDIR`.
- Update `get_debug_event_counters.py` for `log_directory` GUC
- Make the default search be more flexible to accomadate the `log_directory`
  GUC.
- Add log_directory GUC test

postmaster logging:
  Also check the `Log_directory` to see if we can elog to the absolute
  path directory instead of the default `log` directory in datadir.

gprecoverseg:
  - Add log_directory GUC for `gprecoverseg --differential`
    Exclude relative path `log_directory` GUC when doing an rsync with
    `--differential`.
  - Update to use `set` instead of `list` for `exclude_list` to easily add
    directories and to easily determine if there is a test failure

basebackup:
    - Exclude `log_directory` from basebackup.c

pg_basebackup:

src/include/access/url.h
@@ -54,6 +54,7 @@ typedef struct extvar_t
 	char* GP_USER;
 	char* GP_SEG_PG_CONF;   /*location of the segments pg_conf file*/
 	char* GP_SEG_DATADIR;   /*location of the segments datadirectory*/
+	char* GP_SEG_LOGDIR;    /*location of the segment's log directory*/
 	char GP_DATE[9];		/* YYYYMMDD */
 	char GP_TIME[7];		/* HHMMSS */
 	char GP_XID[TMGIDSIZE];		/* global transaction id */

gpMgmt/bin/gppylib/commands/unix.py
@@ -478,7 +478,7 @@ def canonicalize(addr):
 class Rsync(Command):
     def __init__(self, name, srcFile, dstFile, srcHost=None, dstHost=None, recursive=False,
                  verbose=True, archive_mode=True, checksum=False, delete=False, progress=False,
-                 stats=False, dry_run=False, bwlimit=None, exclude_list=[], ctxt=LOCAL,
+                 stats=False, dry_run=False, bwlimit=None, exclude_list={}, ctxt=LOCAL,
                  remoteHost=None, compress=False, progress_file=None, ignore_times=False, whole_file=False):
 
         """

gpMgmt/bin/gppylib/test/unit/test_unit_gpsegrecovery.py
@@ -210,7 +210,6 @@ class FullRecoveryTestCase(GpTestCase):
         self.assertEqual([expected_init_args], self.mock_pgbasebackup_init.call_args_list)
         self.assertEqual(1, self.mock_pgbasebackup_run.call_count)
         self.assertEqual([call(validateAfter=True)], self.mock_pgbasebackup_run.call_args_list)
-        self.mock_logger.info.any_call('Running pg_basebackup failed: backup failed once')
         self.assertEqual(0, gpsegrecovery.start_segment.call_count)
         self._assert_cmd_failed('{"error_type": "full", "error_msg": "backup failed once", "dbid": 2, ' \
                                 '"datadir": "/data/mirror0", "port": 50000, "progress_file": "/tmp/test_progress_file"}')
@@ -228,7 +227,6 @@ class FullRecoveryTestCase(GpTestCase):
         self.assertEqual([expected_init_args], self.mock_pgbasebackup_init.call_args_list)
         self.assertEqual(1, self.mock_pgbasebackup_run.call_count)
         self.assertEqual([call(validateAfter=True)], self.mock_pgbasebackup_run.call_args_list)
-        self.mock_logger.info.any_call('Running pg_basebackup failed: backup failed once')
         self.assertEqual(0, gpsegrecovery.start_segment.call_count)
         self._assert_cmd_failed('{"error_type": "full", "error_msg": "backup failed once", "dbid": 2, ' \
                                 '"datadir": "/data/mirror0", "port": 50000, "progress_file": "/tmp/test_progress_file"}')
@@ -625,18 +623,49 @@ class DifferentialRecoveryRunTestCase(GpTestCase):
         self.assertFalse(self.diff_recovery_cmd.get_results().wasSuccessful())
 
     @patch('gpsegrecovery.DifferentialRecovery.write_conf_files', return_value=Mock())
+    @patch('gppylib.db.dbconn.query', return_value=FakeCursor(my_list=[["some_log"]]))
     @patch('gppylib.commands.unix.Rsync.__init__', return_value=None)
     @patch('gppylib.commands.unix.Rsync.run')
-    def test_rsync_run_passes(self, run, init, mock1):
+    def test_rsync_run_passes_with_changed_log_directory(self, run, init, conn, mock1):
         self.mock_rsync_run = run
         self.mock_rsync_init = init
         self.diff_recovery_cmd.run()
         self._assert_rsync_runs()
         self._assert_cmd_passed()
+        expected_exclude_list = {'/log', 'pgsql_tmp',
+                                 'postgresql.auto.conf.tmp',
+                                 'current_logfiles.tmp', 'postmaster.pid',
+                                 'postmaster.opts', 'pg_dynshmem',
+                                 'pg_notify/*', 'pg_replslot/*', 'pg_serial/*',
+                                 'pg_stat_tmp/*', 'pg_snapshots/*',
+                                 'pg_subtrans/*', 'backups/*', '/db_dumps',
+                                 '/promote', '/some_log'}
+        self.assertEqual(expected_exclude_list, self.mock_rsync_init.call_args_list[0][1]['exclude_list'])
+
+    @patch('gpsegrecovery.DifferentialRecovery.write_conf_files', return_value=Mock())
+    @patch('gppylib.db.dbconn.query', return_value=FakeCursor(my_list=[["log"]]))
+    @patch('gppylib.commands.unix.Rsync.__init__', return_value=None)
+    @patch('gppylib.commands.unix.Rsync.run')
+    def test_rsync_run_passes_with_default_log_directory(self, run, init, conn, mock1):
+        self.mock_rsync_run = run
+        self.mock_rsync_init = init
+        self.diff_recovery_cmd.run()
+        self._assert_rsync_runs()
+        self._assert_cmd_passed()
+        expected_exclude_list = {'/log', 'pgsql_tmp',
+                                 'postgresql.auto.conf.tmp',
+                                 'current_logfiles.tmp', 'postmaster.pid',
+                                 'postmaster.opts', 'pg_dynshmem',
+                                 'pg_notify/*', 'pg_replslot/*', 'pg_serial/*',
+                                 'pg_stat_tmp/*', 'pg_snapshots/*',
+                                 'pg_subtrans/*', 'backups/*', '/db_dumps',
+                                 '/promote'}
+        self.assertEqual(expected_exclude_list, self.mock_rsync_init.call_args_list[0][1]['exclude_list'])
 
     @patch('gppylib.commands.unix.Rsync.__init__', return_value=None)
     @patch('gppylib.commands.unix.Rsync.run')
-    def test_basebackup_run_passes(self, mock1, mock2):
+    @patch('gppylib.db.dbconn.query', return_value=FakeCursor(my_list=[["some_log"]]))
+    def test_basebackup_run_passes(self, mock1, mock2, mock3):
         self.diff_recovery_cmd.run()
         expected_init_args = call("/data/mirror0", "sdw1", '40000', writeconffilesonly=True,
                                   replication_slot_name='internal_wal_replication_slot',
@@ -682,12 +711,14 @@ class DifferentialRecoveryRunTestCase(GpTestCase):
             [call('Running differential recovery with progress output temporarily in /tmp/test_progress_file')],
             self.mock_logger.info.call_args_list)
 
-    def test_diff_recovery_sync_tablespaces_exception(self):
+    @patch('gppylib.db.dbconn.query', return_value=FakeCursor(my_list=[["some_log"]]))
+    def test_diff_recovery_sync_tablespaces_exception(self, mock1):
         self.mock_sync_tablespaces.side_effect = Exception()
         self.diff_recovery_cmd.run()
         self.assertEqual(1, self.mock_pg_stop_backup.call_count)
-        self.assertEqual(1, self.mock_logger.debug.call_count)
-        self.assertEqual([call('Syncing pg_data of dbid 2')],
+        self.assertEqual(2, self.mock_logger.debug.call_count)
+        self.assertEqual([call('Syncing pg_data of dbid 2'),
+                          call('adding /some_log to the exclude list')],
                          self.mock_logger.debug.call_args_list)
         self.assertEqual(1, self.mock_logger.info.call_count)
         self.assertEqual(
@@ -771,7 +802,6 @@ class DifferentialRecoveryRunTestCase(GpTestCase):
         self.assertEqual([expected_init_args], self.mock_pgbasebackup_init.call_args_list)
         self.assertEqual(1, self.mock_pgbasebackup_run.call_count)
         self.assertEqual([call(validateAfter=True)], self.mock_pgbasebackup_run.call_args_list)
-        self.mock_logger.info.any_call('Running pg_basebackup failed: backup failed once')
         self.assertEqual(0, gpsegrecovery.start_segment.call_count)
         self.assertEqual(2, self.mock_logger.debug.call_count)
         self.assertEqual([call('Writing recovery.conf and internal.auto.conf files for dbid 2'),

gpMgmt/sbin/gpsegrecovery.py
@@ -2,6 +2,7 @@
 
 import os
 import signal
+from contextlib import closing
 
 from gppylib.recoveryinfo import RecoveryErrorType
 from gppylib.commands.pg import PgBaseBackup, PgRewind, PgReplicationSlot
@@ -11,6 +12,7 @@ from gppylib.commands.base import Command, LOCAL
 from gppylib.commands.gp import SegmentStart
 from gppylib.gparray import Segment
 from gppylib.commands.gp import ModifyConfSetting
+from gppylib.db import dbconn
 from gppylib.db.catalog import RemoteQueryCommand
 from gppylib.operations.get_segments_in_recovery import is_seg_in_backup_mode
 from gppylib.operations.segment_tablespace_locations import get_segment_tablespace_locations
@@ -166,7 +168,7 @@ class DifferentialRecovery(Command):
             4. In future we will have to add backup_label in rsync_exclude_list if pg_start_backup needs be started in
                non-exclusive mode for differential recovery.
         """
-        rsync_exclude_list = [
+        rsync_exclude_list = {
             "/log",  # logs are segment specific so it can be skipped
             "pgsql_tmp",
             "postgresql.auto.conf.tmp",
@@ -183,7 +185,33 @@ class DifferentialRecovery(Command):
             "backups/*",
             "/db_dumps",  # as we exclude during pg_basebackup
             "/promote",  # Need to check why do we exclude it during pg_basebackup
-        ]
+        }
+
+        log_directory_sql = """
+        SELECT
+            gucs.log_directory
+        FROM (
+            -- split to two separate columns log_directory and data_directory
+            SELECT
+                MAX(setting) FILTER (WHERE name='log_directory') AS log_directory,
+                MAX(setting) FILTER (WHERE name='data_directory') AS data_directory
+            FROM pg_settings WHERE (name='log_directory' AND LEFT(setting,1) NOT LIKE '/') OR name='data_directory'
+        ) AS gucs where gucs.log_directory IS NOT NULL;
+        """
+
+        dburl = dbconn.DbURL(hostname=self.recovery_info.source_hostname,
+                             port=self.recovery_info.source_port,
+                             dbname='template1')
+        with closing(dbconn.connect(dburl, utility=True)) as conn:
+            res = dbconn.query(conn, log_directory_sql)
+            # There should only be a single result.
+            # Only exclude if the log_directory is a relative path,
+            # because absolute path for log_directory will usually be out of the
+            # datadir
+            for row in res.fetchall():
+                rsync_exclude_list.add(f'/{row[0]}')
+                self.logger.debug("adding /%s to the exclude list" % row[0])
+
         """
             Rsync options used:
                 srcFile: source datadir

gpcontrib/gp_toolkit/gp_toolkit--1.0.sql
@@ -204,7 +204,7 @@ CREATE EXTERNAL WEB TABLE gp_toolkit.__gp_log_segment_ext
     logline int,
     logstack text
 )
-EXECUTE E'cat $GP_SEG_DATADIR/log/*.csv'
+EXECUTE E'cat $GP_SEG_LOGDIR/*.csv'
 FORMAT 'CSV' (DELIMITER AS ',' NULL AS '' QUOTE AS '"');
 
 REVOKE ALL ON TABLE gp_toolkit.__gp_log_segment_ext FROM public;
@@ -251,7 +251,7 @@ CREATE EXTERNAL WEB TABLE gp_toolkit.__gp_log_coordinator_ext
     logline int,
     logstack text
 )
-EXECUTE E'cat $GP_SEG_DATADIR/log/*.csv' ON COORDINATOR
+EXECUTE E'cat $GP_SEG_LOGDIR/*.csv' ON COORDINATOR
 FORMAT 'CSV' (DELIMITER AS ',' NULL AS '' QUOTE AS '"');
 
 REVOKE ALL ON TABLE gp_toolkit.__gp_log_coordinator_ext FROM public;

src/backend/access/external/url.c
@@ -98,6 +98,7 @@ external_set_env_vars_ext(extvar_t *extvar, char *uri, bool csv, char *escape, c
 												 * pg_conf file  */
 	extvar->GP_SEG_DATADIR = DataDir;	/* location of the segments
 												 * datadirectory */
+	extvar->GP_SEG_LOGDIR = Log_directory;	/* location of the segments */
 	sprintf(extvar->GP_DATE, "%04d%02d%02d",
 			1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday);
 	sprintf(extvar->GP_TIME, "%02d%02d%02d",

src/backend/access/external/url_curl.c
@@ -1331,6 +1331,7 @@ url_curl_fopen(char *url, bool forwrite, extvar_t *ev, CopyState pstate)
 		set_httpheader(file, "X-GP-CSVOPT", ev->GP_CSVOPT);
 		set_httpheader(file, "X-GP_SEG_PG_CONF", ev->GP_SEG_PG_CONF);
 		set_httpheader(file, "X-GP_SEG_DATADIR", ev->GP_SEG_DATADIR);
+		set_httpheader(file, "X-GP_SEG_LOGDIR", ev->GP_SEG_LOGDIR);
 		set_httpheader(file, "X-GP-DATABASE", ev->GP_DATABASE);
 		set_httpheader(file, "X-GP-USER", ev->GP_USER);
 		set_httpheader(file, "X-GP-SEG-PORT", ev->GP_SEG_PORT);

src/backend/access/external/url_execute.c
@@ -217,6 +217,7 @@ make_command(const char *cmd, extvar_t *ev)
 	make_export("GP_MASTER_PORT", ev->GP_MASTER_PORT, &buf);
 	make_export("GP_SEG_PG_CONF", ev->GP_SEG_PG_CONF, &buf);
 	make_export("GP_SEG_DATADIR", ev->GP_SEG_DATADIR, &buf);
+	make_export("GP_SEG_LOGDIR", ev->GP_SEG_LOGDIR, &buf);
 	make_export("GP_DATABASE", ev->GP_DATABASE, &buf);
 	make_export("GP_USER", ev->GP_USER, &buf);
 	make_export("GP_DATE", ev->GP_DATE, &buf);

greenplum-db/gpdb

commit 738a7578e4cd95b1da9fd6c105cc6fe549a1d150

url.h: Fix ABI breakage in extvar_t
e358573762ee35a40f95ba96630f176b0cd795cc introduced a new field in
extvar_t called GP_SEG_LOGDIR. It was done in the middle of the struct.
So we move it to the end of the struct as a mitigation.

Reviewed-by: Ashwin Agrawal <aashwin@vmware.com>

src/include/access/url.h
@@ -54,7 +54,6 @@ typedef struct extvar_t
 	char* GP_USER;
 	char* GP_SEG_PG_CONF;   /*location of the segments pg_conf file*/
 	char* GP_SEG_DATADIR;   /*location of the segments datadirectory*/
-	char* GP_SEG_LOGDIR;    /*location of the segment's log directory*/
 	char GP_DATE[9];		/* YYYYMMDD */
 	char GP_TIME[7];		/* HHMMSS */
 	char GP_XID[TMGIDSIZE];		/* global transaction id */
@@ -70,6 +69,8 @@ typedef struct extvar_t
  	char* GP_LINE_DELIM_STR;
 	char GP_LINE_DELIM_LENGTH[11];
 	char *GP_QUERY_STRING;
+
+	char *GP_SEG_LOGDIR;    /* location of the segment's log directory */
 } extvar_t;
 
 