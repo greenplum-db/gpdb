9d7e36e37600b009eac119ab7f76306db9a87225
David Kimura <dkimura@vmware.com>
2023-10-28 04:40:23 +0000

    Incorporate more testcase feedback
     - Add another mutibyte character set to the test output
    
     - Combined PLANNER/ORCA answer file as the test doesn't really need the
       explain plans.

--- src/test/regress/sql/gp_locale.sql
+++ src/test/regress/sql/gp_locale.sql
@@ -1,44 +1,52 @@
+-- ORCA uses functions (e.g. vswprintf) to translation to wide character
+-- format. But those libraries may fail if the current locale cannot handle the
+-- character set. This test checks that even when those libraries fail, ORCA is
+-- still able to generate plans.
+
+--
+-- Create a database that sets the minimum locale
+--
 DROP DATABASE IF EXISTS testdb3;
 CREATE DATABASE testdb3 WITH LC_COLLATE='C' LC_CTYPE='C' TEMPLATE=template0;
 \c testdb3
 
+--
+-- drop/add/remove columns
+--
 CREATE TABLE hi_안녕세계 (a int, 안녕세계1 text, 안녕세계2 text, 안녕세계3 text) DISTRIBUTED BY (a);
--- DROP/ADD/RENAME columns
 ALTER TABLE hi_안녕세계 DROP COLUMN 안녕세계2;
 ALTER TABLE hi_안녕세계 ADD COLUMN 안녕세계2_ADD_COLUMN text;
-ALTER TABLE hi_안녕세계 RENAME COLUMN 안녕세계3 TO 안녕세계3_RENAME;
+ALTER TABLE hi_안녕세계 RENAME COLUMN 안녕세계3 TO こんにちわ3;
 
 INSERT INTO hi_안녕세계 VALUES(1, '안녕세계1 first', '안녕세2 first', '안녕세계3 first');
 INSERT INTO hi_안녕세계 VALUES(42, '안녕세계1 second', '안녕세2 second', '안녕세계3 second');
 
+--
+-- Try various queries containing multibyte character set and check the column
+-- name output
+--
 SET optimizer_trace_fallback=on;
 
 -- DELETE
-EXPLAIN DELETE FROM hi_안녕세계 WHERE a=42;
 DELETE FROM hi_안녕세계 WHERE a=42;
 
 -- UPDATE
-EXPLAIN UPDATE hi_안녕세계 SET 안녕세계1='안녕세계1 first UPDATE' WHERE 안녕세계1='안녕세계1 first';
 UPDATE hi_안녕세계 SET 안녕세계1='안녕세계1 first UPDATE' WHERE 안녕세계1='안녕세계1 first';
 
 -- SELECT
-EXPLAIN SELECT * FROM hi_안녕세계;
 SELECT * FROM hi_안녕세계;
 
 -- SELECT ALIAS
-EXPLAIN SELECT 안녕세계1 AS 안녕세계1_Alias FROM hi_안녕세계;
 SELECT 안녕세계1 AS 안녕세계1_Alias FROM hi_안녕세계;
 
 -- SUBQUERY
-EXPLAIN SELECT * FROM (SELECT 안녕세계1 FROM hi_안녕세계) t;
 SELECT * FROM (SELECT 안녕세계1 FROM hi_안녕세계) t;
 
 -- CTE
-EXPLAIN WITH cte AS
-(SELECT 안녕세계1, 안녕세계2_ADD_COLUMN FROM hi_안녕세계) SELECT * FROM cte WHERE 안녕세계1 LIKE '안녕세계1%';
 WITH cte AS
-(SELECT 안녕세계1, 안녕세계2_ADD_COLUMN FROM hi_안녕세계) SELECT * FROM cte WHERE 안녕세계1 LIKE '안녕세계1%';
+(SELECT 안녕세계1, こんにちわ3 FROM hi_안녕세계) SELECT * FROM cte WHERE 안녕세계1 LIKE '안녕세계1%';
 
 -- JOIN
-EXPLAIN SELECT * FROM hi_안녕세계 hi_안녕세계1, hi_안녕세계 hi_안녕세계2 WHERE hi_안녕세계1.안녕세계1 LIKE '%UPDATE';
 SELECT * FROM hi_안녕세계 hi_안녕세계1, hi_안녕세계 hi_안녕세계2 WHERE hi_안녕세계1.안녕세계1 LIKE '%UPDATE';
+
+RESET optimizer_trace_fallback;
