bb4a96c5d6edb84b1d520e19417389148ce76ffa
Gaurab Dey <gaurabd@vmware.com>
2024-01-11 16:49:44 -0800

    Replace os.getlogin to get correct user name
    - os.getlogin() is not the function we should use to get the username;
      it provides the user logged in on the controlling terminal of the
    process. You might get different user name based on who logged in to the
    terminal rather than who is executing the binary.
    
    Authored-by: Gaurab Dey <gaurabd@vmware.com>

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -30,6 +30,7 @@ from __future__ import print_function
 import argparse
 import datetime
 import os
+import pwd
 import re
 import subprocess
 import yaml
@@ -364,7 +365,7 @@ def main():
         '--user',
         action='store',
         dest='user',
-        default=os.getlogin(),
+        default=pwd.getpwuid(os.geteuid())[0],
         help='Developer userid to use for pipeline name and filename.'
     )
 
@@ -407,7 +408,7 @@ def main():
     validate_target(args.pipeline_target)
 
     output_path_is_set = os.path.basename(args.output_filepath) != default_output_filename
-    if (args.user != os.getlogin() and output_path_is_set):
+    if (args.user != pwd.getpwuid(os.geteuid())[0] and output_path_is_set):
         print("You can only use one of --output or --user.")
         exit(1)
 
@@ -444,7 +445,7 @@ def main():
     # don't overwrite the 6X_STABLE pipeline
     if args.pipeline_target != 'prod' and not output_path_is_set:
         pipeline_file_suffix = suggested_git_branch()
-        if args.user != os.getlogin():
+        if args.user != pwd.getpwuid(os.geteuid())[0]:
             pipeline_file_suffix = args.user
         default_dev_output_filename = 'gpdb-' + args.pipeline_target + '-' + pipeline_file_suffix + '.yml'
         args.output_filepath = os.path.join(PIPELINES_DIR, default_dev_output_filename)
