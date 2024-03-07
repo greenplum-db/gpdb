1c17ba516ca532524fc9ff943b0788f28e303d24
Annpurna Shahani <ashahani@vmware.com>
2024-03-07 10:28:06 +0530

    [7X] gpstate -e : track startup recovery remaining bytes
    Issue: gpstate -e shows wal sync remaining bytes as 'unknown' when a failed segment is recovered and it is undergoing startup recovery. Also segment's status is shown as down in configuration which is misleading to the user. Instead of showing unknown gpstate should let the user know that segment is undergoing startup recovery with startup recovery remaining bytes.
    
    Fix: If a segment is undergoing startup recovery gpstate -e will calculate and show the startup recovery remaining bytes. Wal syncing will start after the completion of startup recovery so 'Wal sync remaining bytes' will be shown as 'Not started yet'. After the completion of startup recovery, startup recovery remaining bytes will be shown as 'Completedi' with WAL sync remaining bytes.
    
    Testing: Have added unit and behave test

--- gpMgmt/bin/gppylib/util/gp_utils.py
+++ gpMgmt/bin/gppylib/util/gp_utils.py
@@ -5,6 +5,15 @@
 # Greenplum DB related utility functions
 
 import os
+from contextlib import closing
+
+from gppylib.commands.base import Command
+from gppylib.commands.pg import PgControlData
+from gppylib.commands import base
+from gppylib.db import dbconn
+from gppylib import gplog
+
+logger = gplog.get_default_logger()
 
 def get_gp_prefix(coordinatorDatadir):
     base = os.path.basename(coordinatorDatadir)
@@ -13,3 +22,85 @@ def get_gp_prefix(coordinatorDatadir):
         return None
     return base[0:idx]
 
+
+# returns startup recovery remaining bytes
+def get_startup_recovery_remaining_bytes(hostname, port, datadir):
+    """
+        To get Wal file that is being currently recovered, grep the startup process of the segment.
+        the process looks like below and contains the walfilename
+        gpadmin  30271 30184 90 19:04 ?        00:29:06 postgres:  7001, startup   recovering 000000030000052D0000000C
+    """
+
+    string = 'postgres:\s+[0-9]+,\s+startup\s+recovering\s+[0-9A-F]{24}'
+    cmd_str = "ps -ef | grep -v grep | grep '{}' | grep -Eo '{}'".format(port, string)
+    cmd = Command(name='Running Remote command: {}'.format(cmd_str), cmdStr=cmd_str, ctxt=base.REMOTE, remoteHost=hostname)
+    cmd.run(validateAfter=False)
+    if cmd.get_return_code() != 0:
+        if cmd.get_return_code() != 1:
+            logger.debug("Command {} failed. err: {}".format(cmd_str, cmd.get_stderr()))
+        else:
+            logger.debug("No startup recovery process running")
+        return
+
+    recovery_walfile = cmd.get_stdout().split(" ")[-1]
+    if len(recovery_walfile) != 24:
+        logger.debug("Could not fetch startup recovery walfilename")
+        return
+
+    # Get the 'Minimum recovery ending location' from mirror pg_controldata
+    cmd = PgControlData(name='run pg_controldata', datadir=datadir, ctxt=base.REMOTE, remoteHost=hostname)
+    cmd.run(validateAfter=False)
+    if cmd.get_return_code() != 0:
+        logger.debug("Could not fetch 'Minimum recovery ending location' from pg_controldata")
+        return
+    min_recovery_ending_location = cmd.get_value('Minimum recovery ending location')  # value can be possibly None?
+
+    try:
+        with closing(dbconn.connect(dbconn.DbURL())) as conn:
+            wal_segment_size_bytes = dbconn.querySingleton(conn, "SELECT ps.setting::int FROM pg_show_all_settings() "
+                                                                 "ps WHERE ps.name = 'wal_segment_size';")
+            min_recovery_ending_location_walfile = dbconn.querySingleton(conn, "select pg_walfile_name('{}')".format(min_recovery_ending_location))
+    except Exception as e:
+        logger.debug("Failed to get either wal_segment_size or walfile_name of Minimum recovery ending location, "
+                     "err: {}".format(str(e)))
+        return
+
+    try:
+        current_walfile_bytes = split_walfile_name(recovery_walfile, wal_segment_size_bytes)[1]*wal_segment_size_bytes
+        final_walfile_bytes = split_walfile_name(min_recovery_ending_location_walfile, wal_segment_size_bytes)[1]*wal_segment_size_bytes
+    except Exception as e:
+        logger.debug("Failed to split walfile_name, err: {}".format(str(e)))
+        return
+
+    startup_recovery_remaining_bytes = final_walfile_bytes - current_walfile_bytes
+
+    return startup_recovery_remaining_bytes
+
+
+# GPDB_16_MERGE_FIXME: Use pg_split_walfile_name() when it becomes available and remove below function.
+def split_walfile_name(fname, wal_segment_size):
+    # Convert fname to uppercase, simulating pg_toupper behavior
+    fname_upper = fname.upper()
+
+    XLOG_FNAME_LEN = 24
+
+    # Simulate IsXLogFileName function
+    def is_xlog_file_name(fname):
+        valid_chars = set("0123456789ABCDEF")
+        return len(fname) == XLOG_FNAME_LEN and all(char in valid_chars for char in fname)
+
+    if not is_xlog_file_name(fname_upper):
+        raise ValueError("Invalid WAL file name \"{}\"".format(fname))
+
+    # Simulate the XLogFromFileName function to extract tli and segno
+    # Assuming wal_segment_size is provided
+    def xlog_from_file_name(fname, wal_segsz_bytes):
+        tli = int(fname[:8], 16)
+        log = int(fname[8:16], 16)
+        seg = int(fname[16:24], 16)
+        segno = log * (0x100000000 // wal_segsz_bytes) + seg
+        return tli, segno
+
+    tli, segno = xlog_from_file_name(fname_upper, wal_segment_size)
+
+    return tli, segno
---
+++ gpMgmt/bin/gppylib/util/test/unit/test_unit_gp_utils.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Greenplum Inc 2008. All Rights Reserved.
+#
+# Unit Testing of catalog module.
+#
+
+from mock import call, Mock, patch, ANY, MagicMock
+from gppylib.test.unit.gp_unittest import GpTestCase, FakeCursor
+from gppylib.util.gp_utils import get_startup_recovery_remaining_bytes
+from gppylib.util.gp_utils import split_walfile_name
+
+
+class GpUtilsTestCase(GpTestCase):
+    def setUp(self):
+        mock_logger = Mock(spec=['log', 'warn', 'info', 'debug', 'error', 'warning', 'fatal'])
+        self.apply_patches([
+            patch('gppylib.commands.gp.Command.__init__', return_value=None),
+            patch('gppylib.commands.gp.Command.run', return_value=None),
+            patch('gppylib.commands.gp.Command.get_return_code', return_value=0),
+            patch('gppylib.commands.gp.Command.get_stdout', return_value="000000030000052D0000000C"),
+            patch('gppylib.commands.pg.PgControlData.run'),
+            patch('gppylib.commands.pg.PgControlData.get_value', return_value="BAD/9698CA28"),
+            patch('gppylib.db.dbconn.DbURL'),
+            patch('gppylib.db.dbconn.connect'),
+            patch('gppylib.db.dbconn.querySingleton'),
+            patch('gppylib.util.gp_utils.logger', return_value=mock_logger)
+        ])
+
+        self.mock_logger = self.get_mock_from_apply_patch('logger')
+
+    @patch('gppylib.commands.gp.Command.get_return_code', return_value=1)
+    def test_get_startup_recovery_remaining_bytes_command_fails(self, mock):
+        self.assertEqual(get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1"), None)
+        self.assertEqual([call("No startup recovery process running")], self.mock_logger.debug.call_args_list)
+
+    @patch('gppylib.commands.gp.Command.get_stdout', return_value="")
+    def test_get_startup_recovery_remaining_bytes_returns_None(self, mock):
+        self.assertEqual(get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1"), None)
+        self.assertEqual([call("Could not fetch startup recovery walfilename")], self.mock_logger.debug.call_args_list)
+
+    @patch('gppylib.commands.pg.PgControlData.get_return_code', return_value=1)
+    def test_get_startup_recovery_remaining_bytes_pg_control_data_fails(self, mock):
+        get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1")
+        self.assertEqual([call("Could not fetch 'Minimum recovery ending location' from pg_controldata")], self.mock_logger.debug.call_args_list)
+
+    @patch('gppylib.db.dbconn.connect', side_effect=Exception('Error'))
+    def test_get_startup_recovery_remaining_bytes_db_connect_raises_exception(self, mock):
+        get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1")
+        self.assertEqual([call("Failed to get either wal_segment_size or walfile_name of Minimum recovery ending location, err: Error")], self.mock_logger.debug.call_args_list)
+
+    @patch('gppylib.db.dbconn.querySingleton', side_effect=Exception('Error'))
+    def test_get_startup_recovery_remaining_bytes_db_query_raises_exception(self, mock):
+        get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1")
+        self.assertEqual([call("Failed to get either wal_segment_size or walfile_name of Minimum recovery ending location, err: Error")], self.mock_logger.debug.call_args_list)
+
+    @patch('gppylib.util.gp_utils.split_walfile_name', side_effect=Exception('Error'))
+    def test_get_startup_recovery_remaining_bytes_splitwalfilename_fails(self, mock):
+        get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1")
+        self.assertEqual([call("Failed to split walfile_name, err: Error")],
+                         self.mock_logger.debug.call_args_list)
+
+    @patch('gppylib.db.dbconn.querySingleton', side_effect=[67108864, "0000000100000BAD00000025"])
+    def test_get_startup_recovery_remaining_bytes_succeeds(self, mock):
+        self.assertEqual(get_startup_recovery_remaining_bytes("sdw1", 7002, "/data/demo/1"), 7148503302144)
+
+    def test_split_walfile_name_raises_exception(self):
+        with self.assertRaises(Exception) as ex:
+            split_walfile_name("000000030000052D000000.C", 67108864)
+        self.assertTrue('Invalid WAL file name "000000030000052D000000.C"' in str(ex.exception))
+
+    def test_split_walfile_name_succeeds(self):
+        tli, segno = split_walfile_name("000000030000052D0000000C", 67108864)
+        self.assertEqual(tli, 3)
+        self.assertEqual(segno, 84812)
+
+
+
